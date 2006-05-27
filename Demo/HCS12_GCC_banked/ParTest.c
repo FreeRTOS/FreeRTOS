215d93aa3d06e81e8879ec2b20043a443d5c80e1
Richard Barry <ribarry@amazon.com>
2006-05-27 13:53:15 +0000

    New demo files for HCS12 GCC port.

---
+++ Demo/HCS12_GCC_banked/ParTest.c
@@ -0,0 +1,78 @@
+/*
+	FreeRTOS V3.2.3 - Copyright (C) 2003-2005 Richard Barry.
+
+	This file is part of the FreeRTOS distribution.
+
+	FreeRTOS is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	FreeRTOS is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with FreeRTOS; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+	A special exception to the GPL can be applied should you wish to distribute
+	a combined work that includes FreeRTOS, without being obliged to provide
+	the source code for any proprietary components.  See the licensing section 
+	of http://www.FreeRTOS.org for full details of how and when the exception
+	can be applied.
+
+	***************************************************************************
+	See http://www.FreeRTOS.org for documentation, latest information, license 
+	and contact details.  Please ensure to read the configuration and relevant 
+	port sections of the online documentation.
+	***************************************************************************
+*/
+
+/** 
+ * ParTest.c controls bits (LEDs) for GCC/HCS12 version of FreeRTOS Demo
+ *
+ * Modified from CodeWarrior/HCS12 by Jefferson L Smith, Robotronics Inc.
+ */
+
+#include <sys/ports.h>
+
+/* Scheduler include files. */
+#include "FreeRTOS.h"
+#include "portable.h"
+
+/* Demo application include files. */
+#include "partest.h"
+
+#define LEDIO	PORTIO_8(PORT_LED)
+
+/*-----------------------------------------------------------
+ * Simple parallel port IO routines.
+ *-----------------------------------------------------------*/
+
+void vParTestSetLED( unsigned portBASE_TYPE uxLED, signed portBASE_TYPE xValue )
+{
+	/* This function is required as it is called from the standard demo 
+	application files.  It manipulates a bit to control one LED. */
+	portENTER_CRITICAL();
+
+	if (xValue) {                       /* Is it one to be written? */
+		LEDIO |= (1<<uxLED);            /* Set appropriate bit on port */
+	}
+	else {                             /* Is it zero to be written? */
+		LEDIO &= ~(1<<uxLED);          /* Clear appropriate bit on port */
+	}
+	portEXIT_CRITICAL();
+}
+/*-----------------------------------------------------------*/
+
+void vParTestToggleLED( unsigned portBASE_TYPE uxLED )
+{
+	/* This function is required as it is called from the standard demo
+	application files.  It manipulates a bit to control one LED. */
+	portENTER_CRITICAL();
+		LEDIO ^= (1<<uxLED);           /* Invert appropriate bit on port */
+	portEXIT_CRITICAL();
+}
+
---
+++ Demo/HCS12_GCC_banked/startup.c
@@ -0,0 +1,86 @@
+/*
+	FreeRTOS V3.2.3 - Copyright (C) 2003-2005 Richard Barry.
+
+	This file is part of the FreeRTOS distribution.
+
+	FreeRTOS is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	FreeRTOS is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with FreeRTOS; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+	A special exception to the GPL can be applied should you wish to distribute
+	a combined work that includes FreeRTOS, without being obliged to provide
+	the source code for any proprietary components.  See the licensing section 
+	of http://www.FreeRTOS.org for full details of how and when the exception
+	can be applied.
+
+	***************************************************************************
+	See http://www.FreeRTOS.org for documentation, latest information, license 
+	and contact details.  Please ensure to read the configuration and relevant 
+	port sections of the online documentation.
+	***************************************************************************
+*/
+
+
+/*
+ * startup.c
+ * Author Jefferson L Smith, Robotronics Inc.
+ *
+ * __premain() is the startup code to init hardware and ram to execute the
+ *             C application.
+ *
+ */
+
+#include <sys/ports.h>
+#include "cpu.h"
+
+void ATTR_NEAR __premain (void);
+
+void
+__premain (void)
+{
+	// in case special mode enabled, avoid conflict on PORTE
+	PEAR |= NECLK;
+	// bgnd mode stops COP and RTI clocks
+	COPCTL = RSBCK;
+	// stops TCNT counter when debugging stops
+	TSCR1 |= (1<<5);			// TFRZ
+	
+	// PLL
+	CLKSEL = 0;				// disable PLL to configure
+	// xtal 16MHz, bus 24MHz
+	SYNR  = 3 - 1;
+	REFDV = 2 - 1;
+	while (!(CRGFLG & 0x08))    // wait for PLL LOCK
+	cop_optional_reset();
+	CLKSEL |= 0x80;             // use PLL
+
+	// init switch inputs
+	PERH = 0xff;				// pullups
+
+	// outputs
+#if PORT_LED==M6811_PORTB		//PORTB
+	DDRB = 0xff;	// init LED
+#elif PORT_LED==M6811_PORTA		//PORTA
+	DDRA = 0xff;
+#elif PORT_LED==M6811_PTT	//PTT
+	DDRT = 0xff;
+#elif PORT_LED==M6811_PTM	//PTM
+	DDRM = 0xff;
+#elif PORT_LED==M6811_PTP	//PTP
+	DDRP = 0xff;
+#elif PORT_LED==M6811_PTH	//PTH
+	DDRH = 0xff;
+#endif
+	
+}
+

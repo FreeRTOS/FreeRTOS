03a82c5bc83d86346ff9bed6991620f21fee7493
Richard Barry <ribarry@amazon.com>
2008-05-23 19:22:33 +0000

    Add #define configQUEUE_REGISTRY_SIZE	0

--- Demo/PIC32MX_MPLAB/FreeRTOSConfig.h
+++ Demo/PIC32MX_MPLAB/FreeRTOSConfig.h
@@ -78,6 +78,7 @@
 #define configIDLE_SHOULD_YIELD			1
 #define configUSE_MUTEXES				1
 #define configCHECK_FOR_STACK_OVERFLOW	2
+#define configQUEUE_REGISTRY_SIZE		0
 
 /* Co-routine definitions. */
 #define configUSE_CO_ROUTINES 		0

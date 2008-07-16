1d4e319aa7fc9194e234407fda7801babb8c8c59
Richard Barry <ribarry@amazon.com>
2008-07-16 09:08:32 +0000

    Ensure the queue registry gets initialised.

--- Source/queue.c
+++ Source/queue.c
@@ -162,7 +162,7 @@ unsigned portBASE_TYPE uxQueueMessagesWaitingFromISR( const xQueueHandle pxQueue
 	/* The queue registry is simply an array of xQueueRegistryItem structures.
 	The pcQueueName member of a structure being NULL is indicative of the
 	array position being vacant. */
-	xQueueRegistryItem xQueueRegistry[ configQUEUE_REGISTRY_SIZE ];
+	xQueueRegistryItem xQueueRegistry[ configQUEUE_REGISTRY_SIZE ] = { 0 };
 
 	/* Removes a queue from the registry by simply setting the pcQueueName
 	member to NULL. */

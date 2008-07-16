91805dd05160c3e8d7921d280175beb60df3757e
Richard Barry <ribarry@amazon.com>
2008-07-16 09:49:31 +0000

    Add the queue registry config parameter.

--- Demo/CORTEX_LM3Sxxxx_Eclipse/RTOSDemo/FreeRTOSConfig.h
+++ Demo/CORTEX_LM3Sxxxx_Eclipse/RTOSDemo/FreeRTOSConfig.h
@@ -75,6 +75,7 @@
 #define configUSE_MUTEXES				1
 #define configCHECK_FOR_STACK_OVERFLOW	2
 #define configUSE_RECURSIVE_MUTEXES		1
+#define configQUEUE_REGISTRY_SIZE		10
 
 #define configMAX_PRIORITIES		( ( unsigned portBASE_TYPE ) 5 )
 #define configMAX_CO_ROUTINE_PRIORITIES ( 2 )

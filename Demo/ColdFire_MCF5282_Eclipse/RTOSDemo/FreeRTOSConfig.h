0dffe8d801ced721627c15721267049d1406914b
Richard Barry <ribarry@amazon.com>
2008-08-11 14:51:09 +0000

    Increase stack sizes as we now have interrupts nesting 3 deep.

--- Demo/ColdFire_MCF5282_Eclipse/RTOSDemo/FreeRTOSConfig.h
+++ Demo/ColdFire_MCF5282_Eclipse/RTOSDemo/FreeRTOSConfig.h
@@ -67,8 +67,8 @@
 #define configUSE_TICK_HOOK				0
 #define configCPU_CLOCK_HZ				( ( unsigned portLONG ) 64000000 )
 #define configTICK_RATE_HZ				( ( portTickType ) 1000 )
-#define configMINIMAL_STACK_SIZE		( ( unsigned portSHORT ) 100 )
-#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 30000 ) )
+#define configMINIMAL_STACK_SIZE		( ( unsigned portSHORT ) 160 )
+#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 40000 ) )
 #define configMAX_TASK_NAME_LEN			( 12 )
 #define configUSE_TRACE_FACILITY		1
 #define configUSE_16_BIT_TICKS			0

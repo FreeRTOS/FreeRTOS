c59af80374033a04b39b7d06d0fce707ba385250
Richard Barry <ribarry@amazon.com>
2008-10-23 11:08:14 +0000

    Add demo for the Keil RVDS compiler.

---
+++ Demo/ARM7_LPC2129_Keil_RVDS/main.c
@@ -0,0 +1,301 @@
+/*
+	FreeRTOS.org V5.0.4 - Copyright (C) 2003-2008 Richard Barry.
+
+	This file is part of the FreeRTOS.org distribution.
+
+	FreeRTOS.org is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	FreeRTOS.org is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with FreeRTOS.org; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+	A special exception to the GPL can be applied should you wish to distribute
+	a combined work that includes FreeRTOS.org, without being obliged to provide
+	the source code for any proprietary components.  See the licensing section 
+	of http://www.FreeRTOS.org for full details of how and when the exception
+	can be applied.
+
+    ***************************************************************************
+    ***************************************************************************
+    *                                                                         *
+    * SAVE TIME AND MONEY!  We can port FreeRTOS.org to your own hardware,    *
+    * and even write all or part of your application on your behalf.          *
+    * See http://www.OpenRTOS.com for details of the services we provide to   *
+    * expedite your project.                                                  *
+    *                                                                         *
+    ***************************************************************************
+    ***************************************************************************
+
+	Please ensure to read the configuration and relevant port sections of the
+	online documentation.
+
+	http://www.FreeRTOS.org - Documentation, latest information, license and 
+	contact details.
+
+	http://www.SafeRTOS.com - A version that is certified for use in safety 
+	critical systems.
+
+	http://www.OpenRTOS.com - Commercial support, development, porting, 
+	licensing and training services.
+*/
+
+/* 
+	NOTE : Tasks run in system mode and the scheduler runs in Supervisor mode.
+	The processor MUST be in supervisor mode when vTaskStartScheduler is 
+	called.  The demo applications included in the FreeRTOS.org download switch
+	to supervisor mode prior to main being called.  If you are not using one of
+	these demo application projects then ensure Supervisor mode is used.
+*/
+
+
+/*
+ * Creates all the demo application tasks, then starts the scheduler.  The WEB
+ * documentation provides more details of the demo application tasks.
+ * 
+ * Main.c also creates a task called "Check".  This only executes every three 
+ * seconds but has the highest priority so is guaranteed to get processor time.  
+ * Its main function is to check that all the other tasks are still operational.
+ * Each task (other than the "flash" tasks) maintains a unique count that is 
+ * incremented each time the task successfully completes its function.  Should 
+ * any error occur within such a task the count is permanently halted.  The 
+ * check task inspects the count of each task to ensure it has changed since
+ * the last time the check task executed.  If all the count variables have 
+ * changed all the tasks are still executing error free, and the check task
+ * toggles the onboard LED.  Should any task contain an error at any time 
+ * the LED toggle rate will change from 3 seconds to 500ms.
+ *
+ */
+
+/* Standard includes. */
+#include <stdlib.h>
+
+/* Scheduler includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+
+/* Demo application includes. */
+#include "partest.h"
+#include "flash.h"
+#include "integer.h"
+#include "comtest2.h"
+#include "serial.h"
+
+#ifdef KEIL_THUMB_INTERWORK
+
+	/* 
+		THUMB mode allows more tasks to be created without the executable 
+		binary exceeding the limits allowed by the evaluation version of 
+		uVision3.
+	*/
+	#include "PollQ.h"
+	#include "BlockQ.h"
+	#include "semtest.h"
+	#include "dynamic.h"
+
+#endif /* KEIL_THUMB_INTERWORK */
+
+/*-----------------------------------------------------------*/
+
+/* Constants to setup I/O and processor. */
+#define mainTX_ENABLE		( ( unsigned portLONG ) 0x0001 )
+#define mainRX_ENABLE		( ( unsigned portLONG ) 0x0004 )
+#define mainBUS_CLK_FULL	( ( unsigned portCHAR ) 0x01 )
+#define mainLED_TO_OUTPUT	( ( unsigned portLONG ) 0xff0000 )
+
+/* Constants for the ComTest demo application tasks. */
+#define mainCOM_TEST_BAUD_RATE	( ( unsigned portLONG ) 115200 )
+#define mainCOM_TEST_LED		( 3 )
+
+/* Priorities for the demo application tasks. */
+#define mainLED_TASK_PRIORITY		( tskIDLE_PRIORITY + 3 )
+#define mainCOM_TEST_PRIORITY		( tskIDLE_PRIORITY + 2 )
+#define mainQUEUE_POLL_PRIORITY		( tskIDLE_PRIORITY + 2 )
+#define mainBLOCK_Q_PRIORITY		( tskIDLE_PRIORITY + 2 )
+#define mainSEM_TEST_PRIORITY		( tskIDLE_PRIORITY + 1 )
+#define mainCHECK_TASK_PRIORITY		( tskIDLE_PRIORITY + 4 )
+
+/* Constants used by the "check" task.  As described at the head of this file
+the check task toggles an LED.  The rate at which the LED flashes is used to
+indicate whether an error has been detected or not.  If the LED toggles every
+3 seconds then no errors have been detected.  If the rate increases to 500ms
+then an error has been detected in at least one of the demo application tasks. */
+#define mainCHECK_LED				( 7 )
+#define mainNO_ERROR_FLASH_PERIOD	( ( portTickType ) 3000 / portTICK_RATE_MS  )
+#define mainERROR_FLASH_PERIOD		( ( portTickType ) 500 / portTICK_RATE_MS  )
+
+/*-----------------------------------------------------------*/
+
+/*
+ * Checks that all the demo application tasks are still executing without error
+ * - as described at the top of the file.
+ */
+static portLONG prvCheckOtherTasksAreStillRunning( void );
+
+/*
+ * The task that executes at the highest priority and calls 
+ * prvCheckOtherTasksAreStillRunning().  See the description at the top
+ * of the file.
+ */
+static void vErrorChecks( void *pvParameters );
+
+/*
+ * Configure the processor for use with the Keil demo board.  This is very
+ * minimal as most of the setup is managed by the settings in the project
+ * file.
+ */
+static void prvSetupHardware( void );
+
+/*-----------------------------------------------------------*/
+
+
+
+/*
+ * Application entry point:
+ * Starts all the other tasks, then starts the scheduler. 
+ */
+int main( void )
+{
+	/* Setup the hardware for use with the Keil demo board. */
+	prvSetupHardware();
+
+	/* Start the demo/test application tasks. */
+	vStartIntegerMathTasks( tskIDLE_PRIORITY );
+	vAltStartComTestTasks( mainCOM_TEST_PRIORITY, mainCOM_TEST_BAUD_RATE, mainCOM_TEST_LED );
+	vStartLEDFlashTasks( mainLED_TASK_PRIORITY );
+
+	#ifdef KEIL_THUMB_INTERWORK
+		/* When using THUMB mode we can start more tasks without the executable
+		exceeding the size limit imposed by the evaluation version of uVision3. */
+		vStartPolledQueueTasks( mainQUEUE_POLL_PRIORITY );
+		vStartBlockingQueueTasks( mainBLOCK_Q_PRIORITY );
+		vStartSemaphoreTasks( mainSEM_TEST_PRIORITY );
+		vStartDynamicPriorityTasks();
+	#endif
+
+	/* Start the check task - which is defined in this file.  This is the task
+	that periodically checks to see that all the other tasks are executing 
+	without error. */
+	xTaskCreate( vErrorChecks, "Check", configMINIMAL_STACK_SIZE, NULL, mainCHECK_TASK_PRIORITY, NULL );
+
+	/* Now all the tasks have been started - start the scheduler.
+
+	NOTE : Tasks run in system mode and the scheduler runs in Supervisor mode.
+	The processor MUST be in supervisor mode when vTaskStartScheduler is 
+	called.  The demo applications included in the FreeRTOS.org download switch
+	to supervisor mode prior to main being called.  If you are not using one of
+	these demo application projects then ensure Supervisor mode is used here. */
+	vTaskStartScheduler();
+
+	/* Should never reach here!  If you do then there was not enough heap
+	available for the idle task to be created. */
+	for( ;; );
+}
+/*-----------------------------------------------------------*/
+
+static void vErrorChecks( void *pvParameters )
+{
+portTickType xDelayPeriod = mainNO_ERROR_FLASH_PERIOD;
+
+	/* Parameters are not used. */
+	( void ) pvParameters;
+
+	/* Cycle for ever, delaying then checking all the other tasks are still
+	operating without error.  If an error is detected then the delay period
+	is decreased from mainNO_ERROR_FLASH_PERIOD to mainERROR_FLASH_PERIOD so
+	the on board LED flash rate will increase.
+
+	This task runs at the highest priority. */
+
+	for( ;; )
+	{
+		/* The period of the delay depends on whether an error has been 
+		detected or not.  If an error has been detected then the period
+		is reduced to increase the LED flash rate. */
+		vTaskDelay( xDelayPeriod );
+
+		if( prvCheckOtherTasksAreStillRunning() != pdPASS )
+		{
+			/* An error has been detected in one of the tasks - flash faster. */
+			xDelayPeriod = mainERROR_FLASH_PERIOD;
+		}
+
+		/* Toggle the LED before going back to wait for the next cycle. */
+		vParTestToggleLED( mainCHECK_LED );
+	}
+}
+/*-----------------------------------------------------------*/
+
+static void prvSetupHardware( void )
+{
+	/* Perform the hardware setup required.  This is minimal as most of the
+	setup is managed by the settings in the project file. */
+
+	/* Configure the RS2332 pins.  All other pins remain at their default of 0. */
+	PINSEL0 |= mainTX_ENABLE;
+	PINSEL0 |= mainRX_ENABLE;
+
+	/* LED pins need to be output. */
+	IODIR1 = mainLED_TO_OUTPUT;
+
+	/* Setup the peripheral bus to be the same as the PLL output. */
+	VPBDIV = mainBUS_CLK_FULL;
+}
+/*-----------------------------------------------------------*/
+
+static portLONG prvCheckOtherTasksAreStillRunning( void )
+{
+portLONG lReturn = pdPASS;
+
+	/* Check all the demo tasks (other than the flash tasks) to ensure
+	that they are all still running, and that none of them have detected
+	an error. */
+	if( xAreIntegerMathsTaskStillRunning() != pdPASS )
+	{
+		lReturn = pdFAIL;
+	}
+
+	if( xAreComTestTasksStillRunning() != pdPASS )
+	{
+		lReturn = pdFAIL;
+	}
+
+	#ifdef KEIL_THUMB_INTERWORK
+
+		/* When using THUMB mode we can start more tasks without the executable
+		exceeding the size limit imposed by the evaluation version of uVision3. */
+	
+		if( xArePollingQueuesStillRunning() != pdTRUE )
+		{
+			lReturn = pdFAIL;
+		}
+	
+		if( xAreBlockingQueuesStillRunning() != pdTRUE )
+		{
+			lReturn = pdFAIL;
+		}
+	
+		if( xAreSemaphoreTasksStillRunning() != pdTRUE )
+		{
+			lReturn = pdFAIL;
+		}
+
+		if( xAreDynamicPriorityTasksStillRunning() != pdTRUE )
+		{
+			lReturn = pdFAIL;
+		}
+
+	#endif
+
+	return lReturn;
+}
+/*-----------------------------------------------------------*/
+
+
---
+++ Demo/ARM7_LPC2129_Keil_RVDS/serial/serialISR.s
@@ -0,0 +1,85 @@
+;/*
+;	FreeRTOS.org V5.0.4 - Copyright (C) 2003-2008 Richard Barry.
+;
+;	This file is part of the FreeRTOS.org distribution.
+;
+;	FreeRTOS.org is free software; you can redistribute it and/or modify
+;	it under the terms of the GNU General Public License as published by
+;	the Free Software Foundation; either version 2 of the License, or
+;	(at your option) any later version.
+; 
+;	FreeRTOS.org is distributed in the hope that it will be useful,
+;	but WITHOUT ANY WARRANTY; without even the implied warranty of
+;	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;	GNU General Public License for more details.
+;
+;	You should have received a copy of the GNU General Public License
+;	along with FreeRTOS.org; if not, write to the Free Software
+;	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+;
+;	A special exception to the GPL can be applied should you wish to distribute
+;	a combined work that includes FreeRTOS.org, without being obliged to provide
+;	the source code for any proprietary components.  See the licensing section 
+;	of http://www.FreeRTOS.org for full details of how and when the exception
+;	can be applied.
+;
+;   ***************************************************************************
+;   ***************************************************************************
+;   *                                                                         *
+;   * SAVE TIME AND MONEY!  We can port FreeRTOS.org to your own hardware,    *
+;   * and even write all or part of your application on your behalf.          *
+;   * See http://www.OpenRTOS.com for details of the services we provide to   *
+;   * expedite your project.                                                  *
+;   *                                                                         *
+;   ***************************************************************************
+;   ***************************************************************************
+;
+;	Please ensure to read the configuration and relevant port sections of the
+;	online documentation.
+;
+;	http://www.FreeRTOS.org - Documentation, latest information, license and 
+;	contact details.
+;
+;	http://www.SafeRTOS.com - A version that is certified for use in safety 
+;	critical systems.
+;
+;	http://www.OpenRTOS.com - Commercial support, development, porting, 
+;	licensing and training services.
+;*/		
+		
+	INCLUDE portmacro.inc
+	
+	;The UART interrupt entry point is defined within an assembly wrapper
+	;within this file.  This takes care of the task context saving before it
+	;calls the main handler (vUART_ISRHandler()) which is written in C within
+	;serial.c.  The execution of the handler can unblock tasks that were blocked
+	;waiting for UART events.  Once the handler completes the asm wrapper 
+	;finishes off by	restoring the context of whichever task is now selected to 
+	;enter the RUNNING state (which might now be a different task to that which
+	;was originally interrupted.
+	IMPORT vUART_ISRHandler
+	EXPORT vUART_ISREntry
+
+	;/* Interrupt entry must always be in ARM mode. */
+	AREA	|.text|, CODE, READONLY
+	ARM
+	PRESERVE8
+
+
+
+vUART_ISREntry
+
+	; Save the context of the interrupted task.
+	portSAVE_CONTEXT			
+
+	; Call the C handler function - defined within serial.c.
+	LDR R0, =vUART_ISRHandler
+	MOV LR, PC				
+	BX R0
+
+	; Finish off by restoring the context of the task that has been chosen to 
+	; run next - which might be a different task to that which was originally
+	; interrupted.
+	portRESTORE_CONTEXT
+
+	END

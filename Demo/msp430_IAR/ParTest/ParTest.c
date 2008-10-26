4d302265c24f3f60be913b8e402ee0be1ee2ee53
Richard Barry <ribarry@amazon.com>
2008-10-26 14:57:40 +0000

    Add MSP430/IAR demo project.

---
+++ Demo/msp430_IAR/ParTest/ParTest.c
@@ -0,0 +1,227 @@
+/*
+	FreeRTOS.org V5.1.0 - Copyright (C) 2003-2008 Richard Barry.
+
+	This file is part of the FreeRTOS.org distribution.
+
+	FreeRTOS.org is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	FreeRTOS.org is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with FreeRTOS.org; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+	A special exception to the GPL can be applied should you wish to distribute
+	a combined work that includes FreeRTOS.org, without being obliged to provide
+	the source code for any proprietary components.  See the licensing section 
+	of http://www.FreeRTOS.org for full details of how and when the exception
+	can be applied.
+
+    ***************************************************************************
+    ***************************************************************************
+    *                                                                         *
+    * SAVE TIME AND MONEY!  We can port FreeRTOS.org to your own hardware,    *
+    * and even write all or part of your application on your behalf.          *
+    * See http://www.OpenRTOS.com for details of the services we provide to   *
+    * expedite your project.                                                  *
+    *                                                                         *
+    ***************************************************************************
+    ***************************************************************************
+
+	Please ensure to read the configuration and relevant port sections of the
+	online documentation.
+
+	http://www.FreeRTOS.org - Documentation, latest information, license and 
+	contact details.
+
+	http://www.SafeRTOS.com - A version that is certified for use in safety 
+	critical systems.
+
+	http://www.OpenRTOS.com - Commercial support, development, porting, 
+	licensing and training services.
+*/
+
+/*-----------------------------------------------------------
+ * Characters on the LCD are used to simulate LED's.  In this case the 'ParTest'
+ * is really operating on the LCD display.
+ *-----------------------------------------------------------*/
+
+/*
+ * This demo is configured to execute on the ES449 prototyping board from
+ * SoftBaugh. The ES449 has a built in LCD display and a single built in user
+ * LED.  Therefore, in place of flashing an LED, the 'flash' and 'check' tasks
+ * toggle '*' characters on the LCD.  The left most '*' represents LED 0, the
+ * next LED 1, etc.
+ *
+ * There is a single genuine on board LED referenced as LED 10.
+ */
+
+
+/* Scheduler includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+
+/* Demo application includes. */
+#include "partest.h"
+
+/* Constants required to setup the LCD. */
+#define LCD_DIV_64 5
+
+/* Constants required to access the "LED's".  The LED segments are turned on
+and off to generate '*' characters. */
+#define partstNUM_LEDS			( ( unsigned portCHAR ) 6 )
+#define partstSEGMENTS_ON		( ( unsigned portCHAR ) 0x0f )
+#define partstSEGMENTS_OFF		( ( unsigned portCHAR ) 0x00 )
+
+/* The LED number of the real on board LED, rather than a simulated LED. */
+#define partstON_BOARD_LED		( ( unsigned portBASE_TYPE ) 10 )
+#define mainON_BOARD_LED_BIT	( ( unsigned portCHAR ) 0x01 )
+
+/* The LCD segments used to generate the '*' characters for LED's 0 to 5. */
+unsigned portCHAR * const ucRHSSegments[ partstNUM_LEDS ] = {	( unsigned portCHAR * )0xa4, 
+																( unsigned portCHAR * )0xa2, 
+																( unsigned portCHAR * )0xa0, 
+																( unsigned portCHAR * )0x9e,
+																( unsigned portCHAR * )0x9c,
+																( unsigned portCHAR * )0x9a };
+
+unsigned portCHAR * const ucLHSSegments[ partstNUM_LEDS ] = {	( unsigned portCHAR * )0xa3, 
+																( unsigned portCHAR * )0xa1, 
+																( unsigned portCHAR * )0x9f, 
+																( unsigned portCHAR * )0x9d,
+																( unsigned portCHAR * )0x9b,
+																( unsigned portCHAR * )0x99 };
+
+/*
+ * Toggle the single genuine built in LED.
+ */
+static void prvToggleOnBoardLED( void );
+
+/*-----------------------------------------------------------*/
+
+void vParTestInitialise( void )
+{
+	/* Initialise the LCD hardware. */
+
+	/* Used for the onboard LED. */
+	P1DIR = 0x01;
+
+	// Setup Basic Timer for LCD operation
+	BTCTL = (LCD_DIV_64+0x23);
+
+	// Setup port functions
+	P1SEL = 0x32;
+	P2SEL = 0x00;
+	P3SEL = 0x00;
+	P4SEL = 0xFC;
+	P5SEL = 0xFF;
+	
+	/* Initialise all segments to off. */
+	LCDM1 = partstSEGMENTS_OFF;	
+	LCDM2 = partstSEGMENTS_OFF;	
+	LCDM3 = partstSEGMENTS_OFF;	
+	LCDM4 = partstSEGMENTS_OFF;	
+	LCDM5 = partstSEGMENTS_OFF;	
+	LCDM6 = partstSEGMENTS_OFF;	
+	LCDM7 = partstSEGMENTS_OFF;	
+	LCDM8 = partstSEGMENTS_OFF;	
+	LCDM9 = partstSEGMENTS_OFF;	
+	LCDM10 = partstSEGMENTS_OFF;	
+	LCDM11 = partstSEGMENTS_OFF;	
+	LCDM12 = partstSEGMENTS_OFF;	
+	LCDM13 = partstSEGMENTS_OFF;	
+	LCDM14 = partstSEGMENTS_OFF;	
+	LCDM15 = partstSEGMENTS_OFF;	
+	LCDM16 = partstSEGMENTS_OFF;	
+	LCDM17 = partstSEGMENTS_OFF;	
+	LCDM18 = partstSEGMENTS_OFF;	
+	LCDM19 = partstSEGMENTS_OFF;	
+	LCDM20 = partstSEGMENTS_OFF;	
+
+	/* Setup LCD control. */
+	LCDCTL = (LCDSG0_7|LCD4MUX|LCDON);
+}
+/*-----------------------------------------------------------*/
+
+void vParTestSetLED( unsigned portBASE_TYPE uxLED, signed portBASE_TYPE xValue )
+{
+	/* Set or clear the output [in this case show or hide the '*' character. */
+	if( uxLED < ( portBASE_TYPE ) partstNUM_LEDS )
+	{
+		vTaskSuspendAll();
+		{
+			if( xValue )
+			{
+				/* Turn on the segments required to show the '*'. */
+				*( ucRHSSegments[ uxLED ] ) = partstSEGMENTS_ON;
+				*( ucLHSSegments[ uxLED ] ) = partstSEGMENTS_ON;
+			}
+			else
+			{
+				/* Turn off all the segments. */
+				*( ucRHSSegments[ uxLED ] ) = partstSEGMENTS_OFF;
+				*( ucLHSSegments[ uxLED ] ) = partstSEGMENTS_OFF;
+			}
+		}
+		xTaskResumeAll();
+	}
+}
+/*-----------------------------------------------------------*/
+
+void vParTestToggleLED( unsigned portBASE_TYPE uxLED )
+{
+	if( uxLED < ( portBASE_TYPE ) partstNUM_LEDS )
+	{
+		vTaskSuspendAll();
+		{
+			/* If the '*' is already showing - hide it.  If it is not already
+			showing then show it. */
+			if( *( ucRHSSegments[ uxLED ] ) )
+			{
+				*( ucRHSSegments[ uxLED ] ) = partstSEGMENTS_OFF;
+				*( ucLHSSegments[ uxLED ] ) = partstSEGMENTS_OFF;
+			}
+			else
+			{
+				*( ucRHSSegments[ uxLED ] ) = partstSEGMENTS_ON;
+				*( ucLHSSegments[ uxLED ] ) = partstSEGMENTS_ON;
+			}
+		}
+		xTaskResumeAll();
+	}
+	else
+	{
+		if( uxLED == partstON_BOARD_LED )
+		{
+			/* The request related to the genuine on board LED. */
+			prvToggleOnBoardLED();
+		}
+	}	
+}
+/*-----------------------------------------------------------*/
+
+static void prvToggleOnBoardLED( void )
+{
+static unsigned portSHORT sState = pdFALSE;
+
+	/* Toggle the state of the single genuine on board LED. */
+	if( sState )	
+	{
+		P1OUT |= mainON_BOARD_LED_BIT;
+	}
+	else
+	{
+		P1OUT &= ~mainON_BOARD_LED_BIT;
+	}
+
+	sState = !sState;
+}
+/*-----------------------------------------------------------*/
+
+
---
+++ Demo/msp430_IAR/serial/serial.c
@@ -0,0 +1,306 @@
+/*
+	FreeRTOS.org V5.1.0 - Copyright (C) 2003-2008 Richard Barry.
+
+	This file is part of the FreeRTOS.org distribution.
+
+	FreeRTOS.org is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	FreeRTOS.org is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with FreeRTOS.org; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+	A special exception to the GPL can be applied should you wish to distribute
+	a combined work that includes FreeRTOS.org, without being obliged to provide
+	the source code for any proprietary components.  See the licensing section
+	of http://www.FreeRTOS.org for full details of how and when the exception
+	can be applied.
+
+    ***************************************************************************
+    ***************************************************************************
+    *                                                                         *
+    * SAVE TIME AND MONEY!  We can port FreeRTOS.org to your own hardware,    *
+    * and even write all or part of your application on your behalf.          *
+    * See http://www.OpenRTOS.com for details of the services we provide to   *
+    * expedite your project.                                                  *
+    *                                                                         *
+    ***************************************************************************
+    ***************************************************************************
+
+	Please ensure to read the configuration and relevant port sections of the
+	online documentation.
+
+	http://www.FreeRTOS.org - Documentation, latest information, license and
+	contact details.
+
+	http://www.SafeRTOS.com - A version that is certified for use in safety
+	critical systems.
+
+	http://www.OpenRTOS.com - Commercial support, development, porting,
+	licensing and training services.
+*/
+
+
+/* BASIC INTERRUPT DRIVEN SERIAL PORT DRIVER.
+ *
+ * This file only supports UART 1
+ */
+
+/* Standard includes. */
+#include <stdlib.h>
+
+/* Scheduler includes. */
+#include "FreeRTOS.h"
+#include "queue.h"
+#include "task.h"
+
+/* Demo application includes. */
+#include "serial.h"
+
+/* Constants required to setup the hardware. */
+#define serTX_AND_RX			( ( unsigned portCHAR ) 0x03 )
+
+/* Misc. constants. */
+#define serNO_BLOCK				( ( portTickType ) 0 )
+
+/* Enable the UART Tx interrupt. */
+#define vInterruptOn() IFG2 |= UTXIFG1
+
+/* The queue used to hold received characters. */
+static xQueueHandle xRxedChars;
+
+/* The queue used to hold characters waiting transmission. */
+static xQueueHandle xCharsForTx;
+
+static volatile portSHORT sTHREEmpty;
+
+/*-----------------------------------------------------------*/
+
+xComPortHandle xSerialPortInitMinimal( unsigned portLONG ulWantedBaud, unsigned portBASE_TYPE uxQueueLength )
+{
+unsigned portLONG ulBaudRateCount;
+
+	/* Initialise the hardware. */
+
+	/* Generate the baud rate constants for the wanted baud rate. */
+	ulBaudRateCount = configCPU_CLOCK_HZ / ulWantedBaud;
+
+	portENTER_CRITICAL();
+	{
+		/* Create the queues used by the com test task. */
+		xRxedChars = xQueueCreate( uxQueueLength, ( unsigned portBASE_TYPE ) sizeof( signed portCHAR ) );
+		xCharsForTx = xQueueCreate( uxQueueLength, ( unsigned portBASE_TYPE ) sizeof( signed portCHAR ) );
+
+		/* Reset UART. */
+		UCTL1 |= SWRST;
+
+		/* Set pin function. */
+		P4SEL |= serTX_AND_RX;
+
+		/* All other bits remain at zero for n, 8, 1 interrupt driven operation.
+		LOOPBACK MODE!*/
+		U1CTL |= CHAR + LISTEN;
+		U1TCTL |= SSEL1;
+
+		/* Setup baud rate low byte. */
+		U1BR0 = ( unsigned portCHAR ) ( ulBaudRateCount & ( unsigned portLONG ) 0xff );
+
+		/* Setup baud rate high byte. */
+		ulBaudRateCount >>= 8UL;
+		U1BR1 = ( unsigned portCHAR ) ( ulBaudRateCount & ( unsigned portLONG ) 0xff );
+
+		/* Enable ports. */
+		ME2 |= UTXE1 + URXE1;
+
+		/* Set. */
+		UCTL1 &= ~SWRST;
+
+		/* Nothing in the buffer yet. */
+		sTHREEmpty = pdTRUE;
+
+		/* Enable interrupts. */
+		IE2 |= URXIE1 + UTXIE1;
+	}
+	portEXIT_CRITICAL();
+	
+	/* Unlike other ports, this serial code does not allow for more than one
+	com port.  We therefore don't return a pointer to a port structure and can
+	instead just return NULL. */
+	return NULL;
+}
+/*-----------------------------------------------------------*/
+
+signed portBASE_TYPE xSerialGetChar( xComPortHandle pxPort, signed portCHAR *pcRxedChar, portTickType xBlockTime )
+{
+	/* Get the next character from the buffer.  Return false if no characters
+	are available, or arrive before xBlockTime expires. */
+	if( xQueueReceive( xRxedChars, pcRxedChar, xBlockTime ) )
+	{
+		return pdTRUE;
+	}
+	else
+	{
+		return pdFALSE;
+	}
+}
+/*-----------------------------------------------------------*/
+
+signed portBASE_TYPE xSerialPutChar( xComPortHandle pxPort, signed portCHAR cOutChar, portTickType xBlockTime )
+{
+signed portBASE_TYPE xReturn;
+
+	/* Transmit a character. */
+
+	portENTER_CRITICAL();
+	{
+		if( sTHREEmpty == pdTRUE )
+		{
+			/* If sTHREEmpty is true then the UART Tx ISR has indicated that
+			there are no characters queued to be transmitted - so we can
+			write the character directly to the shift Tx register. */
+			sTHREEmpty = pdFALSE;
+			U1TXBUF = cOutChar;
+			xReturn = pdPASS;
+		}
+		else
+		{
+			/* sTHREEmpty is false, so there are still characters waiting to be
+			transmitted.  We have to queue this character so it gets
+			transmitted	in turn. */
+
+			/* Return false if after the block time there is no room on the Tx
+			queue.  It is ok to block inside a critical section as each task
+			maintains it's own critical section status. */
+			xReturn = xQueueSend( xCharsForTx, &cOutChar, xBlockTime );
+
+			/* Depending on queue sizing and task prioritisation:  While we
+			were blocked waiting to post on the queue interrupts were not
+			disabled.  It is possible that the serial ISR has emptied the
+			Tx queue, in which case we need to start the Tx off again
+			writing directly to the Tx register. */
+			if( ( sTHREEmpty == pdTRUE ) && ( xReturn == pdPASS ) )
+			{
+				/* Get back the character we just posted. */
+				xQueueReceive( xCharsForTx, &cOutChar, serNO_BLOCK );
+				sTHREEmpty = pdFALSE;
+				U1TXBUF = cOutChar;
+			}
+		}
+	}
+	portEXIT_CRITICAL();
+
+	return pdPASS;
+}
+/*-----------------------------------------------------------*/
+
+#if configINTERRUPT_EXAMPLE_METHOD == 1
+
+	/*
+	 * UART RX interrupt service routine.
+	 */
+	#pragma vector=UART1RX_VECTOR
+	__interrupt void vRxISR( void )
+	{
+	signed portCHAR cChar;
+	portBASE_TYPE xHigherPriorityTaskWoken = pdFALSE;
+	
+		/* Get the character from the UART and post it on the queue of Rxed
+		characters. */
+		cChar = U1RXBUF;
+	
+		xQueueSendFromISR( xRxedChars, &cChar, &xHigherPriorityTaskWoken );
+
+		if( xHigherPriorityTaskWoken )
+		{
+			/*If the post causes a task to wake force a context switch
+			as the woken task may have a higher priority than the task we have
+			interrupted. */
+			taskYIELD();
+		}
+
+        /* Make sure any low power mode bits are clear before leaving the ISR. */
+        __bic_SR_register_on_exit( SCG1 + SCG0 + OSCOFF + CPUOFF );
+	}
+	/*-----------------------------------------------------------*/
+	
+	/*
+	 * UART Tx interrupt service routine.
+	 */
+	#pragma vector=UART1TX_VECTOR
+	__interrupt void vTxISR( void )
+	{
+	signed portCHAR cChar;
+	portBASE_TYPE xTaskWoken = pdFALSE;
+	
+		/* The previous character has been transmitted.  See if there are any
+		further characters waiting transmission. */
+	
+		if( xQueueReceiveFromISR( xCharsForTx, &cChar, &xTaskWoken ) == pdTRUE )
+		{
+			/* There was another character queued - transmit it now. */
+			U1TXBUF = cChar;
+		}
+		else
+		{
+			/* There were no other characters to transmit. */
+			sTHREEmpty = pdTRUE;
+		}
+
+        /* Make sure any low power mode bits are clear before leaving the ISR. */
+        __bic_SR_register_on_exit( SCG1 + SCG0 + OSCOFF + CPUOFF );
+	}
+    /*-----------------------------------------------------------*/
+
+#elif configINTERRUPT_EXAMPLE_METHOD == 2
+
+    /* This is a standard C function as an assembly file wrapper is used as an
+    interrupt entry point. */
+	void vRxISR( void )
+	{
+	signed portCHAR cChar;
+	portBASE_TYPE xHigherPriorityTaskWoken = pdFALSE;
+	
+		/* Get the character from the UART and post it on the queue of Rxed
+		characters. */
+		cChar = U1RXBUF;
+	
+		xQueueSendFromISR( xRxedChars, &cChar, &xHigherPriorityTaskWoken );
+
+        /*If the post causes a task to wake force a context switch
+        as the woken task may have a higher priority than the task we have
+        interrupted. */
+        portYIELD_FROM_ISR( xHigherPriorityTaskWoken );
+	}
+	/*-----------------------------------------------------------*/
+	
+    /* This is a standard C function as an assembly file wrapper is used as an
+    interrupt entry point. */
+	void vTxISR( void )
+	{
+	signed portCHAR cChar;
+	portBASE_TYPE xTaskWoken = pdFALSE;
+	
+		/* The previous character has been transmitted.  See if there are any
+		further characters waiting transmission. */
+	
+		if( xQueueReceiveFromISR( xCharsForTx, &cChar, &xTaskWoken ) == pdTRUE )
+		{
+			/* There was another character queued - transmit it now. */
+			U1TXBUF = cChar;
+		}
+		else
+		{
+			/* There were no other characters to transmit. */
+			sTHREEmpty = pdTRUE;
+		}
+	}
+
+#endif /* configINTERRUPT_EXAMPLE_METHOD */
+/*-----------------------------------------------------------*/

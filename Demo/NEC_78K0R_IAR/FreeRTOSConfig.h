8c1495a57d9f1e94539cad4645ae8f829c27146c
Richard Barry <ribarry@amazon.com>
2009-02-06 13:21:35 +0000

    Continue 78K0R development.

--- Demo/NEC_78K0R_IAR/FreeRTOSConfig.h
+++ Demo/NEC_78K0R_IAR/FreeRTOSConfig.h
@@ -50,18 +50,6 @@
 #ifndef FREERTOS_CONFIG_H
 #define FREERTOS_CONFIG_H
 
-/* Only include in C files */
-#ifdef __IAR_SYSTEMS_ICC__
-
-	#pragma language=extended
-	#pragma system_include
-
-	#include <io78f1166_a0.h>
-	#include <io78f1166_a0_ext.h>
-	#include <intrinsics.h>
-
-#endif  /* __IAR_SYSTEMS_ICC__ */
-
 /*
  * 78K0R Clock Source Configuration
  * 1 = use internal High Speed Clock Source (typically 8Mhz on the 78K0R/Kx3)
@@ -90,43 +78,68 @@
 
 #define configUSE_PREEMPTION		1
 
-/* Only use following section for C files */
-#ifdef __IAR_SYSTEMS_ICC__
-
-	#define configUSE_IDLE_HOOK				0
-	#define configUSE_TICK_HOOK				0
-	#define configTICK_RATE_HZ				( ( portTickType ) 1000 )
-	#define configMAX_PRIORITIES			( ( unsigned portBASE_TYPE ) 4 )
-	#define configMINIMAL_STACK_SIZE		( ( unsigned portSHORT ) 100 )
-	#define configTOTAL_HEAP_SIZE			( (size_t ) ( 7000 ) )
-	#define configMAX_TASK_NAME_LEN			( 10 )
-	#define configUSE_TRACE_FACILITY		0
-	#define configUSE_16_BIT_TICKS			1
-	#define configIDLE_SHOULD_YIELD			1
-	#define configCHECK_FOR_STACK_OVERFLOW	2
-	#define configUSE_MUTEXES				1
-
-	/* Co-routine definitions. */
-	#define configUSE_CO_ROUTINES 			0
-	#define configMAX_CO_ROUTINE_PRIORITIES ( 2 )
-
-	/* Set the following definitions to 1 to include the API function, or zero
-	to exclude the API function. */
-	#define INCLUDE_vTaskPrioritySet		1
-	#define INCLUDE_uxTaskPriorityGet		1
-	#define INCLUDE_vTaskDelete				1
-	#define INCLUDE_vTaskCleanUpResources	0
-	#define INCLUDE_vTaskSuspend			1
-	#define INCLUDE_vTaskDelayUntil			1
-	#define INCLUDE_vTaskDelay				1
-
-	#ifdef configCLOCK_SOURCE == 0
-		#define configCPU_CLOCK_HZ			( ( unsigned portLONG ) 20000000 )  /* using the external clock source */
-	#else
-		#define configCPU_CLOCK_HZ			( ( unsigned portLONG ) 8000000 )   /* using the internal high speed clock */
-	#endif /* configCLOCK_SOURCE */
-
-#endif /* __IAR_SYSTEMS_ICC__ */
+	/* Only use following section for C files */
+	#ifdef __IAR_SYSTEMS_ICC__
+	
+		#pragma language=extended
+		#pragma system_include
+	
+		#include <intrinsics.h>
+	
+		#define configUSE_IDLE_HOOK				0
+		#define configUSE_TICK_HOOK				0
+		#define configTICK_RATE_HZ				( ( portTickType ) 1000 )
+		#define configMAX_PRIORITIES			( ( unsigned portBASE_TYPE ) 4 )
+		#define configMINIMAL_STACK_SIZE		( ( unsigned portSHORT ) 100 )
+		#define configMAX_TASK_NAME_LEN			( 10 )
+		#define configUSE_TRACE_FACILITY		0
+		#define configUSE_16_BIT_TICKS			1
+		#define configIDLE_SHOULD_YIELD			1
+		#define configCHECK_FOR_STACK_OVERFLOW	2
+		#define configUSE_MUTEXES				1
+	
+		/* Co-routine definitions. */
+		#define configUSE_CO_ROUTINES 			0
+		#define configMAX_CO_ROUTINE_PRIORITIES ( 2 )
+	
+		/* Set the following definitions to 1 to include the API function, or zero
+		to exclude the API function. */
+		#define INCLUDE_vTaskPrioritySet		1
+		#define INCLUDE_uxTaskPriorityGet		1
+		#define INCLUDE_vTaskDelete				1
+		#define INCLUDE_vTaskCleanUpResources	0
+		#define INCLUDE_vTaskSuspend			1
+		#define INCLUDE_vTaskDelayUntil			1
+		#define INCLUDE_vTaskDelay				1
+	
+		#ifdef configCLOCK_SOURCE == 0
+			#define configCPU_CLOCK_HZ			( ( unsigned portLONG ) 20000000 )  /* using the external clock source */
+		#else
+			#define configCPU_CLOCK_HZ			( ( unsigned portLONG ) 8000000 )   /* using the internal high speed clock */
+		#endif /* configCLOCK_SOURCE */
+	
+		/* Definitions that are specific to the project being used. */
+		#ifdef __IAR_78K0R_Kx3__
+		
+			/* Device specific includes. */
+			#include <io78f1166_a0.h>
+			#include <io78f1166_a0_ext.h>
+		
+			#define configTOTAL_HEAP_SIZE			( (size_t ) ( 7000 ) )
+		
+		#endif /* __IAR_78K0R_Kx3__ */
+		
+		#ifdef __IAR_78K0R_Kx3L__
+		
+			/* Device specific includes. */
+			#include <io78f1009_64.h>
+			#include <io78f1009_64_ext.h>
+		
+			#define configTOTAL_HEAP_SIZE			( (size_t ) ( 2400 ) )
+		
+		#endif /* _IAR_78K0R_Kx3L__ */
+	
+	#endif /* __IAR_SYSTEMS_ICC__ */
 
 #endif /* FREERTOS_CONFIG_H */
 

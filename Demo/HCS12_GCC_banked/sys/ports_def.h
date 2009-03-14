2f40ad739372216ae7c5542af01a5d51e8cb5439
Richard Barry <ribarry@amazon.com>
2009-03-14 19:20:12 +0000

    Ready for V5.2.0 release.

--- Demo/HCS12_GCC_banked/sys/ports_def.h
+++ Demo/HCS12_GCC_banked/sys/ports_def.h
@@ -5,7 +5,7 @@
 This file is part of GEL.
 
 GEL is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
+it under the terms of the GNU General Public License** as published by
 the Free Software Foundation; either version 2, or (at your option)
 any later version.
 
--- Source/portable/GCC/MCF5235/port.c
+++ Source/portable/GCC/MCF5235/port.c
@@ -5,7 +5,7 @@
     This file is part of the FreeRTOS distribution.
 
     FreeRTOS is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
+    it under the terms of the GNU General Public License** as published by
     the Free Software Foundation; either version 2 of the License, or
     (at your option) any later version.
 

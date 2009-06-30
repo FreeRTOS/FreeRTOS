3634ebb497e05245832d53e14e328f1ea07a62b3
Richard Barry <ribarry@amazon.com>
2009-06-30 19:42:21 +0000

    Added PPC440 demo that does not use any floating point hardware.

---
+++ Demo/PPC440_Xilinx_Virtex5_GCC/RTOSDemo/FreeRTOSConfig.h
@@ -0,0 +1,115 @@
+/*
+	FreeRTOS.org V5.3.1 - Copyright (C) 2003-2009 Richard Barry.
+
+	This file is part of the FreeRTOS.org distribution.
+
+	FreeRTOS.org is free software; you can redistribute it and/or modify it
+	under the terms of the GNU General Public License (version 2) as published
+	by the Free Software Foundation and modified by the FreeRTOS exception.
+	**NOTE** The exception to the GPL is included to allow you to distribute a
+	combined work that includes FreeRTOS.org without being obliged to provide
+	the source code for any proprietary components.  Alternative commercial
+	license and support terms are also available upon request.  See the 
+	licensing section of http://www.FreeRTOS.org for full details.
+
+	FreeRTOS.org is distributed in the hope that it will be useful,	but WITHOUT
+	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+	FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+	more details.
+
+	You should have received a copy of the GNU General Public License along
+	with FreeRTOS.org; if not, write to the Free Software Foundation, Inc., 59
+	Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+
+
+	***************************************************************************
+	*                                                                         *
+	* Get the FreeRTOS eBook!  See http://www.FreeRTOS.org/Documentation      *
+	*                                                                         *
+	* This is a concise, step by step, 'hands on' guide that describes both   *
+	* general multitasking concepts and FreeRTOS specifics. It presents and   *
+	* explains numerous examples that are written using the FreeRTOS API.     *
+	* Full source code for all the examples is provided in an accompanying    *
+	* .zip file.                                                              *
+	*                                                                         *
+	***************************************************************************
+
+	1 tab == 4 spaces!
+
+	Please ensure to read the configuration and relevant port sections of the
+	online documentation.
+
+	http://www.FreeRTOS.org - Documentation, latest information, license and
+	contact details.
+
+	http://www.SafeRTOS.com - A version that is certified for use in safety
+	critical systems.
+
+	http://www.OpenRTOS.com - Commercial support, development, porting,
+	licensing and training services.
+*/
+
+#ifndef FREERTOS_CONFIG_H
+#define FREERTOS_CONFIG_H
+
+/*-----------------------------------------------------------
+ * Application specific definitions.
+ *
+ * These definitions should be adjusted for your particular hardware and
+ * application requirements.
+ *
+ * THESE PARAMETERS ARE DESCRIBED WITHIN THE 'CONFIGURATION' SECTION OF THE
+ * FreeRTOS API DOCUMENTATION AVAILABLE ON THE FreeRTOS.org WEB SITE. 
+ *
+ * See http://www.freertos.org/a00110.html.
+ *----------------------------------------------------------*/
+
+#define configUSE_PREEMPTION			1
+#define configUSE_IDLE_HOOK				0
+#define configUSE_TICK_HOOK				0
+#define configMINIMAL_STACK_SIZE		( ( unsigned portSHORT ) 250 )
+#define configCPU_CLOCK_HZ				( ( unsigned portLONG ) 200000000 )	/* Clock setup from start.asm in the demo application. */
+#define configTICK_RATE_HZ				( (portTickType) 1000 )
+#define configMAX_PRIORITIES			( ( unsigned portBASE_TYPE ) 6 )
+#define configTOTAL_HEAP_SIZE			( (size_t) (80 * 1024) )
+#define configMAX_TASK_NAME_LEN			( 20 )
+#define configUSE_16_BIT_TICKS			1
+#define configIDLE_SHOULD_YIELD			1
+#define configUSE_MUTEXES				1
+#define configUSE_TRACE_FACILITY		0
+#define configCHECK_FOR_STACK_OVERFLOW	2
+#define configUSE_COUNTING_SEMAPHORES	1
+#define configUSE_APPLICATION_TASK_TAG	1
+#define configUSE_FPU					0
+
+
+/* Co-routine definitions. */
+#define configUSE_CO_ROUTINES			0
+#define configMAX_CO_ROUTINE_PRIORITIES ( 4 )
+
+/* Set the following definitions to 1 to include the API function, or zero
+to exclude the API function. */
+#define INCLUDE_vTaskPrioritySet			1
+#define INCLUDE_uxTaskPriorityGet			1
+#define INCLUDE_vTaskDelete					1
+#define INCLUDE_vTaskCleanUpResources		1
+#define INCLUDE_vTaskSuspend				1
+#define INCLUDE_vResumeFromISR				1
+#define INCLUDE_vTaskDelayUntil				1
+#define INCLUDE_vTaskDelay					1
+#define INCLUDE_xTaskGetSchedulerState		1
+#define INCLUDE_xTaskGetCurrentTaskHandle	1
+#define INCLUDE_uxTaskGetStackHighWaterMark	1
+#define configUSE_RECURSIVE_MUTEXES			1
+
+
+#if configUSE_FPU == 1
+	/* Include the header that define the traceTASK_SWITCHED_IN() and
+	traceTASK_SWITCHED_OUT() macros to save and restore the floating
+	point registers for tasks that have requested this behaviour. */
+	#include "FPU_Macros.h"
+#endif
+
+#endif /* FREERTOS_CONFIG_H */
+
+
---
+++ Demo/PPC440_Xilinx_Virtex5_GCC/RTOSDemo/partest/partest.c
@@ -0,0 +1,170 @@
+/*
+	FreeRTOS.org V5.3.1 - Copyright (C) 2003-2009 Richard Barry.
+
+	This file is part of the FreeRTOS.org distribution.
+
+	FreeRTOS.org is free software; you can redistribute it and/or modify it
+	under the terms of the GNU General Public License (version 2) as published
+	by the Free Software Foundation and modified by the FreeRTOS exception.
+	**NOTE** The exception to the GPL is included to allow you to distribute a
+	combined work that includes FreeRTOS.org without being obliged to provide
+	the source code for any proprietary components.  Alternative commercial
+	license and support terms are also available upon request.  See the 
+	licensing section of http://www.FreeRTOS.org for full details.
+
+	FreeRTOS.org is distributed in the hope that it will be useful,	but WITHOUT
+	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+	FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+	more details.
+
+	You should have received a copy of the GNU General Public License along
+	with FreeRTOS.org; if not, write to the Free Software Foundation, Inc., 59
+	Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+
+
+	***************************************************************************
+	*                                                                         *
+	* Get the FreeRTOS eBook!  See http://www.FreeRTOS.org/Documentation      *
+	*                                                                         *
+	* This is a concise, step by step, 'hands on' guide that describes both   *
+	* general multitasking concepts and FreeRTOS specifics. It presents and   *
+	* explains numerous examples that are written using the FreeRTOS API.     *
+	* Full source code for all the examples is provided in an accompanying    *
+	* .zip file.                                                              *
+	*                                                                         *
+	***************************************************************************
+
+	1 tab == 4 spaces!
+
+	Please ensure to read the configuration and relevant port sections of the
+	online documentation.
+
+	http://www.FreeRTOS.org - Documentation, latest information, license and
+	contact details.
+
+	http://www.SafeRTOS.com - A version that is certified for use in safety
+	critical systems.
+
+	http://www.OpenRTOS.com - Commercial support, development, porting,
+	licensing and training services.
+*/
+
+
+/* Scheduler includes. */
+#include "FreeRTOS.h"
+
+/* Demo application includes. */
+#include "partest.h"
+
+/* Library includes. */
+#include "xparameters.h"
+#include "xgpio_l.h"
+
+/* Misc hardware specific definitions. */
+#define partstALL_AS_OUTPUT	0x00
+#define partstCHANNEL_1		0x01
+#define partstMAX_8BIT_LED	0x07
+
+/* The outputs are split into two IO sections, these variables maintain the 
+current value of either section. */
+static unsigned portBASE_TYPE uxCurrentOutput8Bit, uxCurrentOutput5Bit;
+
+/*-----------------------------------------------------------*/
+/*
+ * Setup the IO for the LED outputs.
+ */
+void vParTestInitialise( void )
+{
+	/* Set both sets of LED's on the demo board to outputs. */
+	XGpio_mSetDataDirection( XPAR_LEDS_8BIT_BASEADDR, partstCHANNEL_1, partstALL_AS_OUTPUT );
+	XGpio_mSetDataDirection( XPAR_LEDS_POSITIONS_BASEADDR, partstCHANNEL_1, partstALL_AS_OUTPUT );
+
+	/* Start with all outputs off. */
+	uxCurrentOutput8Bit = 0;
+	XGpio_mSetDataReg( XPAR_LEDS_8BIT_BASEADDR, partstCHANNEL_1, 0x00 );
+	uxCurrentOutput5Bit = 0;
+	XGpio_mSetDataReg( XPAR_LEDS_POSITIONS_BASEADDR, partstCHANNEL_1, 0x00 );
+}
+/*-----------------------------------------------------------*/
+
+void vParTestSetLED( unsigned portBASE_TYPE uxLED, signed portBASE_TYPE xValue )
+{
+unsigned portBASE_TYPE uxBaseAddress, *puxCurrentValue;
+
+	portENTER_CRITICAL();
+	{
+		/* Which IO section does the LED being set/cleared belong to?  The
+		8 bit or 5 bit outputs? */
+		if( uxLED <= partstMAX_8BIT_LED )
+		{
+			uxBaseAddress = XPAR_LEDS_8BIT_BASEADDR;
+			puxCurrentValue = &uxCurrentOutput5Bit;
+		}	
+		else
+		{
+			uxBaseAddress = XPAR_LEDS_POSITIONS_BASEADDR;
+			puxCurrentValue = &uxCurrentOutput8Bit;
+			uxLED -= partstMAX_8BIT_LED;
+		}
+
+		/* Setup the bit mask accordingly. */
+		uxLED = 0x01 << uxLED;
+
+		/* Maintain the current output value. */
+		if( xValue )
+		{
+			*puxCurrentValue |= uxLED;
+		}
+		else
+		{
+			*puxCurrentValue &= ~uxLED;
+		}
+
+		/* Write the value to the port. */
+		XGpio_mSetDataReg( uxBaseAddress, partstCHANNEL_1, *puxCurrentValue );
+	}
+	portEXIT_CRITICAL();
+}
+/*-----------------------------------------------------------*/
+
+void vParTestToggleLED( unsigned portBASE_TYPE uxLED )
+{
+unsigned portBASE_TYPE uxBaseAddress, *puxCurrentValue;
+
+	portENTER_CRITICAL();
+	{
+		/* Which IO section does the LED being toggled belong to?  The
+		8 bit or 5 bit outputs? */
+		if( uxLED <= partstMAX_8BIT_LED )
+		{
+
+			uxBaseAddress = XPAR_LEDS_8BIT_BASEADDR;
+			puxCurrentValue = &uxCurrentOutput5Bit;
+		}	
+		else
+		{
+			uxBaseAddress = XPAR_LEDS_POSITIONS_BASEADDR;
+			puxCurrentValue = &uxCurrentOutput8Bit;
+			uxLED -= partstMAX_8BIT_LED;
+		}
+
+		/* Setup the bit mask accordingly. */
+		uxLED = 0x01 << uxLED;
+
+		/* Maintain the current output value. */
+		if( *puxCurrentValue & uxLED )
+		{
+			*puxCurrentValue &= ~uxLED;
+		}
+		else
+		{
+			*puxCurrentValue |= uxLED;
+		}
+
+		/* Write the value to the port. */
+		XGpio_mSetDataReg(uxBaseAddress, partstCHANNEL_1, *puxCurrentValue );
+	}
+	portEXIT_CRITICAL();
+}
+
+

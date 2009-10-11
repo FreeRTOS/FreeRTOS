d80bf5e055ea13016ee528333c3b806a927d2721
Richard Barry <ribarry@amazon.com>
2009-10-11 22:25:59 +0000

    Remove unused definitions.

--- Demo/CORTEX_MPU_LM3Sxxxx_Rowley/FreeRTOSConfig.h
+++ Demo/CORTEX_MPU_LM3Sxxxx_Rowley/FreeRTOSConfig.h
@@ -69,14 +69,14 @@
 #define configMINIMAL_STACK_SIZE		( ( unsigned short ) 100 )
 #define configTOTAL_HEAP_SIZE			( ( size_t ) ( 2560 ) )
 #define configMAX_TASK_NAME_LEN			( 12 )
-#define configUSE_TRACE_FACILITY		1
+#define configUSE_TRACE_FACILITY		0
 #define configUSE_16_BIT_TICKS			0
 #define configIDLE_SHOULD_YIELD			0
 #define configUSE_CO_ROUTINES 			0
-#define configUSE_MUTEXES				1
+#define configUSE_MUTEXES				0
 #define configCHECK_FOR_STACK_OVERFLOW	0
-#define configUSE_RECURSIVE_MUTEXES		1
-#define configQUEUE_REGISTRY_SIZE		10
+#define configUSE_RECURSIVE_MUTEXES		0
+#define configQUEUE_REGISTRY_SIZE		0
 #define configGENERATE_RUN_TIME_STATS	0
 #define configUSE_MALLOC_FAILED_HOOK	1
 
@@ -86,14 +86,14 @@
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
 
-#define INCLUDE_vTaskPrioritySet			1
-#define INCLUDE_uxTaskPriorityGet			1
+#define INCLUDE_vTaskPrioritySet			0
+#define INCLUDE_uxTaskPriorityGet			0
 #define INCLUDE_vTaskDelete					1
 #define INCLUDE_vTaskCleanUpResources		0
-#define INCLUDE_vTaskSuspend				1
-#define INCLUDE_vTaskDelayUntil				1
-#define INCLUDE_vTaskDelay					1
-#define INCLUDE_uxTaskGetStackHighWaterMark	1
+#define INCLUDE_vTaskSuspend				0
+#define INCLUDE_vTaskDelayUntil				0
+#define INCLUDE_vTaskDelay					0
+#define INCLUDE_uxTaskGetStackHighWaterMark	0
 
 #ifdef __NVIC_PRIO_BITS
 	#define configPRIO_BITS       __NVIC_PRIO_BITS

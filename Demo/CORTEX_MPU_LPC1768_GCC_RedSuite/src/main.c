26bf0da04e2b1ceb16aa8e675590ce3ed3b3550f
Richard Barry <ribarry@amazon.com>
2009-10-13 11:05:41 +0000

    Change to the file headers only.

--- Demo/CORTEX_MPU_LPC1768_GCC_RedSuite/src/main.c
+++ Demo/CORTEX_MPU_LPC1768_GCC_RedSuite/src/main.c
@@ -1,40 +1,45 @@
 /*
     FreeRTOS V6.0.0 - Copyright (C) 2009 Real Time Engineers Ltd.
 
+    ***************************************************************************
+    *                                                                         *
+    * If you are:                                                             *
+    *                                                                         *
+    *    + New to FreeRTOS,                                                   *
+    *    + Wanting to learn FreeRTOS or multitasking in general quickly       *
+    *    + Looking for basic training,                                        *
+    *    + Wanting to improve your FreeRTOS skills and productivity           *
+    *                                                                         *
+    * then take a look at the FreeRTOS eBook                                  *
+    *                                                                         *
+    *        "Using the FreeRTOS Real Time Kernel - a Practical Guide"        *
+    *                  http://www.FreeRTOS.org/Documentation                  *
+    *                                                                         *
+    * A pdf reference manual is also available.  Both are usually delivered   *
+    * to your inbox within 20 minutes to two hours when purchased between 8am *
+    * and 8pm GMT (although please allow up to 24 hours in case of            *
+    * exceptional circumstances).  Thank you for your support!                *
+    *                                                                         *
+    ***************************************************************************
+
     This file is part of the FreeRTOS distribution.
 
-    FreeRTOS is free software; you can redistribute it and/or modify it    under
+    FreeRTOS is free software; you can redistribute it and/or modify it under
     the terms of the GNU General Public License (version 2) as published by the
-    Free Software Foundation and modified by the FreeRTOS exception.
-    **NOTE** The exception to the GPL is included to allow you to distribute a
-    combined work that includes FreeRTOS without being obliged to provide the
+    Free Software Foundation AND MODIFIED BY the FreeRTOS exception.
+    ***NOTE*** The exception to the GPL is included to allow you to distribute
+    a combined work that includes FreeRTOS without being obliged to provide the
     source code for proprietary components outside of the FreeRTOS kernel.
-    Alternative commercial license and support terms are also available upon
-    request.  See the licensing section of http://www.FreeRTOS.org for full
-    license details.
-
-    FreeRTOS is distributed in the hope that it will be useful,    but WITHOUT
+    FreeRTOS is distributed in the hope that it will be useful, but WITHOUT
     ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
     FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-    more details.
-
-    You should have received a copy of the GNU General Public License along
-    with FreeRTOS; if not, write to the Free Software Foundation, Inc., 59
-    Temple Place, Suite 330, Boston, MA  02111-1307  USA.
-
-
-    ***************************************************************************
-    *                                                                         *
-    * The FreeRTOS eBook and reference manual are available to purchase for a *
-    * small fee. Help yourself get started quickly while also helping the     *
-    * FreeRTOS project! See http://www.FreeRTOS.org/Documentation for details *
-    *                                                                         *
-    ***************************************************************************
+    more details. You should have received a copy of the GNU General Public 
+    License and the FreeRTOS license exception along with FreeRTOS; if not it 
+    can be viewed here: http://www.freertos.org/a00114.html and also obtained 
+    by writing to Richard Barry, contact details for whom are available on the
+    FreeRTOS WEB site.
 
     1 tab == 4 spaces!
-
-    Please ensure to read the configuration and relevant port sections of the
-    online documentation.
 
     http://www.FreeRTOS.org - Documentation, latest information, license and
     contact details.

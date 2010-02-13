d60032cec3f01a0b99e7c8932768db2ea596e5bb
Richard Barry <ribarry@amazon.com>
2010-02-13 21:15:28 +0000

    Not much more than a reformatted uIP so far - this will eventually replace the FreeRTOS-uIP files.

---
+++ Demo/Common/ethernet/FreeTCPIP/net/pack_struct_end.h
@@ -0,0 +1,7 @@
+#ifdef __GNUC__
+__attribute__( (packed) );
+#endif
+#ifdef __RENESAS__
+;
+#pragma unpack
+#endif
---
+++ Demo/Common/ethernet/FreeTCPIP/net/pack_struct_start.h
@@ -0,0 +1,8 @@
+#ifdef __GNUC__
+
+/* Nothing to do here. */
+;
+#endif
+#ifdef __RENESAS__
+#pragma pack 1
+#endif

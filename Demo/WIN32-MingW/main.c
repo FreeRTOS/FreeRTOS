b09733cdfcb8abe61f426cd53694641ef9a0ed54
Richard Barry <ribarry@amazon.com>
2010-11-21 20:59:57 +0000

    Added the MingW/Eclipse project for the new Win32 port.

---
+++ Demo/WIN32-MingW/main.c
@@ -0,0 +1,235 @@
+/*
+    FreeRTOS V6.1.0 - Copyright (C) 2010 Real Time Engineers Ltd.
+
+    ***************************************************************************
+    *                                                                         *
+    * If you are:                                                             *
+    *                                                                         *
+    *    + New to FreeRTOS,                                                   *
+    *    + Wanting to learn FreeRTOS or multitasking in general quickly       *
+    *    + Looking for basic training,                                        *
+    *    + Wanting to improve your FreeRTOS skills and productivity           *
+    *                                                                         *
+    * then take a look at the FreeRTOS books - available as PDF or paperback  *
+    *                                                                         *
+    *        "Using the FreeRTOS Real Time Kernel - a Practical Guide"        *
+    *                  http://www.FreeRTOS.org/Documentation                  *
+    *                                                                         *
+    * A pdf reference manual is also available.  Both are usually delivered   *
+    * to your inbox within 20 minutes to two hours when purchased between 8am *
+    * and 8pm GMT (although please allow up to 24 hours in case of            *
+    * exceptional circumstances).  Thank you for your support!                *
+    *                                                                         *
+    ***************************************************************************
+
+    This file is part of the FreeRTOS distribution.
+
+    FreeRTOS is free software; you can redistribute it and/or modify it under
+    the terms of the GNU General Public License (version 2) as published by the
+    Free Software Foundation AND MODIFIED BY the FreeRTOS exception.
+    ***NOTE*** The exception to the GPL is included to allow you to distribute
+    a combined work that includes FreeRTOS without being obliged to provide the
+    source code for proprietary components outside of the FreeRTOS kernel.
+    FreeRTOS is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+    more details. You should have received a copy of the GNU General Public
+    License and the FreeRTOS license exception along with FreeRTOS; if not it
+    can be viewed here: http://www.freertos.org/a00114.html and also obtained
+    by writing to Richard Barry, contact details for whom are available on the
+    FreeRTOS WEB site.
+
+    1 tab == 4 spaces!
+
+    http://www.FreeRTOS.org - Documentation, latest information, license and
+    contact details.
+
+    http://www.SafeRTOS.com - A version that is certified for use in safety
+    critical systems.
+
+    http://www.OpenRTOS.com - Commercial support, development, porting,
+    licensing and training services.
+*/
+
+/*
+ *******************************************************************************
+ * -NOTE- The Win32 port is a simulation (or is that emulation?) only!  Do not
+ * expect to get real time behaviour from the Win32 port or this demo
+ * application.  It is provided as a convenient development and demonstration
+ * test bed only.  Also, at the time of writing, a method of deleting theads 
+ * has not been implemented, although doing so would be trivial so this
+ * functionality might be added in at a later date.  At present, calling 
+ * vTaskDelete() will delete the real time task from FreeRTOS but not the Win32
+ * thread in which the task was executing.  DO NOT CALL vTaskDelete() when using 
+ * the Win32 port!  This was tested using Windows XP on a dual core laptop.
+ *
+ * - READ THE WEB DOCUMENTATION FOR THIS PORT FOR MORE INFORMATION ON USING IT -
+ *******************************************************************************
+ *
+ * main() creates all the demo application tasks, then starts the scheduler.  
+ * The web documentation provides more details of the standard demo application 
+ * tasks, which provide no particular functionality but do provide a good 
+ * example of how to use the FreeRTOS API.
+ *
+ * In addition to the standard demo tasks, the following tasks and tests are
+ * defined and/or created within this file:
+ *
+ * "Check" task - This only executes every five seconds but has a high priority
+ * to ensure it gets processor time.  Its main function is to check that all the
+ * standard demo tasks are still operational.  While no errors have been
+ * discovered the check task will print out "OK" and the current simulated tick
+ * time.  If an error is discovered in the execution of a task then the check
+ * task will print out an appropriate error message.
+ *
+ */
+
+
+/* Standard includes. */
+#include <stdio.h>
+
+/* Kernel includes. */
+#include <FreeRTOS.h>
+#include "task.h"
+#include "queue.h"
+
+/* Standard demo includes. */
+#include "BlockQ.h"
+#include "integer.h"
+#include "semtest.h"
+#include "PollQ.h"
+#include "GenQTest.h"
+#include "QPeek.h"
+#include "recmutex.h"
+#include "flop.h"
+
+/* Priorities at which the tasks are created. */
+#define mainCHECK_TASK_PRIORITY		( configMAX_PRIORITIES - 1 )
+#define mainQUEUE_POLL_PRIORITY		( tskIDLE_PRIORITY + 1 )
+#define mainSEM_TEST_PRIORITY		( tskIDLE_PRIORITY + 1 )
+#define mainBLOCK_Q_PRIORITY		( tskIDLE_PRIORITY + 2 )
+#define mainCREATOR_TASK_PRIORITY   ( tskIDLE_PRIORITY + 3 )
+#define mainFLASH_TASK_PRIORITY		( tskIDLE_PRIORITY + 1 )
+#define mainuIP_TASK_PRIORITY		( tskIDLE_PRIORITY + 2 )
+#define mainINTEGER_TASK_PRIORITY   ( tskIDLE_PRIORITY )
+#define mainGEN_QUEUE_TASK_PRIORITY	( tskIDLE_PRIORITY )
+#define mainFLOP_TASK_PRIORITY		( tskIDLE_PRIORITY )
+
+/* Task function prototypes. */
+static void prvCheckTask( void *pvParameters );
+
+/*-----------------------------------------------------------*/
+
+int main( void )
+{
+	/* Start the check task as described at the top of this file. */
+	xTaskCreate( prvCheckTask, ( signed char * ) "Check", configMINIMAL_STACK_SIZE, NULL, mainCHECK_TASK_PRIORITY, NULL );
+
+	/* Create the standard demo tasks. */
+	vStartBlockingQueueTasks( mainBLOCK_Q_PRIORITY );
+	vStartSemaphoreTasks( mainSEM_TEST_PRIORITY );
+	vStartPolledQueueTasks( mainQUEUE_POLL_PRIORITY );
+	vStartIntegerMathTasks( mainINTEGER_TASK_PRIORITY );
+	vStartGenericQueueTasks( mainGEN_QUEUE_TASK_PRIORITY );
+	vStartQueuePeekTasks();
+	vStartMathTasks( mainFLOP_TASK_PRIORITY );
+	vStartRecursiveMutexTasks();
+
+	/* Start the scheduler itself. */
+	vTaskStartScheduler();
+
+    /* Should never get here unless there was not enough heap space to create 
+	the idle and other system tasks. */
+    return 0;
+}
+/*-----------------------------------------------------------*/
+
+static void prvCheckTask( void *pvParameters )
+{
+portTickType xNextWakeTime;
+const portTickType xCycleFrequency = 5000 / portTICK_RATE_MS;
+char *pcStatusMessage = "OK";
+
+	/* Just to remove compiler warning. */
+	( void ) pvParameters;
+
+	/* Initialise xNextWakeTime - this only needs to be done once. */
+	xNextWakeTime = xTaskGetTickCount();
+
+	for( ;; )
+	{
+		/* Place this task in the blocked state until it is time to run again. */
+		vTaskDelayUntil( &xNextWakeTime, xCycleFrequency );
+
+		/* Check the standard demo tasks are running without error. */
+	    if( xAreIntegerMathsTaskStillRunning() != pdTRUE )
+	    {
+			pcStatusMessage = "Error: IntMath";
+	    }	
+		else if( xAreGenericQueueTasksStillRunning() != pdTRUE )
+		{			
+			pcStatusMessage = "Error: GenQueue";
+		}
+		else if( xAreQueuePeekTasksStillRunning() != pdTRUE )
+		{
+			pcStatusMessage = "Error: QueuePeek";
+		}
+		else if( xAreBlockingQueuesStillRunning() != pdTRUE )
+		{
+			pcStatusMessage = "Error: BlockQueue";
+		}
+	    else if( xAreSemaphoreTasksStillRunning() != pdTRUE )
+	    {
+			pcStatusMessage = "Error: SemTest";
+	    }
+	    else if( xArePollingQueuesStillRunning() != pdTRUE )
+	    {
+			pcStatusMessage = "Error: PollQueue";
+	    }
+		else if( xAreMathsTaskStillRunning() != pdPASS )
+		{
+			pcStatusMessage = "Error: Flop";
+		}
+	    else if( xAreRecursiveMutexTasksStillRunning() != pdTRUE )
+	    {
+			pcStatusMessage = "Error: RecMutex";
+		}
+
+		/* This is the only task that uses stdout so its ok to call printf() 
+		directly. */
+		printf( "%s - %d\r\n", pcStatusMessage, ( int ) xTaskGetTickCount() );
+		fflush( stdout ); /* Required by Eclipse console. */
+	}
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationIdleHook( void )
+{
+	/* Sleep to reduce CPU load, but don't sleep indefinitely if not using 
+	preemption as as nothing will cause	a task switch. */
+	#if( configUSE_PREEMPTION != 0 )
+	{
+		SleepEx( INFINITE, TRUE );
+	}
+	#else
+	{
+		const unsigned long ulMSToSleep = 5;
+
+		SleepEx( ulMSToSleep, TRUE );
+	}
+	#endif
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationMallocFailedHook( void )
+{
+	/* Can be implemented if required, but probably not required in this 
+	environment and running this demo. */
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationStackOverflowHook( void )
+{
+	/* Can be implemented if required, but not required in this 
+	environment and running this demo. */
+}
+

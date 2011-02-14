7d76fd568fa264450e0e71304171abfad4a6a57e
Richard Barry <ribarry@amazon.com>
2011-02-14 10:52:52 +0000

    Continue work on the new timer implementation test application.  Nearly complete.

--- Demo/Common/include/TimerDemo.h
+++ Demo/Common/include/TimerDemo.h
@@ -33,9 +33,9 @@
     FreeRTOS is distributed in the hope that it will be useful, but WITHOUT
     ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
     FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-    more details. You should have received a copy of the GNU General Public 
-    License and the FreeRTOS license exception along with FreeRTOS; if not it 
-    can be viewed here: http://www.freertos.org/a00114.html and also obtained 
+    more details. You should have received a copy of the GNU General Public
+    License and the FreeRTOS license exception along with FreeRTOS; if not it
+    can be viewed here: http://www.freertos.org/a00114.html and also obtained
     by writing to Richard Barry, contact details for whom are available on the
     FreeRTOS WEB site.
 
@@ -55,7 +55,8 @@
 #define TIMER_DEMO_H
 
 void vStartTimerDemoTask( portTickType xBaseFrequencyIn );
-portBASE_TYPE xAreTimerDemoTasksStillRunning( void );
+portBASE_TYPE xAreTimerDemoTasksStillRunning( portTickType xCycleFrequency );
+void vTimerPeriodicISRTests( void );
 
 #endif /* TIMER_DEMO_H */
 

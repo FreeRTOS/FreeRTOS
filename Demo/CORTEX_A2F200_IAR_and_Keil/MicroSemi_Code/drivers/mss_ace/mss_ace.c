d23df3d0afd322fb0c80dc51725d99a7795be579
Richard Barry <ribarry@amazon.com>
2011-04-28 14:59:31 +0000

    Remove ACE files that are not necessary for the SmartFusion demo.

--- Demo/CORTEX_A2F200_IAR_and_Keil/MicroSemi_Code/drivers/mss_ace/mss_ace.c
+++ Demo/CORTEX_A2F200_IAR_and_Keil/MicroSemi_Code/drivers/mss_ace/mss_ace.c
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * (c) Copyright 2009 Actel Corporation.  All rights reserved.
- * 
+ *
  * SVN $Revision: 2905 $
  * SVN $Date: 2010-08-20 14:03:28 +0100 (Fri, 20 Aug 2010) $
  */
@@ -16,7 +16,7 @@
 
 #ifdef __cplusplus
 extern "C" {
-#endif 
+#endif
 
 #define START_ADC_CONVERSION    0x80uL
 
@@ -31,8 +31,10 @@ void ace_init_convert(void);
 void ACE_init( void )
 {
     /* Initialize driver's internal data. */
-    ace_init_flags();
-    
+	#if (ACE_NB_OF_PPE_FLAGS > 0)
+	    ace_init_flags();
+	#endif
+
     /* Initialize the data structures used by conversion functions. */
     ace_init_convert();
 }
@@ -70,13 +72,13 @@ uint16_t ACE_get_adc_result
     uint32_t data_valid;
 
     ASSERT( adc_id < NB_OF_ANALOG_MODULES );
-    
+
     if ( adc_id < (uint8_t)NB_OF_ANALOG_MODULES )
     {
         do {
             data_valid = *adc_status_reg_lut[adc_id] & ADC_DATAVALID_MASK;
         } while ( !data_valid );
-        
+
         result = (uint16_t)(*adc_status_reg_lut[adc_id] & ADC_RESULT_MASK);
     }
     return result;
@@ -88,7 +90,7 @@ uint16_t ACE_get_adc_result
 
 #define SDD_ENABLE_MASK     0x20uL
 #define SDD_REG_SEL_MASK    0x40uL
- 
+
 #define DAC0_SYNC_EN_MASK   0x10uL
 #define DAC1_SYNC_EN_MASK   0x20uL
 #define DAC2_SYNC_EN_MASK   0x40uL
@@ -149,7 +151,7 @@ void ACE_configure_sdd
 )
 {
     ASSERT( sdd_id < NB_OF_SDD );
-    
+
     if ( sdd_id < NB_OF_SDD )
     {
         const uint8_t sdd_2_quad_lut[NB_OF_SDD] = {0u, 2u, 4u};
@@ -157,16 +159,16 @@ void ACE_configure_sdd
         uint8_t obd_mode_idx = 1u;
         uint8_t chopping_mode_idx = 0u;
         uint32_t saved_pc2_ctrl;
-        
+
         quad_id = sdd_2_quad_lut[sdd_id];
-        
+
         /* Pause the SSE PC2 while accesses to ACB from APB3 are taking place. */
         saved_pc2_ctrl = ACE->PC2_CTRL;
         ACE->PC2_CTRL = 0u;
-        
+
         /* Select between voltage/current and RTZ modes.*/
         ACE->ACB_DATA[quad_id].b6 = mode;
-        
+
         /* Load manufacturing generated trim value. */
         if ( (mode & OBD_MODE_MASK) > 0u )
         {
@@ -178,17 +180,17 @@ void ACE_configure_sdd
         }
         ACE->ACB_DATA[quad_id].b4
             = p_mtd_data->odb_trimming[sdd_id][obd_mode_idx][chopping_mode_idx];
-        
+
         /* Restore SSE PC2 operations since no ACB accesses should take place
          * beyond this point. */
         ACE->PC2_CTRL = saved_pc2_ctrl;
-    
+
         /* Set SDD resolution. */
         *dac_ctrl_reg_lut[sdd_id] = (uint32_t)resolution;
-        
+
         /* Update SDD value through SSE_DACn_BYTES01. */
         *dac_ctrl_reg_lut[sdd_id] |= SDD_REG_SEL_MASK;
-        
+
         /* Synchronous or individual SDD update. */
         if ( INDIVIDUAL_UPDATE == sync_update )
         {
@@ -210,7 +212,7 @@ void ACE_enable_sdd
 )
 {
     ASSERT( sdd_id < NB_OF_SDD );
-    
+
     if ( sdd_id < NB_OF_SDD )
     {
         *dac_ctrl_reg_lut[sdd_id] |= SDD_ENABLE_MASK;
@@ -226,7 +228,7 @@ void ACE_disable_sdd
 )
 {
     ASSERT( sdd_id < NB_OF_SDD );
-    
+
     if ( sdd_id < NB_OF_SDD )
     {
         *dac_ctrl_reg_lut[sdd_id] &= ~SDD_ENABLE_MASK;
@@ -243,7 +245,7 @@ void ACE_set_sdd_value
 )
 {
     ASSERT( sdd_id < NB_OF_SDD );
-    
+
     if ( sdd_id < NB_OF_SDD )
     {
         *dac_byte2_reg_lut[sdd_id] = sdd_value >> 16;
@@ -262,9 +264,9 @@ void ACE_set_sdd_value_sync
 )
 {
     uint32_t dac_sync_ctrl;
-    
+
     dac_sync_ctrl = ACE->DAC_SYNC_CTRL;
-    
+
     if ( SDD_NO_UPDATE != sdd0_value )
     {
         ACE->DAC0_BYTE2 = sdd0_value >> 16;
@@ -286,7 +288,7 @@ void ACE_set_sdd_value_sync
         ACE->SSE_DAC2_BYTES01 = sdd2_value;
         dac_sync_ctrl |= DAC2_SYNC_UPDATE;
     }
-    
+
     ACE->DAC_SYNC_CTRL = dac_sync_ctrl;
 }
 
@@ -357,23 +359,23 @@ void ACE_set_comp_reference
 {
     uint8_t scb_id;
     uint32_t odd;
-    
+
     odd = (uint32_t)comp_id & 0x01uL;
-    
+
     ASSERT( comp_id < NB_OF_COMPARATORS );
     ASSERT( reference < NB_OF_COMP_REF );
     ASSERT( odd );    /* Only Temperature block comparators have configurable reference input. */
-    
+
     if ( (comp_id < NB_OF_COMPARATORS) && (reference < NB_OF_COMP_REF) && (odd) )
     {
         uint32_t saved_pc2_ctrl;
-        
+
         scb_id = comp_id_2_scb_lut[comp_id];
-        
+
         /* Pause the SSE PC2 while accesses to ACB from APB3 are taking place. */
         saved_pc2_ctrl = ACE->PC2_CTRL;
         ACE->PC2_CTRL = 0u;
-    
+
         if ( ADC_IN_COMP_REF == reference )
         {
             ACE->ACB_DATA[scb_id].b10 &= (uint8_t)~B10_COMP_VREF_SW_MASK;
@@ -384,7 +386,7 @@ void ACE_set_comp_reference
             ACE->ACB_DATA[scb_id].b10 &= (uint8_t)~B10_COMP_VREF_SW_MASK;
             ACE->ACB_DATA[scb_id].b11 = (ACE->ACB_DATA[scb_id].b11 & (uint8_t)~B11_DAC_MUXSEL_MASK) + (uint8_t)reference;
         }
-    
+
         /* Restore SSE PC2 operations since no ACB accesses should take place
          * beyond this point. */
         ACE->PC2_CTRL = saved_pc2_ctrl;
@@ -401,22 +403,22 @@ void ACE_set_comp_hysteresis
 )
 {
     uint8_t scb_id;
-    
+
     ASSERT( comp_id < NB_OF_COMPARATORS );
     ASSERT( hysteresis < NB_OF_HYSTERESIS );
-    
+
     if ( (comp_id < NB_OF_COMPARATORS) && (hysteresis < NB_OF_HYSTERESIS) )
     {
         uint32_t odd;
         uint32_t saved_pc2_ctrl;
-        
+
         scb_id = comp_id_2_scb_lut[comp_id];
         odd = (uint32_t)comp_id & 0x01uL;
-        
+
         /* Pause the SSE PC2 while accesses to ACB from APB3 are taking place. */
         saved_pc2_ctrl = ACE->PC2_CTRL;
         ACE->PC2_CTRL = 0u;
-    
+
         if ( odd )
         {
             /* Temperature monitor block comparator. */
@@ -427,7 +429,7 @@ void ACE_set_comp_hysteresis
             /* Current monitor block comparator. */
             ACE->ACB_DATA[scb_id].b9 = (ACE->ACB_DATA[scb_id].b9 & HYSTERESIS_MASK) | (uint8_t)((uint8_t)hysteresis << HYSTERESIS_SHIFT);
         }
-        
+
         /* Restore SSE PC2 operations since no ACB accesses should take place
          * beyond this point. */
         ACE->PC2_CTRL = saved_pc2_ctrl;
@@ -435,7 +437,7 @@ void ACE_set_comp_hysteresis
 }
 
 /*-------------------------------------------------------------------------*//**
-  
+
  */
 void ACE_enable_comp
 (
@@ -443,21 +445,21 @@ void ACE_enable_comp
 )
 {
     uint8_t scb_id;
-    
+
     ASSERT( comp_id < NB_OF_COMPARATORS );
-    
+
     if ( comp_id < NB_OF_COMPARATORS )
     {
         uint32_t odd;
         uint32_t saved_pc2_ctrl;
-        
+
         scb_id = comp_id_2_scb_lut[comp_id];
         odd = (uint32_t)comp_id & 0x01uL;
-        
+
         /* Pause the SSE PC2 while accesses to ACB from APB3 are taking place. */
         saved_pc2_ctrl = ACE->PC2_CTRL;
         ACE->PC2_CTRL = 0u;
-        
+
         if ( odd )
         {
             /* Temperature monitor block comparator. */
@@ -468,7 +470,7 @@ void ACE_enable_comp
             /* Current monitor block comparator. */
             ACE->ACB_DATA[scb_id].b9 |= COMPARATOR_ENABLE_MASK;
         }
-        
+
         /* Restore SSE PC2 operations since no ACB accesses should take place
          * beyond this point. */
         ACE->PC2_CTRL = saved_pc2_ctrl;
@@ -484,21 +486,21 @@ void ACE_disable_comp
 )
 {
     uint8_t scb_id;
-    
+
     ASSERT( comp_id < NB_OF_COMPARATORS );
-    
+
     if ( comp_id < NB_OF_COMPARATORS )
     {
         uint32_t odd;
         uint32_t saved_pc2_ctrl;
-        
+
         scb_id = comp_id_2_scb_lut[comp_id];
         odd = (uint32_t)comp_id & 0x01uL;
-        
+
         /* Pause the SSE PC2 while accesses to ACB from APB3 are taking place. */
         saved_pc2_ctrl = ACE->PC2_CTRL;
         ACE->PC2_CTRL = 0u;
-        
+
         if ( odd )
         {
             /* Temperature monitor block comparator. */
@@ -509,7 +511,7 @@ void ACE_disable_comp
             /* Current monitor block comparator. */
             ACE->ACB_DATA[scb_id].b9 &= (uint8_t)~COMPARATOR_ENABLE_MASK;
         }
-        
+
         /* Restore SSE PC2 operations since no ACB accesses should take place
          * beyond this point. */
         ACE->PC2_CTRL = saved_pc2_ctrl;
@@ -539,7 +541,7 @@ void ACE_enable_comp_rise_irq
 )
 {
     ASSERT( comp_id < NB_OF_COMPARATORS );
-    
+
     ACE->COMP_IRQ_EN |= (FIRST_RISE_IRQ_MASK << (uint32_t)comp_id);
 }
 
@@ -552,7 +554,7 @@ void ACE_disable_comp_rise_irq
 )
 {
     ASSERT( comp_id < NB_OF_COMPARATORS );
-    
+
     ACE->COMP_IRQ_EN &= ~(FIRST_RISE_IRQ_MASK << (uint32_t)comp_id);
 }
 
@@ -565,7 +567,7 @@ void ACE_clear_comp_rise_irq
 )
 {
     ASSERT( comp_id < NB_OF_COMPARATORS );
-    
+
     ACE->COMP_IRQ_CLR |= (FIRST_RISE_IRQ_MASK << (uint32_t)comp_id);
 }
 
@@ -578,7 +580,7 @@ void ACE_enable_comp_fall_irq
 )
 {
     ASSERT( comp_id < NB_OF_COMPARATORS );
-    
+
     ACE->COMP_IRQ_EN |= (FIRST_FALL_IRQ_MASK << (uint32_t)comp_id);
 }
 
@@ -591,7 +593,7 @@ void ACE_disable_comp_fall_irq
 )
 {
     ASSERT( comp_id < NB_OF_COMPARATORS );
-    
+
     ACE->COMP_IRQ_EN &= ~(FIRST_FALL_IRQ_MASK << (uint32_t)comp_id);
 }
 
@@ -604,7 +606,7 @@ void ACE_clear_comp_fall_irq
 )
 {
     ASSERT( comp_id < NB_OF_COMPARATORS );
-    
+
     ACE->COMP_IRQ_CLR |= (FIRST_FALL_IRQ_MASK << (uint32_t)comp_id);
 }
 
@@ -643,9 +645,9 @@ ACE_get_first_channel
 )
 {
     ace_channel_handle_t channel_handle;
-    
+
     channel_handle = (ace_channel_handle_t)0;
-    
+
     return channel_handle;
 }
 
@@ -659,12 +661,12 @@ ACE_get_next_channel
 )
 {
     ++channel_handle;
-    
+
     if ( channel_handle >= NB_OF_ACE_CHANNEL_HANDLES )
     {
          channel_handle = (ace_channel_handle_t)0;
     }
-    
+
     return channel_handle;
 }
 
@@ -679,7 +681,7 @@ ACE_get_channel_handle
 {
     uint16_t channel_idx;
     ace_channel_handle_t channel_handle = INVALID_CHANNEL_HANDLE;
-    
+
     for ( channel_idx = 0u;  channel_idx < (uint16_t)ACE_NB_OF_INPUT_CHANNELS; ++channel_idx )
     {
         if ( g_ace_channel_desc_table[channel_idx].p_sz_channel_name != 0 )
@@ -708,7 +710,7 @@ ACE_get_input_channel_handle
 {
     uint16_t channel_idx;
     ace_channel_handle_t channel_handle = INVALID_CHANNEL_HANDLE;
-    
+
     for ( channel_idx = 0u;  channel_idx < (uint16_t)ACE_NB_OF_INPUT_CHANNELS; ++channel_idx )
     {
         if ( g_ace_channel_desc_table[channel_idx].signal_id == channel_id )
@@ -732,10 +734,10 @@ ACE_get_ppe_sample
 {
     uint16_t sample;
     uint16_t ppe_offset;
-    
+
     ppe_offset = g_ace_channel_desc_table[channel_handle].signal_ppe_offset;
     sample = (uint16_t)(ACE->PPE_RAM_DATA[ppe_offset] >> 16u);
-    
+
     return sample;
 }
 

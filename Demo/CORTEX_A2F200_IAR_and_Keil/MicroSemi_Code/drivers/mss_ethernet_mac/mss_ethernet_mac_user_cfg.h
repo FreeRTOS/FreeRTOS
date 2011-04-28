f6879be32778aa5448c6f3fdaed4e079682060ba
Richard Barry <ribarry@amazon.com>
2011-04-28 15:01:40 +0000

    Put some belt and braces checks into the updated SmartFusion MAC driver.

--- Demo/CORTEX_A2F200_IAR_and_Keil/MicroSemi_Code/drivers/mss_ethernet_mac/mss_ethernet_mac_user_cfg.h
+++ Demo/CORTEX_A2F200_IAR_and_Keil/MicroSemi_Code/drivers/mss_ethernet_mac/mss_ethernet_mac_user_cfg.h
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * (c) Copyright 2007 Actel Corporation.  All rights reserved.
- * 
+ *
  * Actel:Firmware:MSS_Ethernet_MAC_Driver:2.0.103 configuration.
  *
  */
@@ -17,7 +17,7 @@
 
 #define BUS_ARBITRATION_SCHEME 0
 #define PROGRAMMABLE_BURST_LENGTH 0
-#define RX_RING_SIZE 4
+#define RX_RING_SIZE 5
 #define SETUP_FRAME_TIME_OUT 10000
 #define STATE_CHANGE_TIME_OUT 10000
 #define TX_RING_SIZE 2

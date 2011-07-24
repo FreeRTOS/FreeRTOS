9d1c23f6da99109f1b3e7f17c1117a3546525bf7
Richard Barry <ribarry@amazon.com>
2011-07-24 16:19:52 +0000

    Add in starting point for the MicroBlaze EthernetLite lwIP port layer.

---
+++ Demo/Common/ethernet/lwip-1.4.0/ports/MicroBlaze-Ethernet-Lite/ethernetif.c
@@ -0,0 +1,427 @@
+/*
+    FreeRTOS V7.0.1 - Copyright (C) 2011 Real Time Engineers Ltd.
+	
+
+    ***************************************************************************
+     *                                                                       *
+     *    FreeRTOS tutorial books are available in pdf and paperback.        *
+     *    Complete, revised, and edited pdf reference manuals are also       *
+     *    available.                                                         *
+     *                                                                       *
+     *    Purchasing FreeRTOS documentation will not only help you, by       *
+     *    ensuring you get running as quickly as possible and with an        *
+     *    in-depth knowledge of how to use FreeRTOS, it will also help       *
+     *    the FreeRTOS project to continue with its mission of providing     *
+     *    professional grade, cross platform, de facto standard solutions    *
+     *    for microcontrollers - completely free of charge!                  *
+     *                                                                       *
+     *    >>> See http://www.FreeRTOS.org/Documentation for details. <<<     *
+     *                                                                       *
+     *    Thank you for using FreeRTOS, and thank you for your support!      *
+     *                                                                       *
+    ***************************************************************************
+
+
+    This file is part of the FreeRTOS distribution.
+
+    FreeRTOS is free software; you can redistribute it and/or modify it under
+    the terms of the GNU General Public License (version 2) as published by the
+    Free Software Foundation AND MODIFIED BY the FreeRTOS exception.
+    >>>NOTE<<< The modification to the GPL is included to allow you to
+    distribute a combined work that includes FreeRTOS without being obliged to
+    provide the source code for proprietary components outside of the FreeRTOS
+    kernel.  FreeRTOS is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+    more details. You should have received a copy of the GNU General Public
+    License and the FreeRTOS license exception along with FreeRTOS; if not it
+    can be viewed here: http://www.freertos.org/a00114.html and also obtained
+    by writing to Richard Barry, contact details for whom are available on the
+    FreeRTOS WEB site.
+
+    1 tab == 4 spaces!
+
+    http://www.FreeRTOS.org - Documentation, latest information, license and
+    contact details.
+
+    http://www.SafeRTOS.com - A version that is certified for use in safety
+    critical systems.
+
+    http://www.OpenRTOS.com - Commercial support, development, porting,
+    licensing and training services.
+*/
+
+/* FreeRTOS includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+
+/* BSP includes. */
+#include "xemaclite.h"
+
+/* lwIP includes. */
+#include "lwip/opt.h"
+#include "lwip/def.h"
+#include "lwip/mem.h"
+#include "lwip/pbuf.h"
+#include "lwip/sys.h"
+#include <lwip/stats.h>
+#include <lwip/snmp.h>
+#include "netif/etharp.h"
+
+/* Define those to better describe your network interface. */
+#define IFNAME0 'e'
+#define IFNAME1 'l'
+
+#define netifMAX_MTU 1500
+
+struct xEthernetIf
+{
+	struct eth_addr *ethaddr;
+	/* Add whatever per-interface state that is needed here. */
+};
+
+/*
+ * Place received packet in a pbuf and send a message to the tcpip task to let
+ * it know new data has arrived.
+ */
+static void prvEthernetInput( struct netif *pxNetIf, const unsigned char * const pucInputData, long lInputLength );
+
+/*
+ * Copy the received data into a pbuf.
+ */
+static struct pbuf *prvLowLevelInput( const unsigned char * const pucInputData, long lDataLength );
+
+/*
+ * Send data from a pbuf to the hardware.
+ */
+static err_t prvLowLevelOutput( struct netif *pxNetIf, struct pbuf *p );
+
+/*
+ * Perform any hardware and/or driver initialisation necessary.
+ */
+static void prvLowLevelInit( struct netif *pxNetIf );
+
+/*-----------------------------------------------------------*/
+
+/**
+ * In this function, the hardware should be initialized.
+ * Called from ethernetif_init().
+ *
+ * @param pxNetIf the already initialized lwip network interface structure
+ *		for this etherpxNetIf
+ */
+static void prvLowLevelInit( struct netif *pxNetIf )
+{
+	/* set MAC hardware address length */
+	pxNetIf->hwaddr_len = ETHARP_HWADDR_LEN;
+
+	/* set MAC hardware address */
+	pxNetIf->hwaddr[ 0 ] = configMAC_ADDR0;
+	pxNetIf->hwaddr[ 1 ] = configMAC_ADDR1;
+	pxNetIf->hwaddr[ 2 ] = configMAC_ADDR2;
+	pxNetIf->hwaddr[ 3 ] = configMAC_ADDR3;
+	pxNetIf->hwaddr[ 4 ] = configMAC_ADDR4;
+	pxNetIf->hwaddr[ 5 ] = configMAC_ADDR5;
+
+	/* device capabilities */
+	/* don't set pxNetIf_FLAG_ETHARP if this device is not an ethernet one */
+	pxNetIf->flags = NETIF_FLAG_BROADCAST | NETIF_FLAG_ETHARP | NETIF_FLAG_LINK_UP;
+
+#if 0
+_RB_
+
+	/* Query the computer the simulation is being executed on to find the
+	network interfaces it has installed. */
+	pxAllNetworkInterfaces = prvPrintAvailableNetworkInterfaces();
+
+	/* Open the network interface.  The number of the interface to be opened is
+	set by the configNETWORK_INTERFACE_TO_USE constant in FreeRTOSConfig.h.
+	Calling this function will set the pxOpenedInterfaceHandle variable.  If,
+	after calling this function, pxOpenedInterfaceHandle is equal to NULL, then
+	the interface could not be opened. */
+	if( pxAllNetworkInterfaces != NULL )
+	{
+		prvOpenSelectedNetworkInterface( pxAllNetworkInterfaces );
+	}
+
+	/* Remember which interface was opened as it is used in the interrupt
+	simulator task. */
+	pxlwIPNetIf = pxNetIf;
+#endif
+}
+
+/**
+ * This function should do the actual transmission of the packet. The packet is
+ * contained in the pbuf that is passed to the function. This pbuf
+ * might be chained.
+ *
+ * @param pxNetIf the lwip network interface structure for this etherpxNetIf
+ * @param p the MAC packet to send (e.g. IP packet including MAC addresses and type)
+ * @return ERR_OK if the packet could be sent
+ *		 an err_t value if the packet couldn't be sent
+ *
+ * @note Returning ERR_MEM here if a DMA queue of your MAC is full can lead to
+ *	   strange results. You might consider waiting for space in the DMA queue
+ *	   to become availale since the stack doesn't retry to send a packet
+ *	   dropped because of memory failure (except for the TCP timers).
+ */
+
+static err_t prvLowLevelOutput( struct netif *pxNetIf, struct pbuf *p )
+{
+
+	/* This is taken from lwIP example code and therefore does not conform
+	to the FreeRTOS coding standard. */
+
+struct pbuf *q;
+static unsigned char ucBuffer[ 1520 ];
+unsigned char *pucBuffer = ucBuffer;
+unsigned char *pucChar;
+struct eth_hdr *pxHeader;
+u16_t usTotalLength = p->tot_len - ETH_PAD_SIZE;
+err_t xReturn = ERR_OK;
+
+	#if defined(LWIP_DEBUG) && LWIP_NETIF_TX_SINGLE_PBUF
+		LWIP_ASSERT("p->next == NULL && p->len == p->tot_len", p->next == NULL && p->len == p->tot_len);
+	#endif
+
+	/* Initiate transfer. */
+	if( p->len == p->tot_len ) 
+	{
+		/* No pbuf chain, don't have to copy -> faster. */
+		pucBuffer = &( ( unsigned char * ) p->payload )[ ETH_PAD_SIZE ];
+	} 
+	else 
+	{
+		/* pbuf chain, copy into contiguous ucBuffer. */
+		if( p->tot_len >= sizeof( ucBuffer ) )
+		{
+			LINK_STATS_INC( link.lenerr );
+			LINK_STATS_INC( link.drop );
+			snmp_inc_ifoutdiscards( pxNetIf );
+			xReturn = ERR_BUF;
+		}
+		else
+		{
+			pucChar = ucBuffer;
+
+			for( q = p; q != NULL; q = q->next )
+			{
+				/* Send the data from the pbuf to the interface, one pbuf at a
+				time. The size of the data in each pbuf is kept in the ->len
+				variable. */
+				/* send data from(q->payload, q->len); */
+				LWIP_DEBUGF( NETIF_DEBUG, ("NETIF: send pucChar %p q->payload %p q->len %i q->next %p\n", pucChar, q->payload, ( int ) q->len, ( void* ) q->next ) );
+				if( q == p )
+				{
+					memcpy( pucChar, &( ( char * ) q->payload )[ ETH_PAD_SIZE ], q->len - ETH_PAD_SIZE );
+					pucChar += q->len - ETH_PAD_SIZE;
+				}
+				else
+				{
+					memcpy( pucChar, q->payload, q->len );
+					pucChar += q->len;
+				}
+			}
+		}
+	}
+
+	if( xReturn == ERR_OK )
+	{
+#if 0
+_RB_
+		/* signal that packet should be sent */
+		if( pcap_sendpacket( pxOpenedInterfaceHandle, pucBuffer, usTotalLength ) < 0 ) 
+		{
+			LINK_STATS_INC( link.memerr );
+			LINK_STATS_INC( link.drop );
+			snmp_inc_ifoutdiscards( pxNetIf );
+			xReturn = ERR_BUF;
+		}
+		else
+		{
+			LINK_STATS_INC( link.xmit );
+			snmp_add_ifoutoctets( pxNetIf, usTotalLength );
+			pxHeader = ( struct eth_hdr * )p->payload;
+
+			if( ( pxHeader->dest.addr[ 0 ] & 1 ) != 0 ) 
+			{
+				/* broadcast or multicast packet*/
+				snmp_inc_ifoutnucastpkts( pxNetIf );
+			} 
+			else 
+			{
+				/* unicast packet */
+				snmp_inc_ifoutucastpkts( pxNetIf );
+			}
+		}
+#endif
+	}
+
+	return xReturn;
+}
+
+/**
+ * Should allocate a pbuf and transfer the bytes of the incoming
+ * packet from the interface into the pbuf.
+ *
+ * @param pxNetIf the lwip network interface structure for this etherpxNetIf
+ * @return a pbuf filled with the received packet (including MAC header)
+ *		 NULL on memory error
+ */
+static struct pbuf *prvLowLevelInput( const unsigned char * const pucInputData, long lDataLength )
+{
+struct pbuf *p = NULL, *q;
+
+	if( lDataLength > 0 )
+	{
+		#if ETH_PAD_SIZE
+			len += ETH_PAD_SIZE; /* allow room for Ethernet padding */
+		#endif
+
+		/* We allocate a pbuf chain of pbufs from the pool. */
+		p = pbuf_alloc( PBUF_RAW, lDataLength, PBUF_POOL );
+  
+		if( p != NULL ) 
+		{
+			#if ETH_PAD_SIZE
+				pbuf_header( p, -ETH_PAD_SIZE ); /* drop the padding word */
+			#endif
+
+			/* We iterate over the pbuf chain until we have read the entire
+			* packet into the pbuf. */
+			lDataLength = 0;
+			for( q = p; q != NULL; q = q->next ) 
+			{
+				/* Read enough bytes to fill this pbuf in the chain. The
+				* available data in the pbuf is given by the q->len
+				* variable.
+				* This does not necessarily have to be a memcpy, you can also preallocate
+				* pbufs for a DMA-enabled MAC and after receiving truncate it to the
+				* actually received size. In this case, ensure the usTotalLength member of the
+				* pbuf is the sum of the chained pbuf len members.
+				*/
+				memcpy( q->payload, &( pucInputData[ lDataLength ] ), q->len );
+				lDataLength += q->len;
+			}
+
+			#if ETH_PAD_SIZE
+				pbuf_header( p, ETH_PAD_SIZE ); /* reclaim the padding word */
+			#endif
+
+			LINK_STATS_INC(link.recv);
+		}
+	}
+
+	return p;  
+}
+
+/**
+ * This function should be called when a packet is ready to be read
+ * from the interface. It uses the function prvLowLevelInput() that
+ * should handle the actual reception of bytes from the network
+ * interface. Then the type of the received packet is determined and
+ * the appropriate input function is called.
+ *
+ * @param pxNetIf the lwip network interface structure for this etherpxNetIf
+ */
+static void prvEthernetInput( struct netif *pxNetIf, const unsigned char * const pucInputData, long lInputLength )
+{
+	/* This is taken from lwIP example code and therefore does not conform
+	to the FreeRTOS coding standard. */
+	
+struct eth_hdr *pxHeader;
+struct pbuf *p;
+
+	/* move received packet into a new pbuf */
+	p = prvLowLevelInput( pucInputData, lInputLength );
+
+	/* no packet could be read, silently ignore this */
+	if( p != NULL )
+	{
+		/* points to packet payload, which starts with an Ethernet header */
+		pxHeader = p->payload;
+
+		switch( htons( pxHeader->type ) )
+		{
+			/* IP or ARP packet? */
+			case ETHTYPE_IP:
+			case ETHTYPE_ARP:
+								/* full packet send to tcpip_thread to process */
+								if( pxNetIf->input( p, pxNetIf ) != ERR_OK )
+								{
+									LWIP_DEBUGF(NETIF_DEBUG, ( "ethernetif_input: IP input error\n" ) );
+									pbuf_free(p);
+									p = NULL;
+								}
+								break;
+
+			default:
+								pbuf_free( p );
+								p = NULL;
+			break;
+		}
+	}
+}
+
+/**
+ * Should be called at the beginning of the program to set up the
+ * network interface. It calls the function prvLowLevelInit() to do the
+ * actual setup of the hardware.
+ *
+ * This function should be passed as a parameter to pxNetIf_add().
+ *
+ * @param pxNetIf the lwip network interface structure for this etherpxNetIf
+ * @return ERR_OK if the loopif is initialized
+ *		 ERR_MEM if private data couldn't be allocated
+ *		 any other err_t on error
+ */
+err_t ethernetif_init( struct netif *pxNetIf )
+{
+err_t xReturn = ERR_OK;
+
+	/* This is taken from lwIP example code and therefore does not conform
+	to the FreeRTOS coding standard. */
+	
+struct xEthernetIf *pxEthernetIf;
+
+	LWIP_ASSERT( "pxNetIf != NULL", ( pxNetIf != NULL ) );
+	
+	pxEthernetIf = mem_malloc( sizeof( struct xEthernetIf ) );
+	if( pxEthernetIf == NULL ) 
+	{
+		LWIP_DEBUGF(NETIF_DEBUG, ( "ethernetif_init: out of memory\n" ) );
+		xReturn = ERR_MEM;
+	}
+	else
+	{
+		#if LWIP_NETIF_HOSTNAME
+		{
+			/* Initialize interface hostname */
+			pxNetIf->hostname = "lwip";
+		}
+		#endif /* LWIP_NETIF_HOSTNAME */
+
+		pxNetIf->state = pxEthernetIf;
+		pxNetIf->name[ 0 ] = IFNAME0;
+		pxNetIf->name[ 1 ] = IFNAME1;
+
+		/* We directly use etharp_output() here to save a function call.
+		* You can instead declare your own function an call etharp_output()
+		* from it if you have to do some checks before sending (e.g. if link
+		* is available...) */
+		pxNetIf->output = etharp_output;
+		pxNetIf->flags = NETIF_FLAG_BROADCAST | NETIF_FLAG_ETHARP | NETIF_FLAG_IGMP;
+		pxNetIf->hwaddr_len = ETHARP_HWADDR_LEN;
+		pxNetIf->mtu = netifMAX_MTU;
+		pxNetIf->linkoutput = prvLowLevelOutput;
+
+		pxEthernetIf->ethaddr = ( struct eth_addr * ) &( pxNetIf->hwaddr[ 0 ] );
+
+		/* initialize the hardware */
+		prvLowLevelInit( pxNetIf );
+	}
+
+	return xReturn;
+}
+/*-----------------------------------------------------------*/
---
+++ Demo/Common/ethernet/lwip-1.4.0/ports/MicroBlaze-Ethernet-Lite/include/arch/cc.h
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2001-2003 Swedish Institute of Computer Science.
+ * All rights reserved. 
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission. 
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED 
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+ * SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT 
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING 
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+ * OF SUCH DAMAGE.
+ *
+ * This file is part of the lwIP TCP/IP stack.
+ * 
+ * Author: Adam Dunkels <adam@sics.se>
+ *
+ */
+#ifndef __ARCH_CC_H__
+#define __ARCH_CC_H__
+
+/* Include some files for defining library routines */
+#include <stdio.h> /* printf, fflush, FILE */
+#include <stdlib.h> /* abort */
+
+
+
+/** @todo fix some warnings: don't use #pragma if compiling with cygwin gcc */
+#ifndef __GNUC__
+	#include <limits.h>
+	#pragma warning (disable: 4244) /* disable conversion warning (implicit integer promotion!) */
+	#pragma warning (disable: 4127) /* conditional expression is constant */
+	#pragma warning (disable: 4996) /* 'strncpy' was declared deprecated */
+	#pragma warning (disable: 4103) /* structure packing changed by including file */
+#endif
+
+#define LWIP_PROVIDE_ERRNO
+
+/* Define generic types used in lwIP */
+typedef unsigned   char    u8_t;
+typedef signed     char    s8_t;
+typedef unsigned   short   u16_t;
+typedef signed     short   s16_t;
+typedef unsigned   long    u32_t;
+typedef signed     long    s32_t;
+
+typedef size_t mem_ptr_t;
+typedef u32_t sys_prot_t;
+
+/* Define (sn)printf formatters for these lwIP types */
+#define X8_F  "02x"
+#define U16_F "hu"
+#define S16_F "hd"
+#define X16_F "hx"
+#define U32_F "lu"
+#define S32_F "ld"
+#define X32_F "lx"
+#define SZT_F U32_F
+
+/* Compiler hints for packing structures */
+#define PACK_STRUCT_STRUCT __attribute__( (packed) )
+
+/* Plaform specific diagnostic output */
+#define LWIP_PLATFORM_DIAG(x)   do { printf x; } while(0)
+
+#define LWIP_PLATFORM_ASSERT(x) do { printf("Assertion \"%s\" failed at line %d in %s\n", \
+                                     x, __LINE__, __FILE__); fflush(NULL); abort(); } while(0)
+
+#define LWIP_ERROR(message, expression, handler) do { if (!(expression)) { \
+  printf("Assertion \"%s\" failed at line %d in %s\n", message, __LINE__, __FILE__); \
+  fflush(NULL);handler;} } while(0)
+
+/* C runtime functions redefined */
+#define snprintf _snprintf
+
+u32_t dns_lookup_external_hosts_file(const char *name);
+
+#define LWIP_RAND() ((u32_t)rand())
+
+#endif /* __ARCH_CC_H__ */

6a561bb2e7fc5ab353dc7612dbc27ad2a4846314
Richard Barry <ribarry@amazon.com>
2011-08-26 12:37:54 +0000

    Add queue registry usage to the MicroBlaze demo so it can be used with the state viewer plug in.

--- Demo/MicroBlaze_Spartan-6_EthernetLite/SDKProjects/RTOSDemoSource/FreeRTOSConfig.h
+++ Demo/MicroBlaze_Spartan-6_EthernetLite/SDKProjects/RTOSDemoSource/FreeRTOSConfig.h
@@ -87,7 +87,7 @@
 #define configUSE_16_BIT_TICKS			0
 #define configIDLE_SHOULD_YIELD			1
 #define configUSE_MUTEXES				1
-#define configQUEUE_REGISTRY_SIZE		0
+#define configQUEUE_REGISTRY_SIZE		10
 #define configCHECK_FOR_STACK_OVERFLOW	2
 #define configUSE_RECURSIVE_MUTEXES		1
 #define configUSE_MALLOC_FAILED_HOOK	1

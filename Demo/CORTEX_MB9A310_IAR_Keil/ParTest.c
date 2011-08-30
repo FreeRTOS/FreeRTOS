bcbd6c8a7dc87607eb0e94c8154bcb4a3c1c9bb9
Richard Barry <ribarry@amazon.com>
2011-08-30 20:32:41 +0000

    Added demo for the MB9A314 - the IAR project is working, the Keil one is not set up for the correct chip yet.

---
+++ Demo/CORTEX_MB9A310_IAR_Keil/ParTest.c
@@ -0,0 +1,154 @@
+/*
+    FreeRTOS V7.0.1 - Copyright (C) 2011 Real Time Engineers Ltd.
+	
+
+    ***************************************************************************
+     *                                                                       *
+     *    FreeRTOS tutorial books are available in pdf and paperback.        *
+     *    Complete, revised, and edited pdf reference manuals are also       *
+     *    available.                                                         *
+     *                                                                       *
+     *    Purchasing FreeRTOS documentation will not only help you, by       *
+     *    ensuring you get running as quickly as possible and with an        *
+     *    in-depth knowledge of how to use FreeRTOS, it will also help       *
+     *    the FreeRTOS project to continue with its mission of providing     *
+     *    professional grade, cross platform, de facto standard solutions    *
+     *    for microcontrollers - completely free of charge!                  *
+     *                                                                       *
+     *    >>> See http://www.FreeRTOS.org/Documentation for details. <<<     *
+     *                                                                       *
+     *    Thank you for using FreeRTOS, and thank you for your support!      *
+     *                                                                       *
+    ***************************************************************************
+
+
+    This file is part of the FreeRTOS distribution.
+
+    FreeRTOS is free software; you can redistribute it and/or modify it under
+    the terms of the GNU General Public License (version 2) as published by the
+    Free Software Foundation AND MODIFIED BY the FreeRTOS exception.
+    >>>NOTE<<< The modification to the GPL is included to allow you to
+    distribute a combined work that includes FreeRTOS without being obliged to
+    provide the source code for proprietary components outside of the FreeRTOS
+    kernel.  FreeRTOS is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+    more details. You should have received a copy of the GNU General Public
+    License and the FreeRTOS license exception along with FreeRTOS; if not it
+    can be viewed here: http://www.freertos.org/a00114.html and also obtained
+    by writing to Richard Barry, contact details for whom are available on the
+    FreeRTOS WEB site.
+
+    1 tab == 4 spaces!
+
+    http://www.FreeRTOS.org - Documentation, latest information, license and
+    contact details.
+
+    http://www.SafeRTOS.com - A version that is certified for use in safety
+    critical systems.
+
+    http://www.OpenRTOS.com - Commercial support, development, porting,
+    licensing and training services.
+*/
+
+/*-----------------------------------------------------------
+ * Simple parallel port IO routines.
+ *-----------------------------------------------------------*/
+
+/* Kernel includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+
+/* Fujitsu drivers/libraries. */
+#include "mcu.h"
+
+/* Only the LEDs on one of the two seven segment displays are used. */
+#define partstMAX_LEDS		8
+
+/*-----------------------------------------------------------*/
+
+void vParTestInitialise( void )
+{
+	/* Analog inputs are not used on the LED outputs. */
+	FM3_GPIO->ADE  = 0x0000;
+
+	/* Set to output. */
+	FM3_GPIO->DDR1 |= 0xFFFF;
+	FM3_GPIO->DDR3 |= 0xFFFF;
+	
+	/* Set as GPIO. */
+	FM3_GPIO->PFR1 &= 0x0000;
+	FM3_GPIO->PFR3 &= 0x0000;
+
+	/* Start with all LEDs off. */
+	FM3_GPIO->PDOR1 = 0xFFFF;
+	FM3_GPIO->PDOR1 = 0xFFFF;
+}
+/*-----------------------------------------------------------*/
+
+void vParTestSetLED( unsigned portBASE_TYPE uxLED, signed portBASE_TYPE xValue )
+{
+	if( uxLED < partstMAX_LEDS )
+	{
+		/* A critical section is used as the LEDs are also accessed from an
+		interrupt. */
+		taskENTER_CRITICAL();
+		{
+			if( xValue == pdTRUE )
+			{
+				FM3_GPIO->PDOR1 &= ~( 1UL << uxLED );
+			}
+			else
+			{
+				FM3_GPIO->PDOR1 |= ( 1UL << uxLED );
+			}
+		}
+		taskEXIT_CRITICAL();
+	}
+}
+/*-----------------------------------------------------------*/
+
+void vParTestSetLEDFromISR( unsigned portBASE_TYPE uxLED, signed portBASE_TYPE xValue )
+{
+unsigned portBASE_TYPE uxInterruptFlags;
+
+	uxInterruptFlags = portSET_INTERRUPT_MASK_FROM_ISR();
+	{
+		if( uxLED < partstMAX_LEDS )
+		{
+			if( xValue == pdTRUE )
+			{
+				FM3_GPIO->PDOR1 &= ~( 1UL << uxLED );
+			}
+			else
+			{
+				FM3_GPIO->PDOR1 |= ( 1UL << uxLED );
+			}
+		}
+	}
+	portCLEAR_INTERRUPT_MASK_FROM_ISR( uxInterruptFlags );
+}
+/*-----------------------------------------------------------*/
+
+void vParTestToggleLED( unsigned portBASE_TYPE uxLED )
+{
+	if( uxLED < partstMAX_LEDS )
+	{
+		/* A critical section is used as the LEDs are also accessed from an
+		interrupt. */
+		taskENTER_CRITICAL();
+		{
+			if( ( FM3_GPIO->PDOR1 & ( 1UL << uxLED ) ) != 0UL )
+			{
+				FM3_GPIO->PDOR1 &= ~( 1UL << uxLED );
+			}
+			else
+			{
+				FM3_GPIO->PDOR1 |= ( 1UL << uxLED );
+			}
+		}
+		taskEXIT_CRITICAL();
+	}
+}
+/*-----------------------------------------------------------*/
+

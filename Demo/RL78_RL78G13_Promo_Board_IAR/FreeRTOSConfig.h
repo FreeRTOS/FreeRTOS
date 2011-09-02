1abff5d5cd460ee4d90864d335d61e5b1dd05c30
Richard Barry <ribarry@amazon.com>
2011-09-02 10:49:16 +0000

    Start an RL78 demo project.

---
+++ Demo/RL78_RL78G13_Promo_Board_IAR/FreeRTOSConfig.h
@@ -0,0 +1,161 @@
+/*
+    FreeRTOS V7.0.1 - Copyright (C) 2011 Real Time Engineers Ltd.
+	
+
+	FreeRTOS supports many tools and architectures. V7.0.0 is sponsored by:
+	Atollic AB - Atollic provides professional embedded systems development
+	tools for C/C++ development, code analysis and test automation.
+	See http://www.atollic.com
+	
+
+    ***************************************************************************
+     *                                                                       *
+     *    FreeRTOS tutorial books are available in pdf and paperback.        *
+     *    Complete, revised, and edited pdf reference manuals are also       *
+     *    available.                                                         *
+     *                                                                       *
+     *    Purchasing FreeRTOS documentation will not only help you, by       *
+     *    ensuring you get running as quickly as possible and with an        *
+     *    in-depth knowledge of how to use FreeRTOS, it will also help       *
+     *    the FreeRTOS project to continue with its mission of providing     *
+     *    professional grade, cross platform, de facto standard solutions    *
+     *    for microcontrollers - completely free of charge!                  *
+     *                                                                       *
+     *    >>> See http://www.FreeRTOS.org/Documentation for details. <<<     *
+     *                                                                       *
+     *    Thank you for using FreeRTOS, and thank you for your support!      *
+     *                                                                       *
+    ***************************************************************************
+
+
+    This file is part of the FreeRTOS distribution.
+
+    FreeRTOS is free software; you can redistribute it and/or modify it under
+    the terms of the GNU General Public License (version 2) as published by the
+    Free Software Foundation AND MODIFIED BY the FreeRTOS exception.
+    >>>NOTE<<< The modification to the GPL is included to allow you to
+    distribute a combined work that includes FreeRTOS without being obliged to
+    provide the source code for proprietary components outside of the FreeRTOS
+    kernel.  FreeRTOS is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+    more details. You should have received a copy of the GNU General Public
+    License and the FreeRTOS license exception along with FreeRTOS; if not it
+    can be viewed here: http://www.freertos.org/a00114.html and also obtained
+    by writing to Richard Barry, contact details for whom are available on the
+    FreeRTOS WEB site.
+
+    1 tab == 4 spaces!
+
+    http://www.FreeRTOS.org - Documentation, latest information, license and
+    contact details.
+
+    http://www.SafeRTOS.com - A version that is certified for use in safety
+    critical systems.
+
+    http://www.OpenRTOS.com - Commercial support, development, porting,
+    licensing and training services.
+*/
+
+#ifndef FREERTOS_CONFIG_H
+#define FREERTOS_CONFIG_H
+
+/*-----------------------------------------------------------
+ * Application specific definitions.
+ *
+ * These definitions should be adjusted for your particular hardware and
+ * application requirements.
+ *
+ * THESE PARAMETERS ARE DESCRIBED WITHIN THE 'CONFIGURATION' SECTION OF THE
+ * FreeRTOS API DOCUMENTATION AVAILABLE ON THE FreeRTOS.org WEB SITE.
+ *
+ * See http://www.freertos.org/a00110.html.
+ *----------------------------------------------------------*/
+
+/* This #ifdef prevents the enclosed code being included from within an
+asm file.  It is valid in a C file, but not valid in an asm file. */
+#ifdef __IAR_SYSTEMS_ICC__
+
+	#pragma language=extended
+	#pragma system_include
+
+	#include <intrinsics.h>
+
+	/* Device specific includes. */
+	#include <ior5f100le.h>
+	#include <ior5f100le_ext.h>
+
+#endif /* __IAR_SYSTEMS_ICC__ */
+
+#define configUSE_PREEMPTION			1
+#define configTICK_RATE_HZ				( ( portTickType ) 1000 )
+#define configMAX_PRIORITIES			( ( unsigned portBASE_TYPE ) 4 )
+#define configMINIMAL_STACK_SIZE		( ( unsigned short ) 100 )
+#define configMAX_TASK_NAME_LEN			( 10 )
+#define configUSE_TRACE_FACILITY		0
+#define configUSE_16_BIT_TICKS			1
+#define configIDLE_SHOULD_YIELD			1
+#define configTOTAL_HEAP_SIZE			( (size_t ) ( 2900 ) )
+#define configCHECK_FOR_STACK_OVERFLOW	2
+#define configUSE_MUTEXES				1
+
+/* Hook function definitions. */
+#define configUSE_IDLE_HOOK				0
+#define configUSE_TICK_HOOK				0
+#define configUSE_MALLOC_FAILED_HOOK	1
+
+/* Software timer definitions. */
+#define configUSE_TIMERS				1
+#define configTIMER_TASK_PRIORITY		( 2 )
+#define configTIMER_QUEUE_LENGTH		10
+#define configTIMER_TASK_STACK_DEPTH	( configMINIMAL_STACK_SIZE * 2 )
+
+/* Co-routine definitions. */
+#define configUSE_CO_ROUTINES 			0
+#define configMAX_CO_ROUTINE_PRIORITIES	( 2 )
+
+/* Set the following definitions to 1 to include the API function, or zero
+to exclude the API function. */
+#define INCLUDE_vTaskPrioritySet			1
+#define INCLUDE_uxTaskPriorityGet			1
+#define INCLUDE_vTaskDelete					0
+#define INCLUDE_vTaskCleanUpResources		0
+#define INCLUDE_vTaskSuspend				1
+#define INCLUDE_vTaskDelayUntil				0
+#define INCLUDE_vTaskDelay					1
+#define INCLUDE_xTaskIdleTaskHandleGet 		0
+#define INCLUDE_xTimerDaemonTaskHandleGet 	0
+
+
+/******************************************************************************
+ * PORT SPECIFIC CONFIGURATION OPTIONS
+ ******************************************************************************/
+
+/*
+ * RL78/G13 Clock Source Configuration
+ * 1 = use internal High Speed Clock Source (typically 32Mhz on the RL78/G13)
+ * 0 = use external Clock Source
+ */
+#define configCLOCK_SOURCE			  1
+
+/*
+ * RL78/G13 Memory Model
+ * 1 = use far memory mode
+ * 0 = use near memory mode
+ *
+ * This setting must match the setting in the IAR project options.
+ */
+#define configMEMORY_MODE			   0
+
+#if configCLOCK_SOURCE == 0
+	#define configCPU_CLOCK_HZ		( ( unsigned long ) 20000000 )  /* using the external clock source */
+#else
+	#define configCPU_CLOCK_HZ		( ( unsigned long ) 32000000 )   /* using the internal high speed clock */
+#endif /* configCLOCK_SOURCE */
+
+#define configASSERT( x ) if( ( x ) == 0 ) { taskDISABLE_INTERRUPTS(); for( ;; ); }
+
+
+
+#endif /* FREERTOS_CONFIG_H */
+
---
+++ Demo/RL78_RL78G13_Promo_Board_IAR/main.c
@@ -0,0 +1,312 @@
+/*
+    FreeRTOS V7.0.1 - Copyright (C) 2011 Real Time Engineers Ltd.
+	
+
+	FreeRTOS supports many tools and architectures. V7.0.0 is sponsored by:
+	Atollic AB - Atollic provides professional embedded systems development
+	tools for C/C++ development, code analysis and test automation.
+	See http://www.atollic.com
+	
+
+    ***************************************************************************
+     *                                                                       *
+     *    FreeRTOS tutorial books are available in pdf and paperback.        *
+     *    Complete, revised, and edited pdf reference manuals are also       *
+     *    available.                                                         *
+     *                                                                       *
+     *    Purchasing FreeRTOS documentation will not only help you, by       *
+     *    ensuring you get running as quickly as possible and with an        *
+     *    in-depth knowledge of how to use FreeRTOS, it will also help       *
+     *    the FreeRTOS project to continue with its mission of providing     *
+     *    professional grade, cross platform, de facto standard solutions    *
+     *    for microcontrollers - completely free of charge!                  *
+     *                                                                       *
+     *    >>> See http://www.FreeRTOS.org/Documentation for details. <<<     *
+     *                                                                       *
+     *    Thank you for using FreeRTOS, and thank you for your support!      *
+     *                                                                       *
+    ***************************************************************************
+
+
+    This file is part of the FreeRTOS distribution.
+
+    FreeRTOS is free software; you can redistribute it and/or modify it under
+    the terms of the GNU General Public License (version 2) as published by the
+    Free Software Foundation AND MODIFIED BY the FreeRTOS exception.
+    >>>NOTE<<< The modification to the GPL is included to allow you to
+    distribute a combined work that includes FreeRTOS without being obliged to
+    provide the source code for proprietary components outside of the FreeRTOS
+    kernel.  FreeRTOS is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+    more details. You should have received a copy of the GNU General Public
+    License and the FreeRTOS license exception along with FreeRTOS; if not it
+    can be viewed here: http://www.freertos.org/a00114.html and also obtained
+    by writing to Richard Barry, contact details for whom are available on the
+    FreeRTOS WEB site.
+
+    1 tab == 4 spaces!
+
+    http://www.FreeRTOS.org - Documentation, latest information, license and
+    contact details.
+
+    http://www.SafeRTOS.com - A version that is certified for use in safety
+    critical systems.
+
+    http://www.OpenRTOS.com - Commercial support, development, porting,
+    licensing and training services.
+*/
+
+/* Standard includes. */
+#include <stdlib.h>
+#include <string.h>
+
+/* Scheduler include files. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "timers.h"
+
+/* Standard demo includes. */
+#include "dynamic.h"
+
+/* The period at which the check timer will expire, in ms, provided no errors
+have been reported by any of the standard demo tasks.  ms are converted to the
+equivalent in ticks using the portTICK_RATE_MS constant. */
+#define mainCHECK_TIMER_PERIOD_MS			( 3000UL / portTICK_RATE_MS )
+
+/* The period at which the check timer will expire, in ms, if an error has been
+reported in one of the standard demo tasks.  ms are converted to the equivalent
+in ticks using the portTICK_RATE_MS constant. */
+#define mainERROR_CHECK_TIMER_PERIOD_MS 	( 200UL / portTICK_RATE_MS )
+
+/* The LED toggled by the check task. */
+#define mainLED_0   						P7_bit.no7
+
+/* A block time of zero simple means "don't block". */
+#define mainDONT_BLOCK						( 0U )
+
+/*-----------------------------------------------------------*/
+
+/*
+ * The 'check' timer callback function, as described at the top of this file.
+ */
+static void prvCheckTimerCallback( xTimerHandle xTimer );
+
+/*
+ * This function is called from the C startup routine to setup the processor -
+ * in particular the clock source.
+ */
+int __low_level_init(void);
+
+/*
+ * Functions that define the RegTest tasks as described at the top of this file.
+ */
+extern void vRegTest1( void *pvParameters );
+extern void vRegTest2( void *pvParameters );
+
+
+/*-----------------------------------------------------------*/
+
+/* If an error is discovered by one of the RegTest tasks then this flag is set
+to pdFAIL.  The 'check' task then inspects this flag to detect errors within
+the RegTest tasks. */
+static short sRegTestStatus = pdPASS;
+
+/* The check timer.  This uses prvCheckTimerCallback() as its callback
+function. */
+static xTimerHandle xCheckTimer = NULL;
+
+/* RL78/G13 Option Byte Definition. Watchdog disabled, LVI enabled, OCD interface
+enabled. */
+__root __far const unsigned char OptionByte[] @ 0x00C0 =
+{
+	WATCHDOG_DISABLED, LVI_ENABLED, RESERVED_FF, OCD_ENABLED
+};
+
+/* Security byte definition */
+__root __far const unsigned char SecuIDCode[]  @ 0x00C4 =
+{
+	0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x54
+};
+
+/*-----------------------------------------------------------*/
+
+short main( void )
+{
+	/* Creates all the tasks and timers, then starts the scheduler. */
+
+	/* First create the 'standard demo' tasks.  These are used to demonstrate
+	API functions being used and also to test the kernel port.  More information
+	is provided on the FreeRTOS.org WEB site. */
+	vStartDynamicPriorityTasks();
+
+	/* Create the RegTest tasks as described at the top of this file. */
+	xTaskCreate( vRegTest1, "Reg1", configMINIMAL_STACK_SIZE, NULL, 0, NULL );
+	xTaskCreate( vRegTest2, "Reg2", configMINIMAL_STACK_SIZE, NULL, 0, NULL );	
+
+	/* Create the software timer that performs the 'check' functionality,
+	as described at the top of this file. */
+	xCheckTimer = xTimerCreate( ( const signed char * ) "CheckTimer",/* A text name, purely to help debugging. */
+								( mainCHECK_TIMER_PERIOD_MS ),		/* The timer period, in this case 3000ms (3s). */
+								pdTRUE,								/* This is an auto-reload timer, so xAutoReload is set to pdTRUE. */
+								( void * ) 0,						/* The ID is not used, so can be set to anything. */
+								prvCheckTimerCallback				/* The callback function that inspects the status of all the other tasks. */
+							  );
+							
+	
+	/* Send a command to start the check timer.  It will not actually start
+	until the scheduler is running (when vTaskStartScheduler() is called). */
+	xTimerStart( xCheckTimer, mainDONT_BLOCK );
+	
+	/* Finally start the scheduler running. */
+	vTaskStartScheduler();
+
+	/* If this line is reached then vTaskStartScheduler() returned because there
+	was insufficient heap memory remaining for the idle task to be created. */
+	for( ;; );
+}
+/*-----------------------------------------------------------*/
+
+static void prvCheckTimerCallback( xTimerHandle xTimer )
+{
+static portBASE_TYPE xChangedTimerPeriodAlready = pdFALSE, xErrorStatus = pdPASS;
+
+	if( xAreDynamicPriorityTasksStillRunning() != pdTRUE )
+	{
+		xErrorStatus = pdFAIL;
+	}
+
+	if( sRegTestStatus != pdPASS )
+	{
+		xErrorStatus = pdFAIL;
+	}
+
+	if( ( xErrorStatus == pdFAIL ) && ( xChangedTimerPeriodAlready == pdFALSE ) )
+	{
+		/* An error has occurred, but the timer's period has not yet been changed,
+		change it now, and remember that it has been changed.  Shortening the
+		timer's period means the LED will toggle at a faster rate, giving a
+		visible indication that something has gone wrong. */
+		xChangedTimerPeriodAlready = pdTRUE;
+			
+		/* This call to xTimerChangePeriod() uses a zero block time.  Functions
+		called from inside of a timer callback function must *never* attempt to
+		block. */
+		xTimerChangePeriod( xCheckTimer, ( mainERROR_CHECK_TIMER_PERIOD_MS ), mainDONT_BLOCK );
+	}
+	
+	/* Toggle the LED.  The toggle rate will depend on whether or not an error
+	has been found in any tasks. */
+	mainLED_0 = !mainLED_0;
+}
+/*-----------------------------------------------------------*/
+
+int __low_level_init(void)
+{
+unsigned portCHAR ucResetFlag = RESF;
+
+	portDISABLE_INTERRUPTS();
+
+	/* Clock Configuration:
+	In this port, to use the internal high speed clock source of the
+	microcontroller, define the configCLOCK_SOURCE as 1 in FreeRTOSConfig.h.  To
+	use an external	clock define configCLOCK_SOURCE as 0. */
+	#if configCLOCK_SOURCE == 1
+	{
+		/* Set fMX */
+		CMC = 0x00;
+		MSTOP = 1U;
+		
+		/* Set fMAIN */
+		MCM0 = 0U;
+		
+		/* Set fSUB */
+		XTSTOP = 1U;
+		OSMC = 0x10;
+		
+		/* Set fCLK */
+		CSS = 0U;
+		
+		/* Set fIH */
+		HIOSTOP = 0U;
+	}
+	#else
+	{
+		unsigned char ucTempStabset, ucTempStabWait;	
+
+		/* Set fMX */
+		CMC = 0x41;
+		OSTS = 0x07;
+		MSTOP = 0U;
+		ucTempStabset = 0xFF;
+		
+		do
+		{
+			ucTempStabWait = OSTC;
+			ucTempStabWait &= ucTempStabset;
+		}
+		while( ucTempStabWait != ucTempStabset );
+		
+		/* Set fMAIN */
+		MCM0 = 1U;
+		
+		/* Set fSUB */
+		XTSTOP = 1U;
+		OSMC = 0x10;
+		
+		/* Set fCLK */
+		CSS = 0U;
+		
+		/* Set fIH */
+		HIOSTOP = 0U;
+	}
+	#endif /* configCLOCK_SOURCE == 1 */
+	
+	/* LED port initialization - set port register. */
+	P7 &= 0x7F;
+	
+	/* Set port mode register. */
+	PM7 &= 0x7F;
+	
+	/* Switch pin initialization - enable pull-up resistor. */
+	PU12_bit.no0  = 1;
+
+	return pdTRUE;
+}
+/*-----------------------------------------------------------*/
+
+void vRegTestError( void )
+{
+	/* Called by the RegTest tasks if an error is found.  lRegTestStatus is
+	inspected by the check task. */
+	sRegTestStatus = pdFAIL;
+
+	/* Do not return from here as the reg test tasks clobber all registers so
+	function calls may not function correctly. */
+	for( ;; );
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationMallocFailedHook( void )
+{
+	/* Called if a call to pvPortMalloc() fails because there is insufficient
+	free memory available in the FreeRTOS heap.  pvPortMalloc() is called
+	internally by FreeRTOS API functions that create tasks, queues, software
+	timers, and semaphores.  The size of the FreeRTOS heap is set by the
+	configTOTAL_HEAP_SIZE configuration constant in FreeRTOSConfig.h. */
+	taskDISABLE_INTERRUPTS();
+	for( ;; );
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationStackOverflowHook( xTaskHandle *pxTask, signed char *pcTaskName )
+{
+	( void ) pcTaskName;
+	( void ) pxTask;
+
+	/* Run time stack overflow checking is performed if
+	configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
+	function is called if a stack overflow is detected. */
+	taskDISABLE_INTERRUPTS();
+	for( ;; );
+}

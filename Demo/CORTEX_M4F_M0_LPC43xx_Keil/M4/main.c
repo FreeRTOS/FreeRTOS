b0a6d939f4766bd80259fe4a7785ccd5eaf01eba
Richard Barry <ribarry@amazon.com>
2012-01-19 22:17:10 +0000

    Replace "Infinite loop" comment in main() with something sensible.

--- Demo/CORTEX_M4F_M0_LPC43xx_Keil/M4/main.c
+++ Demo/CORTEX_M4F_M0_LPC43xx_Keil/M4/main.c
@@ -216,7 +216,11 @@ int main( void )
 	/* Start the scheduler. */
 	vTaskStartScheduler();
 	
-	/* Infinite loop */
+	/* If all is well, the scheduler will now be running, and the following line
+	will never be reached.  If the following line does execute, then there was
+	insufficient FreeRTOS heap memory available for the idle and/or timer tasks
+	to be created.  See the memory management section on the FreeRTOS web site
+	for more details. */
 	for( ;; );	
 }
 /*-----------------------------------------------------------*/

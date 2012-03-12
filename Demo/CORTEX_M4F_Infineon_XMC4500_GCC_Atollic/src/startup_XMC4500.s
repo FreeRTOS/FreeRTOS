5267ac07a4f94dd674c3f257db6bfe24f003a09b
Richard Barry <ribarry@amazon.com>
2012-03-12 14:37:27 +0000

    Modified header comments only.

--- Demo/CORTEX_M4F_Infineon_XMC4500_GCC_Atollic/src/startup_XMC4500.s
+++ Demo/CORTEX_M4F_Infineon_XMC4500_GCC_Atollic/src/startup_XMC4500.s
@@ -17,12 +17,8 @@
 **                of any kind.
 **
 **  (c)Copyright Atollic AB.
-**  You may use this file as-is or modify it according to the needs of your
-**  project. Distribution of this file (unmodified or modified) is not
-**  permitted. Atollic AB permit registered Atollic TrueSTUDIO(R) users the
-**  rights to distribute the assembled, compiled & linked contents of this
-**  file as part of an application binary file, provided that it is built
-**  using the Atollic TrueSTUDIO(R) toolchain.
+**  This file may be distributed and used with the FreeRTOS example project
+**  for Atollic TrueSTUDIO only.
 **
 *****************************************************************************
 */

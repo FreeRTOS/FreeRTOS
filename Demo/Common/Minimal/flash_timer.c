785621ff228b993fa8fad77016b337157c2180fb
Richard Barry <ribarry@amazon.com>
2012-03-17 14:59:46 +0000

    Add the flash_timer standard demo timers (rather than tasks).  These behave as the flash tasks, but use timers in place of tasks.

---
+++ Demo/Common/Minimal/flash_timer.c
@@ -0,0 +1,124 @@
+/*
+    FreeRTOS V7.1.0 - Copyright (C) 2011 Real Time Engineers Ltd.
+	
+
+    ***************************************************************************
+     *                                                                       *
+     *    FreeRTOS tutorial books are available in pdf and paperback.        *
+     *    Complete, revised, and edited pdf reference manuals are also       *
+     *    available.                                                         *
+     *                                                                       *
+     *    Purchasing FreeRTOS documentation will not only help you, by       *
+     *    ensuring you get running as quickly as possible and with an        *
+     *    in-depth knowledge of how to use FreeRTOS, it will also help       *
+     *    the FreeRTOS project to continue with its mission of providing     *
+     *    professional grade, cross platform, de facto standard solutions    *
+     *    for microcontrollers - completely free of charge!                  *
+     *                                                                       *
+     *    >>> See http://www.FreeRTOS.org/Documentation for details. <<<     *
+     *                                                                       *
+     *    Thank you for using FreeRTOS, and thank you for your support!      *
+     *                                                                       *
+    ***************************************************************************
+
+
+    This file is part of the FreeRTOS distribution.
+
+    FreeRTOS is free software; you can redistribute it and/or modify it under
+    the terms of the GNU General Public License (version 2) as published by the
+    Free Software Foundation AND MODIFIED BY the FreeRTOS exception.
+    >>>NOTE<<< The modification to the GPL is included to allow you to
+    distribute a combined work that includes FreeRTOS without being obliged to
+    provide the source code for proprietary components outside of the FreeRTOS
+    kernel.  FreeRTOS is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+    more details. You should have received a copy of the GNU General Public
+    License and the FreeRTOS license exception along with FreeRTOS; if not it
+    can be viewed here: http://www.freertos.org/a00114.html and also obtained
+    by writing to Richard Barry, contact details for whom are available on the
+    FreeRTOS WEB site.
+
+    1 tab == 4 spaces!
+
+    http://www.FreeRTOS.org - Documentation, latest information, license and
+    contact details.
+
+    http://www.SafeRTOS.com - A version that is certified for use in safety
+    critical systems.
+
+    http://www.OpenRTOS.com - Commercial support, development, porting,
+    licensing and training services.
+*/
+
+/**
+ * Repeatedly toggles one or more LEDs using software timers - one timer per
+ * LED.
+ */
+ 
+/* Scheduler include files. */
+#include "FreeRTOS.h"
+#include "timers.h"
+
+/* Demo program include files. */
+#include "partest.h"
+#include "flash_timer.h"
+
+/* The toggle rates are all a multple of ledFLASH_RATE_BASE. */
+#define ledFLASH_RATE_BASE	( ( ( portTickType ) 333 ) / portTICK_RATE_MS )
+
+/* A block time of zero simple means "don't block". */
+#define ledDONT_BLOCK		( ( portTickType ) 0 )
+
+/*-----------------------------------------------------------*/
+
+/*
+ * The callback function used by each LED flashing timer.  All the timers use
+ * this function, and the timer ID is used within the function to determine
+ * which timer has actually expired.
+ */
+static void prvLEDTimerCallback( xTimerHandle xTimer );
+
+/*-----------------------------------------------------------*/
+
+void vStartLEDFlashTimers( unsigned portBASE_TYPE xNumberOfLEDs )
+{
+portBASE_TYPE xLEDTimer;
+xTimerHandle xTimer;
+
+	/* Create and start the requested number of timers. */
+	for( xLEDTimer = 0; xLEDTimer < xNumberOfLEDs; ++xLEDTimer )
+	{
+		/* Create the timer. */
+		xTimer = xTimerCreate( 	"Flasher",						/* A text name, purely to help debugging. */
+								ledFLASH_RATE_BASE * ( xLEDTimer + 1 ),	/* The timer period, which is a multiple of ledFLASH_RATE_BASE. */
+								pdTRUE,							/* This is an auto-reload timer, so xAutoReload is set to pdTRUE. */
+								( void * ) xLEDTimer,			/* The ID is used to identify the timer within the timer callback function, as each timer uses the same callback. */
+								prvLEDTimerCallback				/* Each timer uses the same callback. */
+							  );
+				
+		/* If the timer was created successfully, attempt to start it.  If the
+		scheduler has not yet been started then the timer command queue must
+		be long enough to hold each command sent to it until such time that the
+		scheduler is started.  The timer command queue length is set by
+		configTIMER_QUEUE_LENGTH in FreeRTOSConfig.h. */
+		if( xTimer != NULL )
+		{
+			xTimerStart( xTimer, ledDONT_BLOCK );
+		}							  
+	}
+}
+/*-----------------------------------------------------------*/
+
+static void prvLEDTimerCallback( xTimerHandle xTimer )
+{
+portBASE_TYPE xTimerID;
+
+	/* The timer ID is used to identify the timer that has actually expired as
+	each timer uses the same callback.  The ID is then also used as the number
+	of the LED that is to be toggled. */
+	xTimerID = ( portBASE_TYPE ) pvTimerGetTimerID( xTimer );
+	vParTestToggleLED( xTimerID );
+}
+
+

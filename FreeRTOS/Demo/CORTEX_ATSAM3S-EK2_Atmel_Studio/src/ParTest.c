bf12aba59d0c7f37bf5243ed1406c5a552029144
Richard Barry <ribarry@amazon.com>
2012-08-29 15:02:56 +0000

    Add demo for SAM3S-EK2.

---
+++ FreeRTOS/Demo/CORTEX_ATSAM3S-EK2_Atmel_Studio/src/ParTest.c
@@ -0,0 +1,153 @@
+/*
+    FreeRTOS V7.2.0 - Copyright (C) 2012 Real Time Engineers Ltd.
+
+
+    ***************************************************************************
+     *                                                                       *
+     *    FreeRTOS tutorial books are available in pdf and paperback.        *
+     *    Complete, revised, and edited pdf reference manuals are also       *
+     *    available.                                                         *
+     *                                                                       *
+     *    Purchasing FreeRTOS documentation will not only help you, by       *
+     *    ensuring you get running as quickly as possible and with an        *
+     *    in-depth knowledge of how to use FreeRTOS, it will also help       *
+     *    the FreeRTOS project to continue with its mission of providing     *
+     *    professional grade, cross platform, de facto standard solutions    *
+     *    for microcontrollers - completely free of charge!                  *
+     *                                                                       *
+     *    >>> See http://www.FreeRTOS.org/Documentation for details. <<<     *
+     *                                                                       *
+     *    Thank you for using FreeRTOS, and thank you for your support!      *
+     *                                                                       *
+    ***************************************************************************
+
+
+    This file is part of the FreeRTOS distribution.
+
+    FreeRTOS is free software; you can redistribute it and/or modify it under
+    the terms of the GNU General Public License (version 2) as published by the
+    Free Software Foundation AND MODIFIED BY the FreeRTOS exception.
+    >>>NOTE<<< The modification to the GPL is included to allow you to
+    distribute a combined work that includes FreeRTOS without being obliged to
+    provide the source code for proprietary components outside of the FreeRTOS
+    kernel.  FreeRTOS is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+    more details. You should have received a copy of the GNU General Public
+    License and the FreeRTOS license exception along with FreeRTOS; if not it
+    can be viewed here: http://www.freertos.org/a00114.html and also obtained
+    by writing to Richard Barry, contact details for whom are available on the
+    FreeRTOS WEB site.
+
+    1 tab == 4 spaces!
+
+    ***************************************************************************
+     *                                                                       *
+     *    Having a problem?  Start by reading the FAQ "My application does   *
+     *    not run, what could be wrong?                                      *
+     *                                                                       *
+     *    http://www.FreeRTOS.org/FAQHelp.html                               *
+     *                                                                       *
+    ***************************************************************************
+
+
+    http://www.FreeRTOS.org - Documentation, training, latest information,
+    license and contact details.
+
+    http://www.FreeRTOS.org/plus - A selection of FreeRTOS ecosystem products,
+    including FreeRTOS+Trace - an indispensable productivity tool.
+
+    Real Time Engineers ltd license FreeRTOS to High Integrity Systems, who sell
+    the code with commercial support, indemnification, and middleware, under
+    the OpenRTOS brand: http://www.OpenRTOS.com.  High Integrity Systems also
+    provide a safety engineered and independently SIL3 certified version under
+    the SafeRTOS brand: http://www.SafeRTOS.com.
+*/
+
+/*-----------------------------------------------------------
+ * Simple IO routines to control the LEDs.
+ *-----------------------------------------------------------*/
+
+/* Scheduler includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+
+/* Demo includes. */
+#include "partest.h"
+
+/* Library includes. */
+#include <board.h>
+#include <gpio.h>
+
+/* The number of LEDs available to the user on the evaluation kit. */
+#define partestNUM_LEDS			( 3UL )
+
+/* One of the LEDs is wired in the inverse to the others as it is also used as
+the power LED. */
+#define partstsINVERTED_LED		( 0UL )
+
+/* The index of the pins to which the LEDs are connected.  The ordering of the
+LEDs in this array is intentional and matches the order they appear on the
+hardware. */
+static const uint32_t ulLED[] = { LED2_GPIO, LED0_GPIO, LED1_GPIO };
+
+/*-----------------------------------------------------------*/
+
+void vParTestInitialise( void )
+{
+unsigned long ul;
+
+	for( ul = 0; ul < partestNUM_LEDS; ul++ )
+	{
+		/* Configure the LED, before ensuring it starts in the off state. */
+		gpio_configure_pin( ulLED[ ul ],  ( PIO_OUTPUT_1 | PIO_DEFAULT ) );
+		vParTestSetLED( ul, pdFALSE );
+	}
+}
+/*-----------------------------------------------------------*/
+
+void vParTestSetLED( unsigned portBASE_TYPE uxLED, signed portBASE_TYPE xValue )
+{
+	if( uxLED < partestNUM_LEDS )
+	{
+		if( uxLED == partstsINVERTED_LED )
+		{
+			xValue = !xValue;
+		}
+
+		if( xValue != pdFALSE )
+		{
+			/* Turn the LED on. */
+			taskENTER_CRITICAL();
+			{
+				gpio_set_pin_low( ulLED[ uxLED ]);
+			}
+			taskEXIT_CRITICAL();
+		}
+		else
+		{
+			/* Turn the LED off. */
+			taskENTER_CRITICAL();
+			{
+				gpio_set_pin_high( ulLED[ uxLED ]);
+			}
+			taskEXIT_CRITICAL();
+		}
+	}
+}
+/*-----------------------------------------------------------*/
+
+void vParTestToggleLED( unsigned portBASE_TYPE uxLED )
+{
+	if( uxLED < partestNUM_LEDS )
+	{
+		taskENTER_CRITICAL();
+		{
+			gpio_toggle_pin( ulLED[ uxLED ] );
+		}
+		taskEXIT_CRITICAL();
+	}
+}
+
+
+
---
+++ FreeRTOS/Demo/CORTEX_ATSAM3S-EK2_Atmel_Studio/src/serial.c
@@ -0,0 +1,300 @@
+/*
+    FreeRTOS V7.2.0 - Copyright (C) 2012 Real Time Engineers Ltd.
+
+
+    ***************************************************************************
+     *                                                                       *
+     *    FreeRTOS tutorial books are available in pdf and paperback.        *
+     *    Complete, revised, and edited pdf reference manuals are also       *
+     *    available.                                                         *
+     *                                                                       *
+     *    Purchasing FreeRTOS documentation will not only help you, by       *
+     *    ensuring you get running as quickly as possible and with an        *
+     *    in-depth knowledge of how to use FreeRTOS, it will also help       *
+     *    the FreeRTOS project to continue with its mission of providing     *
+     *    professional grade, cross platform, de facto standard solutions    *
+     *    for microcontrollers - completely free of charge!                  *
+     *                                                                       *
+     *    >>> See http://www.FreeRTOS.org/Documentation for details. <<<     *
+     *                                                                       *
+     *    Thank you for using FreeRTOS, and thank you for your support!      *
+     *                                                                       *
+    ***************************************************************************
+
+
+    This file is part of the FreeRTOS distribution.
+
+    FreeRTOS is free software; you can redistribute it and/or modify it under
+    the terms of the GNU General Public License (version 2) as published by the
+    Free Software Foundation AND MODIFIED BY the FreeRTOS exception.
+    >>>NOTE<<< The modification to the GPL is included to allow you to
+    distribute a combined work that includes FreeRTOS without being obliged to
+    provide the source code for proprietary components outside of the FreeRTOS
+    kernel.  FreeRTOS is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+    more details. You should have received a copy of the GNU General Public
+    License and the FreeRTOS license exception along with FreeRTOS; if not it
+    can be viewed here: http://www.freertos.org/a00114.html and also obtained
+    by writing to Richard Barry, contact details for whom are available on the
+    FreeRTOS WEB site.
+
+    1 tab == 4 spaces!
+
+    ***************************************************************************
+     *                                                                       *
+     *    Having a problem?  Start by reading the FAQ "My application does   *
+     *    not run, what could be wrong?                                      *
+     *                                                                       *
+     *    http://www.FreeRTOS.org/FAQHelp.html                               *
+     *                                                                       *
+    ***************************************************************************
+
+
+    http://www.FreeRTOS.org - Documentation, training, latest information,
+    license and contact details.
+
+    http://www.FreeRTOS.org/plus - A selection of FreeRTOS ecosystem products,
+    including FreeRTOS+Trace - an indispensable productivity tool.
+
+    Real Time Engineers ltd license FreeRTOS to High Integrity Systems, who sell
+    the code with commercial support, indemnification, and middleware, under
+    the OpenRTOS brand: http://www.OpenRTOS.com.  High Integrity Systems also
+    provide a safety engineered and independently SIL3 certified version under
+    the SafeRTOS brand: http://www.SafeRTOS.com.
+*/
+
+/*
+	BASIC INTERRUPT DRIVEN SERIAL PORT DRIVER FOR USART1.
+
+	***Note*** This example uses queues to send each character into an interrupt
+	service routine and out of an interrupt service routine individually.  This
+	is done to demonstrate queues being used in an interrupt, and to deliberately
+	load the system to test the FreeRTOS port.  It is *NOT* meant to be an
+	example of an efficient implementation.  An efficient implementation should
+	use FIFO's or DMA if available, and only use FreeRTOS API functions when
+	enough has been received to warrant a task being unblocked to process the
+	data.
+*/
+
+/* Scheduler includes. */
+#include "FreeRTOS.h"
+#include "queue.h"
+#include "semphr.h"
+#include "comtest2.h"
+
+/* Library includes. */
+#include "asf.h"
+
+/* Demo application includes. */
+#include "demo_serial.h"
+/*-----------------------------------------------------------*/
+
+/* Misc defines. */
+#define serINVALID_QUEUE				( ( xQueueHandle ) 0 )
+#define serNO_BLOCK						( ( portTickType ) 0 )
+#define serPMC_USART_ID					( BOARD_ID_USART )
+
+/* The USART supported by this file. */
+#define serUSART_PORT					( USART1 )
+#define serUSART_IRQ					( USART1_IRQn )
+
+/* Every bit in the interrupt mask. */
+#define serMASK_ALL_INTERRUPTS			( 0xffffffffUL )
+
+/*-----------------------------------------------------------*/
+
+/* The queue used to hold received characters. */
+static xQueueHandle xRxedChars;
+static xQueueHandle xCharsForTx;
+
+/*-----------------------------------------------------------*/
+
+
+/*
+ * See the serial.h header file.
+ */
+xComPortHandle xSerialPortInitMinimal( unsigned long ulWantedBaud, unsigned portBASE_TYPE uxQueueLength )
+{
+uint32_t ulChar;
+xComPortHandle xReturn;
+const sam_usart_opt_t xUSARTSettings =
+{
+	ulWantedBaud,
+	US_MR_CHRL_8_BIT,
+	US_MR_PAR_NO,
+	US_MR_NBSTOP_1_BIT,
+	US_MR_CHMODE_NORMAL,
+	0 /* Only used in IrDA mode. */
+};
+
+	/* Create the queues used to hold Rx/Tx characters. */
+	xRxedChars = xQueueCreate( uxQueueLength, ( unsigned portBASE_TYPE ) sizeof( signed char ) );
+	xCharsForTx = xQueueCreate( uxQueueLength + 1, ( unsigned portBASE_TYPE ) sizeof( signed char ) );
+
+	/* If the queues were created correctly then setup the serial port
+	hardware. */
+	if( ( xRxedChars != serINVALID_QUEUE ) && ( xCharsForTx != serINVALID_QUEUE ) )
+	{
+		/* Enable the peripheral clock in the PMC. */
+		pmc_enable_periph_clk( serPMC_USART_ID );
+
+		/* Configure USART in serial mode. */
+		usart_init_rs232( serUSART_PORT, &xUSARTSettings, sysclk_get_cpu_hz() );
+
+		/* Disable all the interrupts. */
+		usart_disable_interrupt( serUSART_PORT, serMASK_ALL_INTERRUPTS );
+
+		/* Enable the receiver and transmitter. */
+		usart_enable_tx( serUSART_PORT );
+		usart_enable_rx( serUSART_PORT );
+
+		/* Clear any characters before enabling interrupt. */
+		usart_getchar( serUSART_PORT, &ulChar );
+
+		/* Enable Rx end interrupt. */
+		usart_enable_interrupt( serUSART_PORT, US_IER_RXRDY );
+
+		/* Configure and enable interrupt of USART. */
+		NVIC_SetPriority( serUSART_IRQ, configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY );
+		NVIC_EnableIRQ( serUSART_IRQ );
+	}
+	else
+	{
+		xReturn = ( xComPortHandle ) 0;
+	}
+
+	/* This demo file only supports a single port but we have to return
+	something to comply with the standard demo header file. */
+	return xReturn;
+}
+/*-----------------------------------------------------------*/
+
+signed portBASE_TYPE xSerialGetChar( xComPortHandle pxPort, signed char *pcRxedChar, portTickType xBlockTime )
+{
+	/* The port handle is not required as this driver only supports one port. */
+	( void ) pxPort;
+
+	/* Get the next character from the buffer.  Return false if no characters
+	are available, or arrive before xBlockTime expires. */
+	if( xQueueReceive( xRxedChars, pcRxedChar, xBlockTime ) )
+	{
+		return pdTRUE;
+	}
+	else
+	{
+		return pdFALSE;
+	}
+}
+/*-----------------------------------------------------------*/
+
+void vSerialPutString( xComPortHandle pxPort, const signed char * const pcString, unsigned short usStringLength )
+{
+signed char *pxNext;
+
+	/* A couple of parameters that this port does not use. */
+	( void ) usStringLength;
+	( void ) pxPort;
+
+	/* NOTE: This implementation does not handle the queue being full as no
+	block time is used! */
+
+	/* The port handle is not required as this driver only supports USART1. */
+	( void ) pxPort;
+
+	/* Send each character in the string, one at a time. */
+	pxNext = ( signed char * ) pcString;
+	while( *pxNext )
+	{
+		xSerialPutChar( pxPort, *pxNext, serNO_BLOCK );
+		pxNext++;
+	}
+}
+/*-----------------------------------------------------------*/
+
+signed portBASE_TYPE xSerialPutChar( xComPortHandle pxPort, signed char cOutChar, portTickType xBlockTime )
+{
+signed portBASE_TYPE xReturn;
+
+	/* This simple example only supports one port. */
+	( void ) pxPort;
+
+	if( xQueueSend( xCharsForTx, &cOutChar, xBlockTime ) == pdPASS )
+	{
+		xReturn = pdPASS;
+		usart_enable_interrupt( serUSART_PORT, US_IER_TXRDY );
+	}
+	else
+	{
+		xReturn = pdFAIL;
+	}
+
+	return xReturn;
+}
+/*-----------------------------------------------------------*/
+
+void vSerialClose( xComPortHandle xPort )
+{
+	/* Not supported as not required by the demo application. */
+	( void ) xPort;
+}
+/*-----------------------------------------------------------*/
+
+/*
+ * It should be noted that the com test tasks (which use make use of this file)
+ * are included to demonstrate queues being used to communicate between tasks
+ * and interrupts, and to demonstrate a context switch being performed from
+ * inside an interrupt service routine.  The serial driver used here is *not*
+ * intended to represent an efficient implementation.  Real applications should
+ * make use of the USARTS peripheral DMA channel (PDC).
+ */
+void USART1_Handler( void )
+{
+portBASE_TYPE xHigherPriorityTaskWoken = pdFALSE;
+uint8_t ucChar;
+uint32_t ulChar;
+uint32_t ulUSARTStatus, ulUSARTMask;
+
+	ulUSARTStatus = usart_get_status( serUSART_PORT );
+	ulUSARTMask = usart_get_interrupt_mask( serUSART_PORT );
+	ulUSARTStatus &= ulUSARTMask;
+
+	if( ( ulUSARTStatus & US_CSR_TXRDY ) != 0UL )
+	{
+		/* The interrupt was caused by the TX register becoming empty.  Are
+		there any more characters to transmit? */
+		if( xQueueReceiveFromISR( xCharsForTx, &ucChar, &xHigherPriorityTaskWoken ) == pdTRUE )
+		{
+			/* A character was retrieved from the queue so can be sent to the
+			USART now. */
+			usart_putchar( serUSART_PORT, ( uint32_t ) ucChar );
+		}
+		else
+		{
+			usart_disable_interrupt( serUSART_PORT, US_IER_TXRDY );
+		}
+	}
+
+	if( ( ulUSARTStatus & US_CSR_RXRDY ) != 0UL )
+	{
+		/* A character has been received on the USART, send it to the Rx
+		handler task. */
+		usart_getchar( serUSART_PORT, &ulChar );
+		ucChar = ( uint8_t ) ( ulChar & 0xffUL );
+		xQueueSendFromISR( xRxedChars, &ucChar, &xHigherPriorityTaskWoken );
+	}
+
+	/* If sending or receiving from a queue has caused a task to unblock, and
+	the unblocked task has a priority equal to or higher than the currently
+	running task (the task this ISR interrupted), then xHigherPriorityTaskWoken
+	will have automatically been set to pdTRUE within the queue send or receive
+	function.  portEND_SWITCHING_ISR() will then ensure that this ISR returns
+	directly to the higher priority unblocked task. */
+	portEND_SWITCHING_ISR( xHigherPriorityTaskWoken );
+}
+
+
+
+
+
+

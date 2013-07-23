f522d6a2da73682ec532e855b66f5afd5b1d0b76
Richard Barry <ribarry@amazon.com>
2013-07-23 09:36:30 +0000

    Remove duplicate definition from FreeRTOSConfig.h.

--- FreeRTOS/Demo/WIN32-MSVC/FreeRTOSConfig.h
+++ FreeRTOS/Demo/WIN32-MSVC/FreeRTOSConfig.h
@@ -102,8 +102,6 @@
 #define configTIMER_TASK_STACK_DEPTH	( configMINIMAL_STACK_SIZE * 2 )
 
 #define configMAX_PRIORITIES			( 7 )
-#define configMAX_CO_ROUTINE_PRIORITIES ( 2 )
-
 
 /* Run time stats gathering definitions. */
 unsigned long ulGetRunTimeCounterValue( void );

2ec97696db531caff2b15d475be20554d1bc8c48
Richard Barry <ribarry@amazon.com>
2016-04-25 13:37:57 +0000

    Remove Zynq 7000 project so it can be re-created using the 2016.1 edition of the SDK.

--- FreeRTOS/Demo/CORTEX_A9_Zynq_ZC702/RTOSDemo/src/platform_config.h
+++
@@ -1,6 +0,0 @@
-#ifndef __PLATFORM_CONFIG_H_
-#define __PLATFORM_CONFIG_H_
-
-#define STDOUT_IS_PS7_UART
-#define UART_DEVICE_ID 0
-#endif
--- FreeRTOS/Demo/CORTEX_A9_Zynq_ZC702/RTOSDemo_bsp/ps7_cortexa9_0/libsrc/standalone_v4_1/src/config.make
+++
@@ -1,3 +0,0 @@
-LIBSOURCES = *.c *.s *.S
-PROFILE_ARCH_OBJS = profile_mcount_arm.o
-LIBS = standalone_libs

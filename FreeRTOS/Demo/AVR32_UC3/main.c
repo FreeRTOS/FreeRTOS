3a1631fda3c26fe96bd8756b127ffb322806b4f8
Richard Barry <ribarry@amazon.com>
2018-08-22 23:23:03 +0000

    Update copyright date ready for tagging V10.1.0.

--- FreeRTOS/Demo/AVR32_UC3/main.c
+++ FreeRTOS/Demo/AVR32_UC3/main.c
@@ -31,7 +31,7 @@
 
 /*
  * FreeRTOS Kernel V10.1.0
- * Copyright (C) 2017 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ * Copyright (C) 2018 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in

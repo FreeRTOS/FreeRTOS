d0ef322b13071242d70021c8b634bacde7d15653
Richard Barry <ribarry@amazon.com>
2018-11-24 04:42:20 +0000

    Add kernel code to the RISC-V-Qemu-sifive_e-FreedomStudio demo.

--- FreeRTOS/Demo/RISC-V-Qemu-sifive_e-FreedomStudio/main.c
+++ FreeRTOS/Demo/RISC-V-Qemu-sifive_e-FreedomStudio/main.c
@@ -1,5 +1,8 @@
 // See LICENSE for license details.
 
+#include <FreeRTOS.h>
+#include <task.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "platform.h"
@@ -8,6 +11,24 @@
 #include "encoding.h"
 #include <unistd.h>
 #include "stdatomic.h"
+
+
+
+/*
+ * FreeRTOS hook for when malloc fails, enable in FreeRTOSConfig.
+ */
+void vApplicationMallocFailedHook( void );
+
+/*
+ * FreeRTOS hook for when FreeRtos is idling, enable in FreeRTOSConfig.
+ */
+void vApplicationIdleHook( void );
+
+/*
+ * FreeRTOS hook for when a stack overflow occurs, enable in FreeRTOSConfig.
+ */
+void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName );
+
 
 void reset_demo (void);
 
@@ -251,8 +272,55 @@ int main(int argc, char **argv)
 
 }
 
+void vApplicationMallocFailedHook( void )
+{
+	/* vApplicationMallocFailedHook() will only be called if
+	configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
+	function that will get called if a call to pvPortMalloc() fails.
+	pvPortMalloc() is called internally by the kernel whenever a task, queue,
+	timer or semaphore is created.  It is also called by various parts of the
+	demo application.  If heap_1.c or heap_2.c are used, then the size of the
+	heap available to pvPortMalloc() is defined by configTOTAL_HEAP_SIZE in
+	FreeRTOSConfig.h, and the xPortGetFreeHeapSize() API function can be used
+	to query the size of free heap space that remains (although it does not
+	provide information on how the remaining heap might be fragmented). */
+	taskDISABLE_INTERRUPTS();
+	for( ;; );
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationIdleHook( void )
+{
+	/* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
+	to 1 in FreeRTOSConfig.h.  It will be called on each iteration of the idle
+	task.  It is essential that code added to this hook function never attempts
+	to block in any way (for example, call xQueueReceive() with a block time
+	specified, or call vTaskDelay()).  If the application makes use of the
+	vTaskDelete() API function (as this demo application does) then it is also
+	important that vApplicationIdleHook() is permitted to return to its calling
+	function, because it is the responsibility of the idle task to clean up
+	memory allocated by the kernel to any task that has since been deleted. */
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName )
+{
+	( void ) pcTaskName;
+	( void ) pxTask;
+
+	/* Run time stack overflow checking is performed if
+	configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
+	function is called if a stack overflow is detected. */
+	taskDISABLE_INTERRUPTS();
+	for( ;; );
+}
+/*-----------------------------------------------------------*/
+
+
 
 void trap_entry( void )
 {
 #warning Dummy until kernel code is incldued.
 }
+
+

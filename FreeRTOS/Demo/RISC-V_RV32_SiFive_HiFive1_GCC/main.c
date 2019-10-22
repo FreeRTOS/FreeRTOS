fccc445865cbda35dd70ae354171bcfd34e86096
Richard Barry <ribarry@amazon.com>
2019-10-22 02:17:15 +0000

    Change version and license text in RISC-V_RV32_SiFive_HiFive1_GCC FreeRTOSConfig.h file.

--- FreeRTOS/Demo/RISC-V_RV32_SiFive_HiFive1_GCC/main.c
+++ FreeRTOS/Demo/RISC-V_RV32_SiFive_HiFive1_GCC/main.c
@@ -58,7 +58,7 @@
 
 /* Set mainCREATE_SIMPLE_BLINKY_DEMO_ONLY to one to run the simple blinky demo,
 or 0 to run the more comprehensive test and demo application. */
-#define mainCREATE_SIMPLE_BLINKY_DEMO_ONLY	0
+#define mainCREATE_SIMPLE_BLINKY_DEMO_ONLY	1
 
 /* Index to first HART (there is only one). */
 #define mainHART_0 		0

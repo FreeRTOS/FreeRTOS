cc673eb6a522166dad594ee6ba88eef63932ce37
Richard Barry <ribarry@amazon.com>
2019-12-30 22:07:33 +0000

    Ensure the CORTEX_M0_STM32F0518_IAR demo builds after updates to the Cortex-M0 port layer - required an update to the project settings to allow IAR extensions as the port layer now uses the _weak qualifier.

--- FreeRTOS/Demo/CORTEX_M0_STM32F0518_IAR/startup_stm32f0xx.s
+++ FreeRTOS/Demo/CORTEX_M0_STM32F0518_IAR/startup_stm32f0xx.s
@@ -7,7 +7,7 @@
 ;*                      This module performs:
 ;*                      - Set the initial SP
 ;*                      - Set the initial PC == __iar_program_start,
-;*                      - Set the vector table entries with the exceptions ISR 
+;*                      - Set the vector table entries with the exceptions ISR
 ;*                        address.
 ;*                      After Reset the Cortex-M0 processor is in Thread mode,
 ;*                      priority is Privileged, and the Stack is set to Main.
@@ -46,7 +46,7 @@
         SECTION .intvec:CODE:NOROOT(2)
 
         EXTERN  __iar_program_start
-        EXTERN  SystemInit        
+        EXTERN  SystemInit
         PUBLIC  __vector_table
 
         DATA
@@ -82,7 +82,7 @@ __vector_table
         DCD     DMA1_Channel1_IRQHandler       ; DMA1 Channel 1
         DCD     DMA1_Channel2_3_IRQHandler     ; DMA1 Channel 2 and Channel 3
         DCD     DMA1_Channel4_5_IRQHandler     ; DMA1 Channel 4 and Channel 5
-        DCD     ADC1_COMP_IRQHandler           ; ADC1, COMP1 and COMP2 
+        DCD     ADC1_COMP_IRQHandler           ; ADC1, COMP1 and COMP2
         DCD     TIM1_BRK_UP_TRG_COM_IRQHandler ; TIM1 Break, Update, Trigger and Commutation
         DCD     TIM1_CC_IRQHandler             ; TIM1 Capture Compare
         DCD     TIM2_IRQHandler                ; TIM2
@@ -102,7 +102,7 @@ __vector_table
         DCD     0                              ; Reserved
         DCD     CEC_IRQHandler                 ; CEC
         DCD     0                              ; Reserved
-        
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;; Default interrupt handlers.
@@ -120,7 +120,7 @@ __vector_table
 
         THUMB
 
-        PUBWEAK Reset_Handler
+        PUBLIC Reset_Handler
         SECTION .text:CODE:REORDER(2)
 Reset_Handler
 
@@ -140,209 +140,209 @@ Reset_Handler
         LDR     R0, =__iar_program_start
         BX      R0
 
-        
+
         PUBWEAK NMI_Handler
         SECTION .text:CODE:NOROOT:REORDER(1)
 NMI_Handler
         B NMI_Handler
-        
-        
+
+
         PUBWEAK HardFault_Handler
         SECTION .text:CODE:NOROOT:REORDER(1)
 HardFault_Handler
         B HardFault_Handler
-       
-        
+
+
         PUBWEAK SVC_Handler
         SECTION .text:CODE:NOROOT:REORDER(1)
 SVC_Handler
         B SVC_Handler
-       
-        
+
+
         PUBWEAK PendSV_Handler
         SECTION .text:CODE:NOROOT:REORDER(1)
 PendSV_Handler
         B PendSV_Handler
-        
-        
+
+
         PUBWEAK SysTick_Handler
         SECTION .text:CODE:NOROOT:REORDER(1)
 SysTick_Handler
         B SysTick_Handler
-        
-        
+
+
         PUBWEAK WWDG_IRQHandler
         SECTION .text:CODE:NOROOT:REORDER(1)
 WWDG_IRQHandler
         B WWDG_IRQHandler
-        
-                
+
+
         PUBWEAK PVD_IRQHandler
         SECTION .text:CODE:NOROOT:REORDER(1)
 PVD_IRQHandler
         B PVD_IRQHandler
-        
-                
+
+
         PUBWEAK RTC_IRQHandler
         SECTION .text:CODE:NOROOT:REORDER(1)
 RTC_IRQHandler
         B RTC_IRQHandler
-        
-                
+
+
         PUBWEAK FLASH_IRQHandler
         SECTION .text:CODE:NOROOT:REORDER(1)
 FLASH_IRQHandler
         B FLASH_IRQHandler
-        
-                
+
+
         PUBWEAK RCC_IRQHandler
         SECTION .text:CODE:NOROOT:REORDER(1)
 RCC_IRQHandler
         B RCC_IRQHandler
-        
-                
+
+
         PUBWEAK EXTI0_1_IRQHandler
         SECTION .text:CODE:NOROOT:REORDER(1)
 EXTI0_1_IRQHandler
         B EXTI0_1_IRQHandler
-        
-                
+
+
         PUBWEAK EXTI2_3_IRQHandler
         SECTION .text:CODE:NOROOT:REORDER(1)
 EXTI2_3_IRQHandler
         B EXTI2_3_IRQHandler
-        
-                
+
+
         PUBWEAK EXTI4_15_IRQHandler
         SECTION .text:CODE:NOROOT:REORDER(1)
 EXTI4_15_IRQHandler
         B EXTI4_15_IRQHandler
-        
-                
+
+
         PUBWEAK TS_IRQHandler
         SECTION .text:CODE:NOROOT:REORDER(1)
 TS_IRQHandler
         B TS_IRQHandler
-        
-                
+
+
         PUBWEAK DMA1_Channel1_IRQHandler
         SECTION .text:CODE:NOROOT:REORDER(1)
 DMA1_Channel1_IRQHandler
         B DMA1_Channel1_IRQHandler
-        
-                
+
+
         PUBWEAK DMA1_Channel2_3_IRQHandler
         SECTION .text:CODE:NOROOT:REORDER(1)
 DMA1_Channel2_3_IRQHandler
         B DMA1_Channel2_3_IRQHandler
-        
-                
+
+
         PUBWEAK DMA1_Channel4_5_IRQHandler
         SECTION .text:CODE:NOROOT:REORDER(1)
 DMA1_Channel4_5_IRQHandler
         B DMA1_Channel4_5_IRQHandler
-        
-                
+
+
         PUBWEAK ADC1_COMP_IRQHandler
         SECTION .text:CODE:NOROOT:REORDER(1)
 ADC1_COMP_IRQHandler
         B ADC1_COMP_IRQHandler
-        
-                 
+
+
         PUBWEAK TIM1_BRK_UP_TRG_COM_IRQHandler
         SECTION .text:CODE:NOROOT:REORDER(1)
 TIM1_BRK_UP_TRG_COM_IRQHandler
         B TIM1_BRK_UP_TRG_COM_IRQHandler
-        
-                
+
+
         PUBWEAK TIM1_CC_IRQHandler
         SECTION .text:CODE:NOROOT:REORDER(1)
 TIM1_CC_IRQHandler
         B TIM1_CC_IRQHandler
-        
-                
+
+
         PUBWEAK TIM2_IRQHandler
         SECTION .text:CODE:NOROOT:REORDER(1)
 TIM2_IRQHandler
         B TIM2_IRQHandler
-        
-                
+
+
         PUBWEAK TIM3_IRQHandler
         SECTION .text:CODE:NOROOT:REORDER(1)
 TIM3_IRQHandler
         B TIM3_IRQHandler
-        
-                
+
+
         PUBWEAK TIM6_DAC_IRQHandler
         SECTION .text:CODE:NOROOT:REORDER(1)
 TIM6_DAC_IRQHandler
         B TIM6_DAC_IRQHandler
-        
-                
+
+
         PUBWEAK TIM14_IRQHandler
         SECTION .text:CODE:NOROOT:REORDER(1)
 TIM14_IRQHandler
         B TIM14_IRQHandler
-        
-                
+
+
         PUBWEAK TIM15_IRQHandler
         SECTION .text:CODE:NOROOT:REORDER(1)
 TIM15_IRQHandler
         B TIM15_IRQHandler
-        
-                
+
+
         PUBWEAK TIM16_IRQHandler
         SECTION .text:CODE:NOROOT:REORDER(1)
 TIM16_IRQHandler
         B TIM16_IRQHandler
-        
-                
+
+
         PUBWEAK TIM17_IRQHandler
         SECTION .text:CODE:NOROOT:REORDER(1)
 TIM17_IRQHandler
         B TIM17_IRQHandler
-        
-                
+
+
         PUBWEAK I2C1_IRQHandler
         SECTION .text:CODE:NOROOT:REORDER(1)
 I2C1_IRQHandler
         B I2C1_IRQHandler
-        
-                
+
+
         PUBWEAK I2C2_IRQHandler
         SECTION .text:CODE:NOROOT:REORDER(1)
 I2C2_IRQHandler
         B I2C2_IRQHandler
-        
-                
+
+
         PUBWEAK SPI1_IRQHandler
         SECTION .text:CODE:NOROOT:REORDER(1)
 SPI1_IRQHandler
         B SPI1_IRQHandler
-        
-                
+
+
         PUBWEAK SPI2_IRQHandler
         SECTION .text:CODE:NOROOT:REORDER(1)
 SPI2_IRQHandler
         B SPI2_IRQHandler
-        
-                
+
+
         PUBWEAK USART1_IRQHandler
         SECTION .text:CODE:NOROOT:REORDER(1)
 USART1_IRQHandler
         B USART1_IRQHandler
-        
-                
+
+
         PUBWEAK USART2_IRQHandler
         SECTION .text:CODE:NOROOT:REORDER(1)
 USART2_IRQHandler
         B USART2_IRQHandler
-        
-                
+
+
         PUBWEAK CEC_IRQHandler
         SECTION .text:CODE:NOROOT:REORDER(1)
 CEC_IRQHandler
-        B CEC_IRQHandler                        
+        B CEC_IRQHandler
 
         END
 /******************* (C) COPYRIGHT 2012 STMicroelectronics *****END OF FILE****/

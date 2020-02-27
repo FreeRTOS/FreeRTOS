36d7eceb5ff020c5861e7eca609025ddfeb23929
Yuhui Zheng <10982575+yuhui-zheng@users.noreply.github.com>
2020-02-26 17:56:14 -0800

    We really don't need to have dependency on TCP in FreeRTOS Tasks related proofs.

--- FreeRTOS/Test/CBMC/proofs/TaskPool/TaskSetTimeOutState/TaskSetTimeOutState_harness.c
+++ FreeRTOS/Test/CBMC/proofs/TaskPool/TaskSetTimeOutState/TaskSetTimeOutState_harness.c
@@ -32,10 +32,6 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-/* FreeRTOS+TCP includes. */
-#include "FreeRTOS_IP.h"
-#include "FreeRTOS_IP_Private.h"
-
 /*
  * We assume xTime to allocated since the function includes a configASSERT
  * call at the beginning to make sure the pointer to it is not NULL
--- FreeRTOS/Test/CBMC/proofs/TaskPool/TaskSuspendAll/TaskSuspendAll_harness.c
+++ FreeRTOS/Test/CBMC/proofs/TaskPool/TaskSuspendAll/TaskSuspendAll_harness.c
@@ -32,10 +32,6 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-/* FreeRTOS+TCP includes. */
-#include "FreeRTOS_IP.h"
-#include "FreeRTOS_IP_Private.h"
-
 /*
  * We just call vTaskSuspendAll(). No assumption
  * or abstraction is required for this proof

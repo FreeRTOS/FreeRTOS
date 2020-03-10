bd435da6a93f7a9b056fabea91a72157305d9684
AniruddhaKanhere <60444055+AniruddhaKanhere@users.noreply.github.com>
2020-03-10 12:52:16 -0700

    Sync up

--- FreeRTOS-Plus/Source/FreeRTOS-Plus-TCP/portable/NetworkInterface/RX/NetworkInterface.c
+++ FreeRTOS-Plus/Source/FreeRTOS-Plus-TCP/portable/NetworkInterface/RX/NetworkInterface.c
@@ -305,7 +305,15 @@ static void prvEMACDeferredInterruptHandlerTask( void * pvParameters )
         if( xBytesReceived < 0 )
         {
             /* This is an error. Logged. */
-            FreeRTOS_printf( ( "R_ETHER_Read_ZC2: rc = %d\n", xBytesReceived ) );
+        	if( xBytesReceived == ETHER_ERR_LINK )
+        	{
+				/* Auto-negotiation is not completed, and transmission/
+				reception is not enabled. Will be logged elsewhere. */
+        	}
+        	else
+         	{
+        		FreeRTOS_printf( ( "R_ETHER_Read_ZC2: rc = %d not %d\n", xBytesReceived, ETHER_ERR_LINK ) );
+        	}
         }
         else if( xBytesReceived > 0 )
         {
@@ -452,7 +460,6 @@ void prvLinkStatusChange( BaseType_t xStatus )
 {
     if( xReportedStatus != xStatus )
     {
-        FreeRTOS_printf( ( "prvLinkStatusChange( %d )\n", xStatus ) );
         xReportedStatus = xStatus;
     }
 }

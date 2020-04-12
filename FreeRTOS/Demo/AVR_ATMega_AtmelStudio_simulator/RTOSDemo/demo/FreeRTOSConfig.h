e42eaefd2c091ffe2777329cf45e71264f98cbb9
Yuhui Zheng <10982575+yuhui-zheng@users.noreply.github.com>
2020-04-12 16:36:35 -0700

    Increasing stack size, as with optimization disabled, and stack overflow enabled (2) I'm observing: - at minimum size 85, timer task overflows. - at minimum size 90, timer task runs somewhat fine but idle task overflows.
    increase to 120 just to get things going. As simulator runs slow, tune this number on hardware or calculate from code.

--- FreeRTOS/Demo/AVR_ATMega_AtmelStudio_simulator/RTOSDemo/demo/FreeRTOSConfig.h
+++ FreeRTOS/Demo/AVR_ATMega_AtmelStudio_simulator/RTOSDemo/demo/FreeRTOSConfig.h
@@ -71,7 +71,7 @@
 #define configSUPPORT_STATIC_ALLOCATION		1
 
 /* FreeRTOS memory management. */
-#define configMINIMAL_STACK_SIZE	( ( unsigned short ) 85 )
+#define configMINIMAL_STACK_SIZE	( ( unsigned short ) 120 )
 #define configTOTAL_HEAP_SIZE		( (size_t ) ( 1500 ) )
 
 #define configCHECK_FOR_STACK_OVERFLOW		2

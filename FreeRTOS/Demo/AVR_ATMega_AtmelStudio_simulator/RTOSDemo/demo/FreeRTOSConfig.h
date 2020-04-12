e2204c27d1cd7d9120cd9dbc087f2e559b82d058
Yuhui Zheng <10982575+yuhui-zheng@users.noreply.github.com>
2020-04-12 15:46:12 -0700

    Somewhat workable FreeRTOSConfig.h.
    Note that "<util/delay.h> warning is triggered -- Severity	Code	Description	Project	File	Line
    Warning		#warning "Compiler optimizations disabled; functions from <util/delay.h> won't work as designed" [-Wcpp]	RTOSDemo	c:\program files (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h	112
    "

--- FreeRTOS/Demo/AVR_ATMega_AtmelStudio_simulator/RTOSDemo/demo/FreeRTOSConfig.h
+++ FreeRTOS/Demo/AVR_ATMega_AtmelStudio_simulator/RTOSDemo/demo/FreeRTOSConfig.h
@@ -39,24 +39,42 @@
  *
  * See http://www.freertos.org/a00110.html
  *----------------------------------------------------------*/
-
-#define configUSE_PREEMPTION		1
-#define configUSE_IDLE_HOOK			1
-#define configUSE_TICK_HOOK			0
+/* Clock setting. */
 #define configCPU_CLOCK_HZ			( ( unsigned long ) 8000000 )
 #define configTICK_RATE_HZ			( ( TickType_t ) 1000 )
+
+/* FreeRTOS kernel tick data width. */
+#define configUSE_16_BIT_TICKS			1
+
+/* FreeRTOS task configuration. */
 #define configMAX_PRIORITIES		( 4 )
+#define configMAX_TASK_NAME_LEN		( 8 )
+
+#define configUSE_PREEMPTION		1
+#define configIDLE_SHOULD_YIELD		1
+
+#define configUSE_IDLE_HOOK			1
+#define configUSE_TICK_HOOK			0
+
+/* FreeRTOS debugging and tracing. */
+#define configQUEUE_REGISTRY_SIZE	0
+#define configUSE_TRACE_FACILITY	0
+
+/* FreeRTOS software timer. */
+#define configUSE_TIMERS				1
+#define configTIMER_TASK_PRIORITY		2
+#define configTIMER_QUEUE_LENGTH		5 
+#define configTIMER_TASK_STACK_DEPTH	configMINIMAL_STACK_SIZE
+
+/* FreeRTOS memory allocation scheme. */
+#define configSUPPORT_DYNAMIC_ALLOCATION	1
+#define configSUPPORT_STATIC_ALLOCATION		1
+
+/* FreeRTOS memory management. */
 #define configMINIMAL_STACK_SIZE	( ( unsigned short ) 85 )
 #define configTOTAL_HEAP_SIZE		( (size_t ) ( 1500 ) )
-#define configMAX_TASK_NAME_LEN		( 8 )
-#define configUSE_TRACE_FACILITY	0
-#define configUSE_16_BIT_TICKS		1
-#define configIDLE_SHOULD_YIELD		1
-#define configQUEUE_REGISTRY_SIZE	0
 
-/* FreeRTOS Timer. */
-#define configUSE_TIMER				1
-#define configTIMER_TASK_PRIORITY	2
+#define configCHECK_FOR_STACK_OVERFLOW		2
 
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */

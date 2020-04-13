fcca1b71b6e0303ad5905b3fc5b375daa1f7987a
Yuhui Zheng <10982575+yuhui-zheng@users.noreply.github.com>
2020-04-12 23:40:58 -0700

    Demo main.c update.
    - Decrease mainCHECK_PERIOD, since simulator is running slow.
    - Add register test and check.
    - stack overflow hook.
    - vApplicationGetIdleTaskMemory and vApplicationGetTimerTaskMemory to support static allocaiton.

--- FreeRTOS/Demo/AVR_ATMega_AtmelStudio_simulator/RTOSDemo/main.c
+++ FreeRTOS/Demo/AVR_ATMega_AtmelStudio_simulator/RTOSDemo/main.c
@@ -31,23 +31,26 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
+/* Tests. */
+#include "regtest.h"
+
 /* Priority definitions for most of the tasks in the demo application. */
 #define mainCHECK_TASK_PRIORITY			( tskIDLE_PRIORITY + 3 )
 
 /* The period between executions of the check task. */
-#define mainCHECK_PERIOD				( ( TickType_t ) 3000 / portTICK_PERIOD_MS  )
+#define mainCHECK_PERIOD				( ( TickType_t ) 100 / portTICK_PERIOD_MS  )
 
 /* The task function for the "Check" task. */
 static void vErrorChecks( void *pvParameters );
-
-/* The idle hook is used. */
-void vApplicationIdleHook( void );
 
 int main(void)
 {
 	/* Initializes MCU, drivers and middleware.
 	   This is generated from Atmel START project. */
 	atmel_start_init();
+	
+	/* Standard tests. */
+	vStartRegTestTasks();
 
 	/* Create the tasks defined within this file. */
 	xTaskCreate( vErrorChecks, "Check", configMINIMAL_STACK_SIZE, NULL, mainCHECK_TASK_PRIORITY, NULL );
@@ -62,6 +65,8 @@ int main(void)
 
 static void vErrorChecks( void *pvParameters )
 {
+static UBaseType_t uxErrorHasOccurred = 0;
+
 	/* The parameters are not used. */
 	( void ) pvParameters;
 
@@ -69,6 +74,11 @@ static void vErrorChecks( void *pvParameters )
 	operating without error. */
 	for( ;; )
 	{
+		if( xAreRegTestTasksStillRunning() != pdTRUE )
+		{
+			uxErrorHasOccurred |= 0x0U;
+		}
+		
 		vTaskDelay( mainCHECK_PERIOD );
 	}
 }
@@ -77,5 +87,73 @@ static void vErrorChecks( void *pvParameters )
 
 void vApplicationIdleHook( void )
 {
+	/* Doesn't do anything yet. */
 }
 
+/*-----------------------------------------------------------*/
+
+void vApplicationStackOverflowHook( TaskHandle_t xTask, signed char *pcTaskName )
+{
+	/* When stack overflow happens, trap instead of attempting to recover. 
+	Read input arguments to learn about the offending task. */
+	for( ;; )
+	{
+		/* Doesn't do anything yet. */
+	}
+}
+
+/*-----------------------------------------------------------*/
+
+/* configSUPPORT_STATIC_ALLOCATION is set to 1, so the application must provide an
+implementation of vApplicationGetIdleTaskMemory() to provide the memory that is
+used by the Idle task. */
+void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer,
+                                    StackType_t **ppxIdleTaskStackBuffer,
+                                    uint32_t *pulIdleTaskStackSize )
+{
+/* If the buffers to be provided to the Idle task are declared inside this
+function then they must be declared static – otherwise they will be allocated on
+the stack and so not exists after this function exits. */
+static StaticTask_t xIdleTaskTCB;
+static StackType_t uxIdleTaskStack[ configMINIMAL_STACK_SIZE ];
+
+    /* Pass out a pointer to the StaticTask_t structure in which the Idle task’s
+    state will be stored. */
+    *ppxIdleTaskTCBBuffer = &xIdleTaskTCB;
+
+    /* Pass out the array that will be used as the Idle task’s stack. */
+    *ppxIdleTaskStackBuffer = uxIdleTaskStack;
+
+    /* Pass out the size of the array pointed to by *ppxIdleTaskStackBuffer.
+    Note that, as the array is necessarily of type StackType_t,
+    configMINIMAL_STACK_SIZE is specified in words, not bytes. */
+    *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
+}
+/*-----------------------------------------------------------*/
+
+/* configSUPPORT_STATIC_ALLOCATION and configUSE_TIMERS are both set to 1, so the
+application must provide an implementation of vApplicationGetTimerTaskMemory()
+to provide the memory that is used by the Timer service task. */
+void vApplicationGetTimerTaskMemory( StaticTask_t **ppxTimerTaskTCBBuffer,
+                                     StackType_t **ppxTimerTaskStackBuffer,
+                                     uint32_t *pulTimerTaskStackSize )
+{
+/* If the buffers to be provided to the Timer task are declared inside this
+function then they must be declared static – otherwise they will be allocated on
+the stack and so not exists after this function exits. */
+static StaticTask_t xTimerTaskTCB;
+static StackType_t uxTimerTaskStack[ configTIMER_TASK_STACK_DEPTH ];
+
+    /* Pass out a pointer to the StaticTask_t structure in which the Timer
+    task’s state will be stored. */
+    *ppxTimerTaskTCBBuffer = &xTimerTaskTCB;
+
+    /* Pass out the array that will be used as the Timer task’s stack. */
+    *ppxTimerTaskStackBuffer = uxTimerTaskStack;
+
+    /* Pass out the size of the array pointed to by *ppxTimerTaskStackBuffer.
+    Note that, as the array is necessarily of type StackType_t,
+    configTIMER_TASK_STACK_DEPTH is specified in words, not bytes. */
+    *pulTimerTaskStackSize = configTIMER_TASK_STACK_DEPTH;
+}
+/*-----------------------------------------------------------*/

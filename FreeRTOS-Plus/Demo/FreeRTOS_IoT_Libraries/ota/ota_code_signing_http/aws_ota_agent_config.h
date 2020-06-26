ac5c4d431471b21dbd7e8fda2eee05508e26cf5b
Tiangang Song <ts.whu@outlook.com>
2020-06-25 18:23:26 -0700

    Update OTA demo config to match from AFR

--- FreeRTOS-Plus/Demo/FreeRTOS_IoT_Libraries/ota/ota_code_signing_http/aws_ota_agent_config.h
+++ FreeRTOS-Plus/Demo/FreeRTOS_IoT_Libraries/ota/ota_code_signing_http/aws_ota_agent_config.h
@@ -84,7 +84,7 @@
  *  Please note that this must be set larger than zero.
  *
  */
-#define otaconfigMAX_NUM_BLOCKS_REQUEST		 32U
+#define otaconfigMAX_NUM_BLOCKS_REQUEST		 1U
 
 /**
  * @brief The maximum number of requests allowed to send without a response before we abort.

969848e02b7163a5d7fad461e97c3f9d510749eb
Lee George Thomas <ltham@amazon.com>
2020-06-30 17:46:28 -0700

    Common MQTT demo files

---
+++ FreeRTOS-Plus/Demo/FreeRTOS-IoT-Libraries-LTS-Beta2/mqtt/mqtt_light_weight/DemoTasks/LightWeightMQTTExample.c
@@ -0,0 +1,86 @@
+/*
+ * FreeRTOS Kernel V10.3.0
+ * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * http://www.FreeRTOS.org
+ * http://aws.amazon.com/freertos
+ *
+ * 1 tab == 4 spaces!
+ */
+
+/*
+ * Light weight MQTT demo.
+ * TODO - To be implemented.
+ */
+
+/* Standard includes. */
+#include <string.h>
+#include <stdio.h>
+
+/* Kernel includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+
+/* Demo Specific configs. */
+#include "demo_config.h"
+
+/* MQTT library config. */
+#include "mqtt_config.h"
+
+
+/*-----------------------------------------------------------*/
+
+/**
+ * @brief The task used to demonstrate the light weight MQTT API.
+ *
+ * @note To be implemented.
+ *
+ * @param[in] pvParameters Parameters as passed at the time of task creation. Not
+ * used in this example.
+ */
+static void prvMQTTDemoTask( void * pvParameters );
+
+/*-----------------------------------------------------------*/
+
+/*
+ * @brief Create the task that demonstrates the Light Weight MQTT API Demo.
+ */
+void vStartSimpleMQTTDemo( void )
+{
+	xTaskCreate( prvMQTTDemoTask,          /* Function that implements the task. */
+				 "MQTTLWDemo",             /* Text name for the task - only used for debugging. */
+				 configMINIMAL_STACK_SIZE, /* Size of stack (in words, not bytes) to allocate for the task. */
+				 NULL,                     /* Task parameter - not used in this case. */
+				 tskIDLE_PRIORITY,         /* Task priority, must be between 0 and configMAX_PRIORITIES - 1. */
+				 NULL );                   /* Used to pass out a handle to the created task - not used in this case. */
+}
+
+/*-----------------------------------------------------------*/
+
+static void prvMQTTDemoTask( void * pvParameters )
+{
+
+	/* Demo stub. */
+	vTaskDelay( pdMS_TO_TICKS( 1000 ) );
+	LogInfo( ( "In the light weight MQTT demo." ) );
+	vTaskDelay( pdMS_TO_TICKS( 1000 ) );
+}
+
+/*-----------------------------------------------------------*/

bd6753bf3f5c3316c7f8a33df2cfec9bfb7a3fa3
AniruddhaKanhere <kanherea@amazon.com>
2020-08-12 10:05:17 -0700

    Update

--- FreeRTOS-Plus/Test/CBMC/proofs/UDP/vProcessGeneratedUDPPacket/vProcessGeneratedUDPPacket_harness.c
+++ FreeRTOS-Plus/Test/CBMC/proofs/UDP/vProcessGeneratedUDPPacket/vProcessGeneratedUDPPacket_harness.c
@@ -6,14 +6,20 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "queue.h"
+#include "list.h"
 #include "semphr.h"
 
 /* FreeRTOS+TCP includes. */
+#include "FreeRTOS_IP.h"
+
+#include "FreeRTOS_ARP.h"
 #include "FreeRTOS_UDP_IP.h"
+//#include "FreeRTOS_TCP_IP.h"
+#include "FreeRTOS_IP_Private.h"
 
-#include "cbmc.h"
+uint16_t usGenerateChecksum( uint16_t usSum, const uint8_t * pucNextData, size_t uxByteCount ) { }
 
-
+uint16_t usGenerateProtocolChecksum( const uint8_t * const pucEthernetBuffer, size_t uxBufferLength, BaseType_t xOutgoingPacket ) { }
 
 /* This function has been tested separately. Therefore, we assume that the implementation is correct. */
 void vARPRefreshCacheEntry( const MACAddress_t * pxMACAddress, const uint32_t ulIPAddress ) { }
@@ -21,10 +27,12 @@ void vARPRefreshCacheEntry( const MACAddress_t * pxMACAddress, const uint32_t ul
 /* This function has been tested separately. Therefore, we assume that the implementation is correct. */
 void vARPGenerateRequestPacket( NetworkBufferDescriptor_t * const pxNetworkBuffer )         { }
 
-
-void prvProcessEthernetPacket( NetworkBufferDescriptor_t * const pxNetworkBuffer );
-
-
+/* Provide a stub for this function. */
+eARPLookupResult_t eARPGetCacheEntry( uint32_t *pulIPAddress, MACAddress_t * const pxMACAddress )
+{
+	eARPLookupResult_t eResult;
+	return eResult;
+}
 
 /* Implementation of safe malloc */
 void *safeMalloc(size_t xWantedSize ){
@@ -48,14 +56,6 @@ NetworkBufferDescriptor_t *pxGetNetworkBufferWithDescriptor( size_t xRequestedSi
 }
 
 
-/* This Proof has been done separately. In 'parsing/ProcessIPPacket'. Hence we assume it to be correct here. */
-eFrameProcessingResult_t prvProcessIPPacket( IPPacket_t * pxIPPacket, NetworkBufferDescriptor_t * const pxNetworkBuffer )
-{
-	eFrameProcessingResult_t result;
-	return result;
-}
-
-
 /* Network Interface function needs to be stubbed out since we do not have an actual network interface. Hence we assume it to be correct. */
 BaseType_t xNetworkInterfaceOutput( NetworkBufferDescriptor_t * const pxNetworkBuffer, BaseType_t bReleaseAfterSend )
 {
@@ -66,7 +66,13 @@ BaseType_t xNetworkInterfaceOutput( NetworkBufferDescriptor_t * const pxNetworkB
 
 void harness()
 {
-	NetworkBufferDescriptor_t pxNetworkBuffer;
+	NetworkBufferDescriptor_t * const pxNetworkBuffer = malloc( sizeof( NetworkBufferDescriptor_t ) );
 
-	void vProcessGeneratedUDPPacket( &pxNetworkBuffer );
+	/* Pointer to the start of the Ethernet frame. It should be able to access the whole Ethernet frame.*/
+	pxNetworkBuffer->pucEthernetBuffer = malloc( ipTOTAL_ETHERNET_FRAME_SIZE );
+
+	/* Minimum length of the pxNetworkBuffer->xDataLength is at least the size of the IPPacket_t. */
+	__CPROVER_assume(pxNetworkBuffer->xDataLength >= sizeof(EthernetHeader_t)  && pxNetworkBuffer->xDataLength <= ipTOTAL_ETHERNET_FRAME_SIZE);
+
+	vProcessGeneratedUDPPacket( &pxNetworkBuffer );
 }

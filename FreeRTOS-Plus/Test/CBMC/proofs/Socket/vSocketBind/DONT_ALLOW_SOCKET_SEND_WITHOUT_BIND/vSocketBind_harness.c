52bb96a0d845cd32cc671361e262dfa7822fa96a
AniruddhaKanhere <kanherea@amazon.com>
2020-08-13 14:27:03 -0700

    Update

--- FreeRTOS-Plus/Test/CBMC/proofs/Socket/vSocketBind/DONT_ALLOW_SOCKET_SEND_WITHOUT_BIND/vSocketBind_harness.c
+++ FreeRTOS-Plus/Test/CBMC/proofs/Socket/vSocketBind/DONT_ALLOW_SOCKET_SEND_WITHOUT_BIND/vSocketBind_harness.c
@@ -7,10 +7,14 @@
 #include "list.h"
 
 /* FreeRTOS+TCP includes. */
-#include "FreeRTOS_IP_Private.h"
 #include "FreeRTOS_IP.h"
+#include "FreeRTOS_IP_Private.h"
 #include "FreeRTOS_Sockets.h"
 
+
+void *safeMalloc(size_t xWantedSize) {
+        return nondet_bool() ? malloc(xWantedSize) : NULL;
+}
 
 uint16_t prvGetPrivatePortNumber( BaseType_t xProtocol )
 {
@@ -18,13 +22,28 @@ uint16_t prvGetPrivatePortNumber( BaseType_t xProtocol )
 	return usResult;
 }
 
+BaseType_t xIPIsNetworkTaskReady( void )
+{
+	/* Return true saying that the task is ready. */
+	return pdTRUE;
+}
+
+
+/* Random number generator provided by the application. In our case, CBMC provides
+ * an indeterministic value. */
+BaseType_t xApplicationGetRandomNumber( uint32_t *pulNumber )
+{
+	BaseType_t xResult;
+	return xResult;
+}
+
 /* Memory assignment for FreeRTOS_Socket_t */
 FreeRTOS_Socket_t * ensure_FreeRTOS_Socket_t_is_allocated () {
 	FreeRTOS_Socket_t *pxSocket = malloc(sizeof(FreeRTOS_Socket_t));
 
-	pxSocket->u.xTCP.rxStream = safeMalloc(sizeof(StreamBuffer_t));
-	pxSocket->u.xTCP.txStream = safeMalloc(sizeof(StreamBuffer_t));
-	pxSocket->u.xTCP.pxPeerSocket = safeMalloc(sizeof(FreeRTOS_Socket_t));
+	pxSocket->u.xTCP.rxStream = malloc(sizeof(StreamBuffer_t));
+	pxSocket->u.xTCP.txStream = malloc(sizeof(StreamBuffer_t));
+	pxSocket->u.xTCP.pxPeerSocket = malloc(sizeof(FreeRTOS_Socket_t));
 
 	return pxSocket;
 }
@@ -32,15 +51,20 @@ FreeRTOS_Socket_t * ensure_FreeRTOS_Socket_t_is_allocated () {
 void harness()
 {
 	FreeRTOS_Socket_t *pxSocket = ensure_FreeRTOS_Socket_t_is_allocated();
+
 	__CPROVER_assume( pxSocket != NULL );
-	__CPROVER_assume( pxSocket != FREERTOS_INVALID_SOCKET )
+	__CPROVER_assume( pxSocket != FREERTOS_INVALID_SOCKET );
 	
+	/* malloc instead of safeMalloc since we do not allow socket without binding. */
 	struct freertos_sockaddr * pxBindAddress = malloc( sizeof( struct freertos_sockaddr ) );
 
 	/* uxAddressLength is not used in this implementation. */
 	size_t uxAddressLength;
 
 	BaseType_t xInternal;
+
+	/* Call to init the socket list. */
+	vNetworkSocketsInit();
 
 	vSocketBind( pxSocket, pxBindAddress, uxAddressLength, xInternal );
 }

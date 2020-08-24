a821166ecbc5b45ee3a0351e3f41a9960fda36f3
AniruddhaKanhere <kanherea@amazon.com>
2020-08-23 19:25:57 -0700

    Update the proof

--- FreeRTOS-Plus/Test/CBMC/proofs/UDP/vProcessGeneratedUDPPacket/vProcessGeneratedUDPPacket_harness.c
+++ FreeRTOS-Plus/Test/CBMC/proofs/UDP/vProcessGeneratedUDPPacket/vProcessGeneratedUDPPacket_harness.c
@@ -11,50 +11,59 @@
 
 /* FreeRTOS+TCP includes. */
 #include "FreeRTOS_IP.h"
-
 #include "FreeRTOS_ARP.h"
 #include "FreeRTOS_UDP_IP.h"
-//#include "FreeRTOS_TCP_IP.h"
 #include "FreeRTOS_IP_Private.h"
 
+#include "../../utility/memory_assignments.c"
 
-
-uint16_t usGenerateChecksum( uint16_t usSum, const uint8_t * pucNextData, size_t uxByteCount ) 
+/* We do not need to calculate the actual checksum for the proof to be complete.
+ * Neither does the checksum matter for completeness. */
+uint16_t usGenerateChecksum( uint16_t usSum, const uint8_t * pucNextData, size_t uxByteCount )
 {
+	__CPROVER_assert( pucNextData != NULL, "Next data in GenerateChecksum cannot be NULL" );
+
 	uint16_t usChecksum;
 
 	/* Return any random value of checksum since it does not matter for CBMC checks. */
 	return usChecksum;
 }
 
+
+/* We do not need to calculate the actual checksum for the proof to be complete.
+ * Neither does the checksum matter for completeness. */
 uint16_t usGenerateProtocolChecksum( const uint8_t * const pucEthernetBuffer, size_t uxBufferLength, BaseType_t xOutgoingPacket )
 {
+	__CPROVER_assert( pucEthernetBuffer != NULL, "The Ethernet buffer cannot be NULL while generating Protocol Checksum" );
 	uint16_t usProtocolChecksum;
 
 	/* Return random value of checksum since it does not matter for CBMC checks. */
 	return usProtocolChecksum;
 }
 
-/* This function has been tested separately. Therefore, we assume that the implementation is correct. */
-void vARPRefreshCacheEntry( const MACAddress_t * pxMACAddress, const uint32_t ulIPAddress ) { }
 
 /* This function has been tested separately. Therefore, we assume that the implementation is correct. */
-void vARPGenerateRequestPacket( NetworkBufferDescriptor_t * const pxNetworkBuffer )         { }
+void vARPRefreshCacheEntry( const MACAddress_t * pxMACAddress, const uint32_t ulIPAddress )
+{
+	/* pxMACAddress can be NULL or Non-NULL, no need to assert. */
+}
+
+
+/* This function has been tested separately. Therefore, we assume that the implementation is correct. */
+void vARPGenerateRequestPacket( NetworkBufferDescriptor_t * const pxNetworkBuffer )
+{
+	__CPROVER_assert( pxNetworkBuffer != NULL, "The network buffer cannot be NULL." );
+}
+
 
 /* This function has been tested separately. Therefore, we assume that the implementation is correct. */
 eARPLookupResult_t eARPGetCacheEntry( uint32_t *pulIPAddress, MACAddress_t * const pxMACAddress )
 {
+	__CPROVER_assert( pulIPAddress != NULL, "pulIPAddress cannot be NULL" );
+	__CPROVER_assert( pxMACAddress != NULL, "pxMACAddress cannot be NULL" );
+
 	eARPLookupResult_t eResult;
 	return eResult;
-}
-
-/* Implementation of safe malloc. Return a NULL or the expected buffer indeterministically. */
-void *safeMalloc(size_t xWantedSize ){
-	if(xWantedSize == 0){
-		return NULL;
-	}
-	uint8_t byte = nondet_uint8_t();
-	return byte ? malloc(xWantedSize) : NULL;
 }
 
 
@@ -65,8 +74,18 @@ NetworkBufferDescriptor_t *pxGetNetworkBufferWithDescriptor( size_t xRequestedSi
 		pxNetworkBuffer->pucEthernetBuffer = safeMalloc(xRequestedSizeBytes);
 
 		__CPROVER_assume(pxNetworkBuffer->xDataLength == ipSIZE_OF_ETH_HEADER + sizeof(int32_t));
-	}	
+	}
 	return pxNetworkBuffer;
+}
+
+
+/* Provide a stub for ReleaseNetworkBufferAndDescriptor since it is not required for the proof.  */
+void vReleaseNetworkBufferAndDescriptor( NetworkBufferDescriptor_t * pxNetworkBuffer )
+{
+	__CPROVER_assert( pxNetworkBuffer != NULL, "The network buffer to be freed cannot be NULL" );
+
+	pxNetworkBuffer->pucEthernetBuffer = NULL;
+	pxNetworkBuffer = NULL;
 }
 
 
@@ -80,13 +99,18 @@ BaseType_t xNetworkInterfaceOutput( NetworkBufferDescriptor_t * const pxNetworkB
 
 void harness()
 {
-	NetworkBufferDescriptor_t * const pxNetworkBuffer = malloc( sizeof( NetworkBufferDescriptor_t ) );
+	size_t xRequestedSizeBytes;
 
-	/* Pointer to the start of the Ethernet frame. It should be able to access the whole Ethernet frame.*/
-	pxNetworkBuffer->pucEthernetBuffer = malloc( ipTOTAL_ETHERNET_FRAME_SIZE );
+	/* Assume that the size of packet must be greater than that of UDP-Packet and less than
+	 * that of the Ethernet Frame Size. */
+	__CPROVER_assume( xRequestedSizeBytes >= sizeof(UDPPacket_t) && xRequestedSizeBytes <= ipTOTAL_ETHERNET_FRAME_SIZE );
 
-	/* Minimum length of the pxNetworkBuffer->xDataLength is at least the size of the IPPacket_t. */
-	__CPROVER_assume(pxNetworkBuffer->xDataLength >= sizeof(EthernetHeader_t)  && pxNetworkBuffer->xDataLength <= ipTOTAL_ETHERNET_FRAME_SIZE);
+	/* Second parameter is not used from CBMC's perspective. */
+	NetworkBufferDescriptor_t * const pxNetworkBuffer = pxGetNetworkBufferWithDescriptor( xRequestedSizeBytes, 0 );
+
+	/* The buffer cannot be NULL for the function call. */
+	__CPROVER_assume(pxNetworkBuffer != NULL);
+	__CPROVER_assume(pxNetworkBuffer->pucEthernetBuffer != NULL);
 
 	vProcessGeneratedUDPPacket( pxNetworkBuffer );
 }

305201bbbf09ae3d11716293b70de955c6ff6e38
David Chalco <david@chalco.io>
2020-09-24 13:01:45 -0700

    run cmock tests in action script

--- scripts/test-run.sh
+++ scripts/test-run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo "Running tests..."
-false
-RC=$?
+SOURCE_DIR=FreeRTOS/Test/CMock
+BUILD_DIR=FreeRTOS/Test/CMock/build
+cmake -S ${SOURCE_DIR} -B ${BUILD_DIR} && make -C ${BUILD_DIR} && ${BUILD_DIR}/bin/tests/queue_utest
+TEST_RESULT=$?
 echo "Done"
-
-exit $RC
+exit ${TEST_RESULT}

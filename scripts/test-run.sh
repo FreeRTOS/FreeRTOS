4b19747f48ad0b393d0ab4a5c1e33cc3d48bc080
David Chalco <david@chalco.io>
2020-09-24 13:18:26 -0700

    dont clone cmock again in cmake

--- scripts/test-run.sh
+++ scripts/test-run.sh
@@ -3,7 +3,7 @@
 echo "Running tests..."
 SOURCE_DIR=FreeRTOS/Test/CMock
 BUILD_DIR=FreeRTOS/Test/CMock/build
-cmake -S ${SOURCE_DIR} -B ${BUILD_DIR} && make -C ${BUILD_DIR} && ${BUILD_DIR}/bin/tests/queue_utest
+cmake -DBUILD_CLONE_SUBMODULES=0 -S ${SOURCE_DIR} -B ${BUILD_DIR} && make -C ${BUILD_DIR} && ${BUILD_DIR}/bin/tests/queue_utest
 TEST_RESULT=$?
 echo "Done"
 exit ${TEST_RESULT}

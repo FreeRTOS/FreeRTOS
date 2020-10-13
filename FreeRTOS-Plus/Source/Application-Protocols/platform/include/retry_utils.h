5f0bf944cb489f09d286c804844ce051dc556d0b
Oscar Michael Abrina <abrinao@amazon.com>
2020-10-13 12:55:26 -0700

    Fix MISRA warnings for platform code used by MQTT demos (#337)
    In addition to fixing MISRA warnings, code is updated to have complexity <= 8. Also, this adds changes from PR #313, which allows the support of simultaneous connections in the mbedTLS transport wrapper. 
    
    Co-authored-by: Muneeb Ahmed <54290492+muneebahmed10@users.noreply.github.com>

--- FreeRTOS-Plus/Source/Application-Protocols/platform/include/retry_utils.h
+++ FreeRTOS-Plus/Source/Application-Protocols/platform/include/retry_utils.h
@@ -128,7 +128,7 @@
  *
  *     // If pRetryParams->maxRetryAttempts is set to 0, try forever.
  *     if( ( pRetryParams->attemptsDone < pRetryParams->maxRetryAttempts ) ||
- *         ( 0 == pRetryParams->maxRetryAttempts ) )
+ *         ( 0U == pRetryParams->maxRetryAttempts ) )
  *     {
  *         // Choose a random value for back-off time between 0 and the max jitter value.
  *         backOffDelay = rand() % pRetryParams->nextJitterMax;

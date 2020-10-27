6e0005450febf0b9c183b27aa256e79379dfb423
Filip Manole <filip.manole@microchip.com>
2020-10-27 10:52:37 +0200

    Changed project AVR_Dx_IAR to use 'heap_1.c'.

--- FreeRTOS/Demo/AVR_Dx_IAR/settings/RTOSDemo.Debug.cspy.bat
+++ FreeRTOS/Demo/AVR_Dx_IAR/settings/RTOSDemo.Debug.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\m17336\Documents\FreeRTOS\FreeRTOS\Demo\AVR_Dx_IAR\settings\RTOSDemo.Debug.general.xcl" --backend -f "C:\Users\m17336\Documents\FreeRTOS\FreeRTOS\Demo\AVR_Dx_IAR\settings\RTOSDemo.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4\common\bin\cspybat" -f "C:\Users\m50673\Desktop\freeRTOS\bugfix\pr\FreeRTOS\FreeRTOS\Demo\AVR_Dx_IAR\settings\RTOSDemo.Debug.general.xcl" --backend -f "C:\Users\m50673\Desktop\freeRTOS\bugfix\pr\FreeRTOS\FreeRTOS\Demo\AVR_Dx_IAR\settings\RTOSDemo.Debug.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\m17336\Documents\FreeRTOS\FreeRTOS\Demo\AVR_Dx_IAR\settings\RTOSDemo.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\m17336\Documents\FreeRTOS\FreeRTOS\Demo\AVR_Dx_IAR\settings\RTOSDemo.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4\common\bin\cspybat" -f "C:\Users\m50673\Desktop\freeRTOS\bugfix\pr\FreeRTOS\FreeRTOS\Demo\AVR_Dx_IAR\settings\RTOSDemo.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\m50673\Desktop\freeRTOS\bugfix\pr\FreeRTOS\FreeRTOS\Demo\AVR_Dx_IAR\settings\RTOSDemo.Debug.driver.xcl" 
 
 @echo off 
 :end
--- FreeRTOS/Demo/AVR_Dx_IAR/settings/RTOSDemo.Debug.cspy.ps1
+++ FreeRTOS/Demo/AVR_Dx_IAR/settings/RTOSDemo.Debug.cspy.ps1
@@ -23,9 +23,9 @@
 
 if ($debugfile -eq "")
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\m17336\Documents\FreeRTOS\FreeRTOS\Demo\AVR_Dx_IAR\settings\RTOSDemo.Debug.general.xcl" --backend -f "C:\Users\m17336\Documents\FreeRTOS\FreeRTOS\Demo\AVR_Dx_IAR\settings\RTOSDemo.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4\common\bin\cspybat" -f "C:\Users\m50673\Desktop\freeRTOS\bugfix\pr\FreeRTOS\FreeRTOS\Demo\AVR_Dx_IAR\settings\RTOSDemo.Debug.general.xcl" --backend -f "C:\Users\m50673\Desktop\freeRTOS\bugfix\pr\FreeRTOS\FreeRTOS\Demo\AVR_Dx_IAR\settings\RTOSDemo.Debug.driver.xcl" 
 }
 else
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\m17336\Documents\FreeRTOS\FreeRTOS\Demo\AVR_Dx_IAR\settings\RTOSDemo.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\m17336\Documents\FreeRTOS\FreeRTOS\Demo\AVR_Dx_IAR\settings\RTOSDemo.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4\common\bin\cspybat" -f "C:\Users\m50673\Desktop\freeRTOS\bugfix\pr\FreeRTOS\FreeRTOS\Demo\AVR_Dx_IAR\settings\RTOSDemo.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\m50673\Desktop\freeRTOS\bugfix\pr\FreeRTOS\FreeRTOS\Demo\AVR_Dx_IAR\settings\RTOSDemo.Debug.driver.xcl" 
 }

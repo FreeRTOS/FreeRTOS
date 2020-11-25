f0b9afeb92fc27bcc8458489d4e93541df49d241
Archit Aggarwal <architag@amazon.com>
2020-11-25 23:44:39 +0000

    Formatting change

--- FreeRTOS-Plus/Demo/coreMQTT_Windows_Simulator/MQTT_Basic_TLS/DemoTasks/BasicTLSMQTTExample.c
+++ FreeRTOS-Plus/Demo/coreMQTT_Windows_Simulator/MQTT_Basic_TLS/DemoTasks/BasicTLSMQTTExample.c
@@ -702,7 +702,7 @@ static void prvMQTTSubscribeWithBackoffRetries( MQTTContext_t * pxMQTTContext )
 
                 /* Generate a random number and calculate backoff value (in milliseconds) for
                  * the next connection retry.
-                 *  Note: It is recommended to seed the random number generator with a device-specific
+                 * Note: It is recommended to seed the random number generator with a device-specific
                  * entropy source so that possibility of multiple devices retrying failed network operations
                  * at similar intervals can be avoided. */
                 xBackoffAlgStatus = BackoffAlgorithm_GetNextBackoff( &xRetryParams, uxRand(), &usNextRetryBackOff );

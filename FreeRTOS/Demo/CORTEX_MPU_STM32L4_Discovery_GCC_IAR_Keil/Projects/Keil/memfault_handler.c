d72edf188d25a0e08685c045059e69948cf07009
Gaurav Aggarwal <aggarg@amazon.com>
2020-12-06 14:51:39 -0800

    Fix build failure in STM32 Discovery MPU Projects
    Privileged data region size needs to be increased because heap is now
    placed in this region.
    
    Signed-off-by: Gaurav Aggarwal <aggarg@amazon.com>

--- FreeRTOS/Demo/CORTEX_MPU_STM32L4_Discovery_GCC_IAR_Keil/Projects/Keil/memfault_handler.c
+++ FreeRTOS/Demo/CORTEX_MPU_STM32L4_Discovery_GCC_IAR_Keil/Projects/Keil/memfault_handler.c
@@ -40,7 +40,7 @@ const uint32_t * __SRAM_segment_end__ = ( uint32_t * ) 0x20018000;
 const uint32_t * __privileged_functions_start__ = ( uint32_t * ) 0x08000000;
 const uint32_t * __privileged_functions_end__ = ( uint32_t * ) 0x08008000;
 const uint32_t * __privileged_data_start__ = ( uint32_t * ) 0x20000000;
-const uint32_t * __privileged_data_end__ = ( uint32_t * ) 0x20000400;
+const uint32_t * __privileged_data_end__ = ( uint32_t * ) 0x20008000;
 
 const uint32_t * __syscalls_flash_start__ = ( uint32_t * ) &( Image$$ER_IROM_FREERTOS_SYSTEM_CALLS$$Base );
 const uint32_t * __syscalls_flash_end__ = ( uint32_t * ) &( Image$$ER_IROM_FREERTOS_SYSTEM_CALLS$$Limit );

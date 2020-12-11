6ea05c407853b18518cdea8a900055cdd118f349
Oscar Abrina <abrinao@amazon.com>
2020-12-10 20:04:56 -0800

    Update Plaintext_FreeRTOS_send to check -pdFREERTOS_ERRNO_ENOSPC

--- FreeRTOS-Plus/Source/Application-Protocols/network_transport/freertos_plus_tcp/using_plaintext/using_plaintext.c
+++ FreeRTOS-Plus/Source/Application-Protocols/network_transport/freertos_plus_tcp/using_plaintext/using_plaintext.c
@@ -149,5 +149,13 @@ int32_t Plaintext_FreeRTOS_send( NetworkContext_t * pNetworkContext,
                                   bytesToSend,
                                   0 );
 
+    if( socketStatus == -pdFREERTOS_ERRNO_ENOSPC )
+    {
+        /* The TCP buffers could not accept any more bytes so zero bytes were sent
+         * but this is not necessarily an error that should cause a disconnect
+         * unless it persists. */
+        socketStatus = 0;
+    }
+
     return socketStatus;
 }

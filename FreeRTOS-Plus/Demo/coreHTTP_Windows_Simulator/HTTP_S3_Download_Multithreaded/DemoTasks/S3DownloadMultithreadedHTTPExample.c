d74336b33491e39ba47b35937b6e12c3fdb44ce7
Sukhmani Minhas <50919130+sukhmanm@users.noreply.github.com>
2020-12-10 21:11:43 -0500

    Add comment about request limits

--- FreeRTOS-Plus/Demo/coreHTTP_Windows_Simulator/HTTP_S3_Download_Multithreaded/DemoTasks/S3DownloadMultithreadedHTTPExample.c
+++ FreeRTOS-Plus/Demo/coreHTTP_Windows_Simulator/HTTP_S3_Download_Multithreaded/DemoTasks/S3DownloadMultithreadedHTTPExample.c
@@ -56,6 +56,13 @@
  * demo_config.h. Please use the provided script "presigned_urls_gen.py"
  * (located in located in coreHTTP_Windows_Simulator/Common) to generate these
  * URLs. For detailed instructions, see the accompanied README.md.
+ *
+ * @note If your file requires more than 99 range requests to S3 (depending on the
+ * size of the file and the length specified in democonfigRANGE_REQUEST_LENGTH),
+ * your connection may be dropped by S3. In this case, either increase the
+ * buffer size and range request length (if feasible), to reduce the number of
+ * requests required, or re-establish the connection with S3 after receiving a
+ * "Connection: close" response header.
  */
 
 /**

de24a74e39ae7014992a1053f563f09199abc4a3
Alfred Gedeon <alfred2g@hotmail.com>
2020-12-14 14:59:36 -0800

    Add Full Demo to Qemu with MPS2

--- FreeRTOS/Demo/CORTEX_M3_MPS2_QEMU_GCC/FreeRTOSConfig.h
+++ FreeRTOS/Demo/CORTEX_M3_MPS2_QEMU_GCC/FreeRTOSConfig.h
@@ -39,21 +39,22 @@
  * See https://www.freertos.org/a00110.html
  *----------------------------------------------------------*/
 
-#define configASSERT_DEFINED 1
+#define configASSERT_DEFINED                     1
 extern void vAssertCalled( void );
 #define configASSERT( x ) if( ( x ) == 0 ) vAssertCalled( )
+#define configQUEUE_REGISTRY_SIZE                20
 
-#define configUSE_PREEMPTION        1
-#define configUSE_TIME_SLICING      1
+#define configUSE_PREEMPTION                     1
+#define configUSE_TIME_SLICING                   1
 
-#define configUSE_IDLE_HOOK         0
-#define configUSE_TICK_HOOK         0
+#define configUSE_IDLE_HOOK                      0
+#define configUSE_TICK_HOOK                      0
 #define configCPU_CLOCK_HZ        ( ( unsigned long ) 20000000 )
 #define configTICK_RATE_HZ        ( ( TickType_t ) 1000 )
 #define configMINIMAL_STACK_SIZE  ( ( unsigned short ) 2000 )
 #define configTOTAL_HEAP_SIZE     ( ( size_t ) ( 279000 ) )
 #define configMAX_TASK_NAME_LEN   ( 10 )
-#define configUSE_TRACE_FACILITY    0
+#define configUSE_TRACE_FACILITY    1
 #define configUSE_16_BIT_TICKS      0
 #define configIDLE_SHOULD_YIELD     0
 #define configUSE_CO_ROUTINES       0
@@ -72,13 +73,41 @@ to exclude the API function. */
 #define configUSE_MALLOC_FAILED_HOOK    1
 #define configUSE_MUTEXES               1
 #define configUSE_RECURSIVE_MUTEXES     1
-#define INCLUDE_vTaskPrioritySet        0
-#define INCLUDE_uxTaskPriorityGet       0
-#define INCLUDE_vTaskDelete             0
+#define configUSE_TIMERS                1
+#define configTIMER_TASK_STACK_DEPTH                    ( configMINIMAL_STACK_SIZE * 2 )
+/*
+#define INCLUDE_vTaskPrioritySet        1
+#define INCLUDE_uxTaskPriorityGet       1
+#define INCLUDE_vTaskDelete             1
 #define INCLUDE_vTaskCleanUpResources   0
 #define INCLUDE_vTaskSuspend            0
 #define INCLUDE_vTaskDelayUntil         1
 #define INCLUDE_vTaskDelay              1
+*/
+
+#define INCLUDE_vTaskPrioritySet                                1
+#define INCLUDE_uxTaskPriorityGet                               1
+#define INCLUDE_vTaskDelete                                             1
+#define INCLUDE_vTaskCleanUpResources                   0
+#define INCLUDE_vTaskSuspend                                    1
+#define INCLUDE_vTaskDelayUntil                                 1
+#define INCLUDE_vTaskDelay                                              1
+#define INCLUDE_uxTaskGetStackHighWaterMark             1
+#define INCLUDE_uxTaskGetStackHighWaterMark2    1
+#define INCLUDE_xTaskGetSchedulerState                  1
+#define INCLUDE_xTimerGetTimerDaemonTaskHandle  1
+#define INCLUDE_xTaskGetIdleTaskHandle                  1
+#define INCLUDE_xTaskGetHandle                                  1
+#define INCLUDE_eTaskGetState                                   1
+#define INCLUDE_xSemaphoreGetMutexHolder                1
+#define INCLUDE_xTimerPendFunctionCall                  1
+#define INCLUDE_xTaskAbortDelay                                 1
+
+unsigned long ulGetRunTimeCounterValue( void ); /* Prototype of function that returns run time counter. */
+
+//#include "trcRecorder.h"
+#define projCOVERAGE_TEST 0
+
 
 
 #define configKERNEL_INTERRUPT_PRIORITY         255
--- FreeRTOS/Demo/CORTEX_M3_MPS2_QEMU_GCC/main.c
+++ FreeRTOS/Demo/CORTEX_M3_MPS2_QEMU_GCC/main.c
@@ -38,14 +38,21 @@ void vApplicationMallocFailedHook( void );
 void vApplicationIdleHook( void );
 void vApplicationTickHook( void );
 void main_blinky( void );
+void main_full( void );
 
 extern void initialise_monitor_handles(void);
+
+StackType_t uxTimerTaskStack[ configTIMER_TASK_STACK_DEPTH ];
 
 int main ()
 {
 #if (mainCREATE_SIMPLE_BLINKY_DEMO_ONLY == 1)
     {
     main_blinky();
+    }
+#elif (mainCREATE_FULL_DEMO_ONLY == 1)
+    {
+        main_full();
     }
 #else
     {
@@ -123,4 +130,26 @@ void vLoggingPrintf( const char *pcFormat, ... )
         va_start( arg, pcFormat );
         vprintf( pcFormat, arg );
         va_end( arg );
+}
+/*-----------------------------------------------------------*/
+void vApplicationGetTimerTaskMemory( StaticTask_t **ppxTimerTaskTCBBuffer,
+                                     StackType_t **ppxTimerTaskStackBuffer,
+                                     uint32_t *pulTimerTaskStackSize )
+{
+    /* If the buffers to be provided to the Timer task are declared inside this
+    function then they must be declared static - otherwise they will be allocated on
+    the stack and so not exists after this function exits. */
+    static StaticTask_t xTimerTaskTCB;
+
+    /* Pass out a pointer to the StaticTask_t structure in which the Timer
+    task's state will be stored. */
+    *ppxTimerTaskTCBBuffer = &xTimerTaskTCB;
+
+    /* Pass out the array that will be used as the Timer task's stack. */
+    *ppxTimerTaskStackBuffer = uxTimerTaskStack;
+
+     /* Pass out the size of the array pointed to by *ppxTimerTaskStackBuffer.
+     Note that, as the array is necessarily of type StackType_t,
+     configMINIMAL_STACK_SIZE is specified in words, not bytes. */
+    *pulTimerTaskStackSize = configTIMER_TASK_STACK_DEPTH;
 }

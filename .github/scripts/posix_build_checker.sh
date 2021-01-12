8aa06ef1120a8ed585e6d54fcfdf6b56e77cd8c2
Alfred Gedeon <alfred2g@hotmail.com>
2021-01-12 15:06:10 -0800

    Add Posix build checker to git actions

---
+++ .github/scripts/posix_build_checker.sh
@@ -0,0 +1,19 @@
+PROJECT=$1
+echo "Verifying url links of: ${PROJECT}"
+if [ ! -d "$PROJECT" ]
+then
+    echo "Directory passed does not exist"
+    exit 2
+fi
+
+SCRIPT_RET=0
+
+set -o nounset        # Treat unset variables as an error
+
+cd ${PROJECT}/FreeRTOS/Demo/Posix_GCC
+
+make
+
+SCRIPT_RET = $?
+exit ${SCRIPT_RET}
+

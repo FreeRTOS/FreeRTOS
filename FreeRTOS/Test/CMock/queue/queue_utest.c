d64d405f4cc9ea641b3bc94206913017a55e51c9
Alfred Gedeon <alfred2g@hotmail.com>
2021-02-04 14:20:23 -0800

    Fix file header checks

--- FreeRTOS/Test/CMock/queue/queue_utest.c
+++ FreeRTOS/Test/CMock/queue/queue_utest.c
@@ -1,4 +1,5 @@
 /*
+ * FreeRTOS V202012.00
  * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
@@ -18,8 +19,8 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
- * http://aws.amazon.com/freertos
- * http://www.FreeRTOS.org
+ * https://www.FreeRTOS.org
+ * https://github.com/FreeRTOS
  */
 
 /*! @file queue_utest.c */

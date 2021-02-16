70e5b8de66c52088057c3a38c6653f7f30338998
David Chalco <david@chalco.io>
2021-02-16 10:35:35 -0800

    add attempted mangle test

--- FreeRTOS/Test/Integration/timers/itest.c
+++ FreeRTOS/Test/Integration/timers/itest.c
@@ -18,7 +18,77 @@ TEST_GROUP_RUNNER( Timers )
     RUN_TEST_CASE( Timers, TimerHappyPath );
 }
 
+
+void prvCallBack_TimerTest( TimerHandle_t xTimer )
+{
+
+}
+
+TimerHandle_t prvCreateTimer()
+{
+
+}
+
 TEST( Timers, TimerHappyPath )
 {
-    TEST_ASSERT( 1 );
+    TimerHandle_t xTimer_Handle = NULL;
+    TickType_t xTimerPeriod = pdMS_TO_TICKS( 1 * 1000 );
+    UBaseType_t ulID = 0;
+    static StaticTimer_t xTimer_Memory;
+
+    xTimer_Handle = xTimerCreateStatic( "static-timer",
+                                       portMAX_DELAY,
+                                       pdFALSE,
+                                       &ulID,
+                                       prvCallBack_TimerTest,
+                                       &xTimer_Memory
+                                       );
+
+    TEST_ASSERT( xTimer_Handle != NULL );
+}
+
+/**
+ * @brief Attempts to mangle list [IoTClock_TimerCreate, IotClock_
+ * 
+ */
+TEST( Timers, TimerAttemptMangle )
+{
+    /* IotClock_TimerCreate */
+    TimerHandle_t xTimer_Handle = NULL;
+    TickType_t xTimerPeriod = pdMS_TO_TICKS( 1 * 1000 );
+    UBaseType_t ulID = 0;
+    static StaticTimer_t xTimer_Memory;
+
+    xTimer_Handle = xTimerCreateStatic( "static-timer",
+                                       portMAX_DELAY,
+                                       pdFALSE,
+                                       &ulID,
+                                       prvCallBack_TimerTest,
+                                       &xTimer_Memory
+                                       );
+
+    /* IotClock_TimerArm */
+    xTimerChangePeriod( xTimer_Handle, 2 * xTimerPeriod, portMAX_DELAY );
+
+    /* IotClock_TimerDestroy */
+    if( xTimerIsTimerActive( xTimer_Handle ))
+    {
+        xTimerStop( xTimer_Handle, portMAX_DELAY );
+
+        while( xTimerIsTimerActive( xTimer_Handle ) == pdTRUE )
+        {
+            vTaskDelay(1);
+        }
+    }
+
+    /* IotClock_TimerCreate */
+    xTimer_Handle = xTimerCreateStatic( "static-timer",
+                                    portMAX_DELAY,
+                                    pdFALSE,
+                                    &ulID,
+                                    prvCallBack_TimerTest,
+                                    &xTimer_Memory
+                                    );
+
+    TEST_ASSERT( xTimer_Handle != NULL );
 }

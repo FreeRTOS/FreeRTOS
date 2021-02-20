ea51a3bfbf7c06e6e3024403369f7313656dc371
RichardBarry <ribarry@amazon.com>
2021-02-20 13:34:56 -0800

    Introduce tasks that test the coherency of the reported space available in a message buffer from two separate tasks. Designed to highlight the issue reported in FreeRTOS/FreeRTOS-Kernel#264
    Introduce configRUN_ADDITIONAL_TESTS which must be set to 1 to run the new tests. That is because the new tests got added to an existing standard demo file and smaller platforms may not have the resources to run them.
    
    Set configRUN_ADDITIONAL_TESTS to 1 in the MSVC and IAR/QEMU project so both project run the new test. Also add missing 'volatile' qualifier on some register accesses in the same file.

--- FreeRTOS/Demo/WIN32-MSVC/main.c
+++ FreeRTOS/Demo/WIN32-MSVC/main.c
@@ -80,7 +80,7 @@ smaller heap regions - in which case heap_4.c would be the more appropriate
 choice.  See http://www.freertos.org/a00111.html for an explanation. */
 #define mainREGION_1_SIZE	8201
 #define mainREGION_2_SIZE	29905
-#define mainREGION_3_SIZE	7607
+#define mainREGION_3_SIZE	7807
 
 /*-----------------------------------------------------------*/
 

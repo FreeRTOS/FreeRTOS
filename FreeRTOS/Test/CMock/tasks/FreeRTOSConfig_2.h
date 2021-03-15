c7c309dc0b6f7c8d3438a018f318f9c73ef46abe
Alfred Gedeon <alfred2g@hotmail.com>
2021-03-15 13:50:57 -0700

    Cleanup and common header

--- FreeRTOS/Test/CMock/tasks/FreeRTOSConfig_2.h
+++ FreeRTOS/Test/CMock/tasks/FreeRTOSConfig_2.h
@@ -117,6 +117,7 @@ void vConfigureTimerForRunTimeStats( void );    /* Prototype of function that in
 /* It is a good idea to define configASSERT() while developing.  configASSERT()
  * uses the same semantics as the standard C assert() macro. */
 #define configASSERT( x )
+#define portREMOVE_STATIC_QUALIFIER              1
 
 #define configINCLUDE_MESSAGE_BUFFER_AMP_DEMO    0
 #if ( configINCLUDE_MESSAGE_BUFFER_AMP_DEMO == 1 )

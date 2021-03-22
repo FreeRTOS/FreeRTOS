38c6a3ca8d0a95fe2bb1fee78bd1d63333fc0239
Ming Yue <mingyue86010@gmail.com>
2021-03-22 10:38:56 -0700

    Formatted

--- FreeRTOS/Test/CMock/event_groups/list_macros.h
+++ FreeRTOS/Test/CMock/event_groups/list_macros.h
@@ -33,40 +33,41 @@
 #include <list.h>
 
 struct tskTaskControlBlock;
-typedef struct tskTaskControlBlock  TCB_t;
+typedef struct tskTaskControlBlock TCB_t;
 
 #undef  listLIST_IS_EMPTY
-BaseType_t listLIST_IS_EMPTY( const List_t *pxList );
+BaseType_t listLIST_IS_EMPTY( const List_t * pxList );
 
 #undef  listGET_OWNER_OF_HEAD_ENTRY
-TCB_t * listGET_OWNER_OF_HEAD_ENTRY(const  List_t * pxList );
+TCB_t * listGET_OWNER_OF_HEAD_ENTRY( const List_t * pxList );
 
 #undef listIS_CONTAINED_WITHIN
-BaseType_t listIS_CONTAINED_WITHIN( List_t * list, const ListItem_t * listItem);
+BaseType_t listIS_CONTAINED_WITHIN( List_t * list,
+                                    const ListItem_t * listItem );
 
 #undef listGET_LIST_ITEM_VALUE
-TickType_t listGET_LIST_ITEM_VALUE( ListItem_t * listItem  );
+TickType_t listGET_LIST_ITEM_VALUE( ListItem_t * listItem );
 
 #undef listSET_LIST_ITEM_VALUE
-void listSET_LIST_ITEM_VALUE( ListItem_t * listItem, TickType_t itemValue);
-
+void listSET_LIST_ITEM_VALUE( ListItem_t * listItem,
+                              TickType_t itemValue );
 
 #undef listLIST_ITEM_CONTAINER
-List_t * listLIST_ITEM_CONTAINER(const ListItem_t * listItem);
+List_t * listLIST_ITEM_CONTAINER( const ListItem_t * listItem );
 
 #undef listCURRENT_LIST_LENGTH
-UBaseType_t listCURRENT_LIST_LENGTH(List_t * list);
+UBaseType_t listCURRENT_LIST_LENGTH( List_t * list );
 
 #undef listGET_ITEM_VALUE_OF_HEAD_ENTRY
-TickType_t listGET_ITEM_VALUE_OF_HEAD_ENTRY(List_t * list);
+TickType_t listGET_ITEM_VALUE_OF_HEAD_ENTRY( List_t * list );
 
 #undef listGET_LIST_ITEM_OWNER
-TCB_t * listGET_LIST_ITEM_OWNER(ListItem_t * listItem);
+TCB_t * listGET_LIST_ITEM_OWNER( ListItem_t * listItem );
 
 #undef listGET_END_MARKER
-ListItem_t * listGET_END_MARKER(ListItem_t * pxList );
+ListItem_t * listGET_END_MARKER( ListItem_t * pxList );
 
 #undef listGET_NEXT
-ListItem_t * listGET_NEXT(ListItem_t * pxList );
+ListItem_t * listGET_NEXT( ListItem_t * pxList );
 
-#endif
+#endif /* ifndef LIST_MACRO_H */

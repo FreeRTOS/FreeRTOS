67171d8eb633bcb81b9075e6b2f740a7b92acb56
Archit Aggarwal <architag@amazon.com>
2021-04-29 18:23:08 +0000

    [AUTO][RELEASE]: Bump file header version to "202104.00"

--- .github/scripts/check-header.py
+++ .github/scripts/check-header.py
@@ -369,7 +369,7 @@ FREERTOS_IGNORED_PATTERNS = [
 
 FREERTOS_HEADER = [
     '/*\n',
-    ' * FreeRTOS V202104.00\n'
+    ' * FreeRTOS V202104.00
     ' * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.\n',
     ' *\n',
     ' * Permission is hereby granted, free of charge, to any person obtaining a copy of\n',
@@ -422,7 +422,7 @@ KERNEL_IGNORED_PATTERNS = [r'.*\.git.*']
 
 KERNEL_HEADER = [
     '/*\n',
-    ' * FreeRTOS Kernel V10.4.3\n'
+    ' * FreeRTOS V202104.00
     ' * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.\n',
     ' *\n',
     ' * Permission is hereby granted, free of charge, to any person obtaining a copy of\n',

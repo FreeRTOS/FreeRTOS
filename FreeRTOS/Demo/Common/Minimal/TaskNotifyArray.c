1cace848b12b6218f98f02d243a1289bcafa5cba
root <root@ip-172-31-5-28.us-west-2.compute.internal>
2021-05-11 00:49:12 +0000

    Fix last of header checks

--- FreeRTOS/Demo/Common/Minimal/TaskNotifyArray.c
+++ FreeRTOS/Demo/Common/Minimal/TaskNotifyArray.c
@@ -19,8 +19,8 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
- * https://github.com/FreeRTOS
  * https://www.FreeRTOS.org
+ * https://github.com/FreeRTOS
  *
  */
 

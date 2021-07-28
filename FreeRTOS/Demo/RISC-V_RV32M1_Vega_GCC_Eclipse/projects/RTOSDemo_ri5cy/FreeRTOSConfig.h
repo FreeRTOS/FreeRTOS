5f21507703a220580c696648d33cf0003912aa9b
Gaurav-Aggarwal-AWS <33462878+aggarg@users.noreply.github.com>
2021-07-28 10:44:52 -0700

    Add prints for CI (#670)
    * Add prints for CI markers
    
    Signed-off-by: Gaurav Aggarwal <aggarg@amazon.com>

--- FreeRTOS/Demo/RISC-V_RV32M1_Vega_GCC_Eclipse/projects/RTOSDemo_ri5cy/FreeRTOSConfig.h
+++ FreeRTOS/Demo/RISC-V_RV32M1_Vega_GCC_Eclipse/projects/RTOSDemo_ri5cy/FreeRTOSConfig.h
@@ -72,6 +72,7 @@
 #define FREERTOS_CONFIG_H
 
 #include "clock_config.h"
+#include "fsl_debug_console.h"
 
 /*-----------------------------------------------------------
  * Application specific definitions.
@@ -148,5 +149,7 @@ header file. */
 #define configUSE_PORT_OPTIMISED_TASK_SELECTION 0
 #define configKERNEL_INTERRUPT_PRIORITY 7
 
+/* Map to the platform printf function. */
+#define configPRINT_STRING( pcString )  PRINTF( pcString )
 
 #endif /* FREERTOS_CONFIG_H */

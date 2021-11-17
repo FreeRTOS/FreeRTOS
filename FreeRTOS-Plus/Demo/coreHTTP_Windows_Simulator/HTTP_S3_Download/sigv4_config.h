02f44a74bcede44d08a8d3b6945bcf3c2175d390
Shivangi <gshvang@amazon.com>
2021-11-17 14:30:17 -0800

    Sigv4_config header changes

--- FreeRTOS-Plus/Demo/coreHTTP_Windows_Simulator/HTTP_S3_Download/sigv4_config.h
+++ FreeRTOS-Plus/Demo/coreHTTP_Windows_Simulator/HTTP_S3_Download/sigv4_config.h
@@ -1,5 +1,5 @@
 /*
- * FreeRTOS V202111.00
+ * SigV4 Utility Library v1.0.0
  * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
@@ -18,10 +18,12 @@
  * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- * https://www.FreeRTOS.org
- * https://github.com/FreeRTOS
- *
+ */
+
+/**
+ * @file sigv4_config.h
+ * @brief Values for configuration macros provided by the application to be
+ * used by the SigV4 Utility Library.
  */
 
 #ifndef SIGV4_CONFIG_H_

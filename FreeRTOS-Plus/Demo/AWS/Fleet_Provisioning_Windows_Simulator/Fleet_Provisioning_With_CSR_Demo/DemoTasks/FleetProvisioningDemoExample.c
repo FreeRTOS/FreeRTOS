2fcf4f432f1ce7596a302b797a23f40e400be1a2
johnrhen <johnrhen@amazon.com>
2021-12-01 12:20:43 -0800

    Update lexicon.txt

--- FreeRTOS-Plus/Demo/AWS/Fleet_Provisioning_Windows_Simulator/Fleet_Provisioning_With_CSR_Demo/DemoTasks/FleetProvisioningDemoExample.c
+++ FreeRTOS-Plus/Demo/AWS/Fleet_Provisioning_Windows_Simulator/Fleet_Provisioning_With_CSR_Demo/DemoTasks/FleetProvisioningDemoExample.c
@@ -267,14 +267,14 @@ static bool prvUnsubscribeFromRegisterThingResponseTopics( void );
  */
 static uint32_t prvGetTimeMs( void );
 
-/** TODO: Add description for FP demo
+/**
  * @brief The task used to demonstrate the FP API.
  *
- * This task collects metrics from the device using the functions in
- * metrics_collector.h and uses them to build a defender report using functions
- * in report_builder.h. Metrics include the number for bytes written and read
- * over the network, open TCP and UDP ports, and open TCP sockets. The
- * generated report is then published to the AWS IoT Device Defender service.
+ * This task uses the provided claim key and certificate files to connect to
+ * AWS and use PKCS #11 to generate a new device key and certificate with a CSR. 
+ * The task then creates a new Thing with the Fleet Provisioning API using the 
+ * newly-created credentials. The task finishes by connecting to the newly-created
+ * Thing to verify that it was successfully created and accessible using the key/cert.
  *
  * @param[in] pvParameters Parameters as passed at the time of task creation.
  * Not used in this example.

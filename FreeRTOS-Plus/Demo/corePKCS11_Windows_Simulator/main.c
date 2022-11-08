1629dac1dda6279b16a9f8889676bbfd688e4f77
Paul Bartell <pbartell@amazon.com>
2022-11-07 16:24:02 -0800

    Remove boileplate FreeRTOS kernel and +tcp hooks

--- FreeRTOS-Plus/Demo/corePKCS11_Windows_Simulator/main.c
+++ FreeRTOS-Plus/Demo/corePKCS11_Windows_Simulator/main.c
@@ -36,7 +36,7 @@
 #include <intrin.h>
 
 /* FreeRTOS includes. */
-#include <FreeRTOS.h>
+#include "FreeRTOS.h"
 #include "task.h"
 
 /* Windows Crypto includes. */
@@ -49,11 +49,9 @@
 #include "pkcs11_demo_config.h"
 #include "pkcs11_demos.h"
 
-/*
- * Private function for starting the various PKCS #11 demos.
- *
- */
-static void prvStartPKCS11Demo( void )
+/*-----------------------------------------------------------*/
+
+static void prvPKCS11DemoTask( void * pvParameters )
 {
     configPRINTF( ( "---------STARTING DEMO---------\r\n" ) );
     #if ( configPKCS11_MANAGEMENT_AND_RNG_DEMO == 1 )
@@ -77,19 +75,16 @@ static void prvStartPKCS11Demo( void )
 
 int main( void )
 {
-    configPRINTF( ( "Creating PKCS #11 Demo Task.\r\n" ) );
-    BaseType_t xReturned;
-    TaskHandle_t xHandle = NULL;
+    vPlatformInitLogging();
 
-    /* Create the PKCS #11 demo task. */
-    xReturned = xTaskCreate(
-        ( TaskFunction_t ) prvStartPKCS11Demo,
-        "PKCS11 Demo",
-        configPKCS11_DEMO_STACK_SIZE,
-        NULL,
-        tskIDLE_PRIORITY + 1,
-        &xHandle );
-    configASSERT( xReturned == pdPASS );
+    /* Create the SNTP client task that is responsible for synchronizing system time with the time servers
+     * periodically. This is created as a high priority task to keep the SNTP client operation unhindered. */
+    xTaskCreate( prvPKCS11DemoTask,             /* Function that implements the task. */
+                 "PKCS11 Demo",                 /* Text name for the task - only used for debugging. */
+                 configPKCS11_DEMO_STACK_SIZE,  /* Size of stack (in words, not bytes) to allocate for the task. */
+                 NULL,                          /* Task parameter - not used in this case. */
+                 configMAX_PRIORITIES - 1,      /* Task priority, must be between 0 and configMAX_PRIORITIES - 1. */
+                 NULL );
 
     /* Start the RTOS scheduler. */
     vTaskStartScheduler();
@@ -98,101 +93,13 @@ int main( void )
      * line will never be reached.  If the following line does execute, then
      * there was insufficient FreeRTOS heap memory available for the idle and/or
      * timer tasks to be created.  See the memory management section on the
-     * FreeRTOS web site for more details (this is standard text that is not
-     * really applicable to the Win32 simulator port). */
+     * FreeRTOS web site for more details.
+     */
+
     for( ; ; )
     {
-        __debugbreak();
+        configASSERT( pdFALSE );
     }
 }
-/*-----------------------------------------------------------*/
 
-void vLoggingPrintf( const char * pcFormat,
-                     ... )
-{
-    va_list arg;
-
-    va_start( arg, pcFormat );
-    vprintf( pcFormat, arg );
-    va_end( arg );
-}
-/*-----------------------------------------------------------*/
-
-void vAssertCalled( const char * pcFile,
-                    uint32_t ulLine )
-{
-    volatile uint32_t ulBlockVariable = 0UL;
-    volatile char * pcFileName = ( volatile char * ) pcFile;
-    volatile uint32_t ulLineNumber = ulLine;
-
-    ( void ) pcFileName;
-    ( void ) ulLineNumber;
-
-    printf( "vAssertCalled( %s, %u\n", pcFile, ulLine );
-
-    /* Setting ulBlockVariable to a non-zero value in the debugger will allow
-     * this function to be exited. */
-    taskDISABLE_INTERRUPTS();
-    {
-        while( ulBlockVariable == 0UL )
-        {
-            __debugbreak();
-        }
-    }
-    taskENABLE_INTERRUPTS();
-}
-/*-----------------------------------------------------------*/
-
-/* configUSE_STATIC_ALLOCATION is set to 1, so the application must provide an
- * implementation of vApplicationGetIdleTaskMemory() to provide the memory that is
- * used by the Idle task. */
-void vApplicationGetIdleTaskMemory( StaticTask_t ** ppxIdleTaskTCBBuffer,
-                                    StackType_t ** ppxIdleTaskStackBuffer,
-                                    uint32_t * pulIdleTaskStackSize )
-{
-    /* If the buffers to be provided to the Idle task are declared inside this
-     * function then they must be declared static - otherwise they will be allocated on
-     * the stack and so not exists after this function exits. */
-    static StaticTask_t xIdleTaskTCB;
-    static StackType_t uxIdleTaskStack[ configMINIMAL_STACK_SIZE ];
-
-    /* Pass out a pointer to the StaticTask_t structure in which the Idle task's
-     * state will be stored. */
-    *ppxIdleTaskTCBBuffer = &xIdleTaskTCB;
-
-    /* Pass out the array that will be used as the Idle task's stack. */
-    *ppxIdleTaskStackBuffer = uxIdleTaskStack;
-
-    /* Pass out the size of the array pointed to by *ppxIdleTaskStackBuffer.
-     * Note that, as the array is necessarily of type StackType_t,
-     * configMINIMAL_STACK_SIZE is specified in words, not bytes. */
-    *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
-}
-/*-----------------------------------------------------------*/
-
-/* configUSE_STATIC_ALLOCATION and configUSE_TIMERS are both set to 1, so the
- * application must provide an implementation of vApplicationGetTimerTaskMemory()
- * to provide the memory that is used by the Timer service task. */
-void vApplicationGetTimerTaskMemory( StaticTask_t ** ppxTimerTaskTCBBuffer,
-                                     StackType_t ** ppxTimerTaskStackBuffer,
-                                     uint32_t * pulTimerTaskStackSize )
-{
-    /* If the buffers to be provided to the Timer task are declared inside this
-     * function then they must be declared static - otherwise they will be allocated on
-     * the stack and so not exists after this function exits. */
-    static StaticTask_t xTimerTaskTCB;
-    static StackType_t uxTimerTaskStack[ configTIMER_TASK_STACK_DEPTH ];
-
-    /* Pass out a pointer to the StaticTask_t structure in which the Timer
-     * task's state will be stored. */
-    *ppxTimerTaskTCBBuffer = &xTimerTaskTCB;
-
-    /* Pass out the array that will be used as the Timer task's stack. */
-    *ppxTimerTaskStackBuffer = uxTimerTaskStack;
-
-    /* Pass out the size of the array pointed to by *ppxTimerTaskStackBuffer.
-     * Note that, as the array is necessarily of type StackType_t,
-     * configMINIMAL_STACK_SIZE is specified in words, not bytes. */
-    *pulTimerTaskStackSize = configTIMER_TASK_STACK_DEPTH;
-}
 /*-----------------------------------------------------------*/

7938d2e8e4def4b9d2a449a9df9d6b6a03d6a5e2
Paul Bartell <pbartell@amazon.com>
2022-11-17 09:51:04 -0800

    AVR_Dx_Atmel_Studio: Remove co-routine usage

--- FreeRTOS/Demo/AVR_Dx_Atmel_Studio/RTOSDemo/main_minimal.c
+++ FreeRTOS/Demo/AVR_Dx_Atmel_Studio/RTOSDemo/main_minimal.c
@@ -2,12 +2,10 @@
 
 #include "FreeRTOS.h"
 #include "task.h"
-#include "croutine.h"
 #include "PollQ.h"
 #include "integer.h"
 #include "serial.h"
 #include "comtest.h"
-#include "crflash.h"
 #include "partest.h"
 #include "regtest.h"
 
@@ -37,9 +35,6 @@ again. */
 the demo application is not unexpectedly resetting. */
 #define mainRESET_COUNT_ADDRESS     ( 0x1400 )
 
-/* The number of coroutines to create. */
-#define mainNUM_FLASH_COROUTINES    ( 3 )
-
 /*
  * The task function for the "Check" task.
  */
@@ -66,13 +61,10 @@ void main_minimal( void )
     vAltStartComTestTasks( mainCOM_TEST_PRIORITY, mainCOM_TEST_BAUD_RATE, mainCOM_TEST_LED );
     vStartPolledQueueTasks( mainQUEUE_POLL_PRIORITY );
     vStartRegTestTasks();
-    
+
     /* Create the tasks defined within this file. */
     xTaskCreate( vErrorChecks, "Check", configMINIMAL_STACK_SIZE, NULL, mainCHECK_TASK_PRIORITY, NULL );
 
-    /* Create the co-routines that flash the LED's. */
-    vStartFlashCoRoutines( mainNUM_FLASH_COROUTINES );
-    
     /* In this port, to use preemptive scheduler define configUSE_PREEMPTION
     as 1 in portmacro.h.  To use the cooperative scheduler define
     configUSE_PREEMPTION as 0. */
@@ -84,7 +76,7 @@ void init_minimal( void )
     /* Configure UART pins: PC1 Rx, PC0 Tx */
     PORTC.DIR &= ~PIN0_bm;
     PORTC.DIR |= PIN1_bm;
-    
+
     vParTestInitialise();
 }
 
@@ -105,7 +97,7 @@ static volatile unsigned long ulDummyVariable = 3UL;
         integer tasks get some exercise.  The result here is not important -
         see the demo application documentation for more info. */
         ulDummyVariable *= 3;
-        
+
         prvCheckOtherTasksAreStillRunning();
     }
 }
@@ -134,7 +126,7 @@ static portBASE_TYPE xErrorHasOccurred = pdFALSE;
     {
         xErrorHasOccurred = pdTRUE;
     }
-    
+
     if( xErrorHasOccurred == pdFALSE )
     {
         /* Toggle the LED if everything is okay so we know if an error occurs even if not
@@ -156,5 +148,4 @@ unsigned char ucResetCount;
 
 void vApplicationIdleHook( void )
 {
-    vCoRoutineSchedule();
 }

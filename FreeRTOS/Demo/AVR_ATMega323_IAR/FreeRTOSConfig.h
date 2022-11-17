08cfc26a4b6db82351ac717bee1381c47e98ab90
Paul Bartell <pbartell@amazon.com>
2022-11-17 10:04:30 -0800

    AVR_ATMega323_IAR: Remove co-routine usage

--- FreeRTOS/Demo/AVR_ATMega323_IAR/FreeRTOSConfig.h
+++ FreeRTOS/Demo/AVR_ATMega323_IAR/FreeRTOSConfig.h
@@ -38,7 +38,7 @@
  * application requirements.
  *
  * THESE PARAMETERS ARE DESCRIBED WITHIN THE 'CONFIGURATION' SECTION OF THE
- * FreeRTOS API DOCUMENTATION AVAILABLE ON THE FreeRTOS.org WEB SITE. 
+ * FreeRTOS API DOCUMENTATION AVAILABLE ON THE FreeRTOS.org WEB SITE.
  *
  * See http://www.freertos.org/a00110.html
  *----------------------------------------------------------*/
@@ -55,10 +55,6 @@
 #define configUSE_TRACE_FACILITY	0
 #define configUSE_16_BIT_TICKS		1
 #define configIDLE_SHOULD_YIELD		1
-
-/* Co-routine definitions. */
-#define configUSE_CO_ROUTINES 		1
-#define configMAX_CO_ROUTINE_PRIORITIES ( 2 )
 
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */

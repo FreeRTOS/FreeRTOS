1aafdc734dc2e81e7e2d34bfb5eaba6360efafcb
Soren Ptak <skptak@outlook.com>
2022-11-18 12:34:45 -0800

    Updating headers

--- FreeRTOS-Plus/Demo/Common/Logging/windows/Logging_WinSim.c
+++ FreeRTOS-Plus/Demo/Common/Logging/windows/Logging_WinSim.c
@@ -20,7 +20,7 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  * https://www.FreeRTOS.org
- * https://github.com/Freertos
+ * https://github.com/FreeRTOS
  *
  */
 

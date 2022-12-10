6f7f9fd9ed56fbb34d5314af21f17cfa571133a7
jasonpcarroll <jasonpcarroll@users.noreply.github.com>
2022-12-10 01:17:30 +0000

    [AUTO][RELEASE]: Bump file header version to "202212.00"

--- FreeRTOS/Demo/RISC-V_RV64_PolarFire_SoftConsole/FreeRTOSConfig.h
+++ FreeRTOS/Demo/RISC-V_RV64_PolarFire_SoftConsole/FreeRTOSConfig.h
@@ -1,5 +1,5 @@
 /*
- * FreeRTOS V202211.00
+ * FreeRTOS V202212.00
  * Copyright (C) 2021 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of

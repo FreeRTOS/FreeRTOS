a98ad18f08e11b60675a10d8f5a826e980c955bf
ActoryOu <ousc@amazon.com>
2023-02-08 11:08:02 +0800

    Porting to new framework.

--- FreeRTOS/Test/Target/tests/smp/schedule_affinity/schedule_affinity.c
+++ FreeRTOS/Test/Target/tests/smp/schedule_affinity/schedule_affinity.c
@@ -1,8 +1,6 @@
 /*
- * FreeRTOS Kernel <DEVELOPMENT BRANCH>
+ * FreeRTOS V202212.00
  * Copyright (C) 2022 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
- *
- * SPDX-License-Identifier: MIT
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -27,120 +25,111 @@
  */
 
 /**
- * @file test.c
+ * @file schedule_affinity.c
  * @brief The scheduler shall not schedule a task that is pinned to a specific core on any other core.
  *
  * Procedure:
- *   - Create two tasks A & B, each pinned to a specific and different core.
+ *   - Create ( num of cores ) tasks ( T0~Tn-1 ).
+ *   - Pin T0 to core 0, T1 to core 1, and so on.
  *   - Each task will iterate 25 times, with a 10ms yielding delay bectween each
  *     test iteration. The test will confirm it is running on the core it was pinned
  *     to.
  * Expected:
- *   - That both tasks will only run on the cores that they were pinned to.
+ *   - All tasks will only run on the cores that they were pinned to.
  */
 
 /* Kernel includes. */
 #include "FreeRTOS.h" /* Must come first. */
 #include "task.h"     /* RTOS task related API prototypes. */
 
-#include <string.h>
-
-#include "bsl.h"
-#include "unity.h" /* unit testing support functions */
+#include "unity.h"    /* unit testing support functions */
 /*-----------------------------------------------------------*/
 
-/* Priorities at which the tasks are created.  The max priority can be specified
- * as ( configMAX_PRIORITIES - 1 ). */
-#define mainTASK_A_PRIORITY    ( tskIDLE_PRIORITY + 1 )
-#define mainTASK_B_PRIORITY    ( tskIDLE_PRIORITY + 2 )
+/**
+ * @brief Timeout value to stop test.
+ */
+#define TEST_TIMEOUT_MS    ( 10000 )
 /*-----------------------------------------------------------*/
 
-static void vPrvTaskA( void * pvParameters );
-static void vPrvTaskB( void * pvParameters );
-static void fr05_validateTasksOnlyRunOnAssignedCores( void );
-/*-----------------------------------------------------------*/
-
-#if configNUMBER_OF_CORES != 2
-    #error Require two cores be configured for FreeRTOS
+#if ( configNUMBER_OF_CORES < 2 )
+    #error This test is for FreeRTOS SMP and therefore, requires at least 2 cores.
 #endif /* if configNUMBER_OF_CORES != 2 */
 
-#if configUSE_TASK_PREEMPTION_DISABLE != 1
-    #error configUSE_TASK_PREEMPTION_DISABLE shoud be 1 in this test case. Please check if testConfig.h is included.
-#endif /* if configUSE_CORE_AFFINITY != 0 */
+#if ( configUSE_TASK_PREEMPTION_DISABLE != 1 )
+    #error Need to include testConfig.h in FreeRTOSConfig.h
+#endif /* if configRUN_MULTIPLE_PRIORITIES != 0 */
 /*-----------------------------------------------------------*/
 
-static TaskHandle_t xTaskBHandler;
-static BaseType_t xTaskBDone = pdFALSE;
-static BaseType_t xTaskAOnCorrectCore = pdTRUE;
-static BaseType_t xTaskBOnCorrectCore = pdTRUE;
+/**
+ * @brief Test case "Schedule Affinity".
+ */
+void Test_ScheduleAffinity( void );
+
+/**
+ * @brief Function that checks if it's pinned to correct core.
+ */
+static void vPrvTaskCheckPinCore( void * pvParameters );
+
+/**
+ * @brief Function that returns which index does the xCurrntTaskHandle match.
+ *        0 for T0, 1 for T1, -1 for not match.
+ */
+static int lFindTaskIdx( TaskHandle_t xCurrntTaskHandle );
 /*-----------------------------------------------------------*/
 
-static void fr05_validateTasksOnlyRunOnAssignedCores( void )
+/**
+ * @brief Handles of the tasks created in this test.
+ */
+static TaskHandle_t xTaskHanldes[ configNUMBER_OF_CORES ];
+
+/**
+ * @brief Flas to indicate if task T0~Tn-1 finish or not.
+ */
+static BaseType_t xHasTaskFinished[ configNUMBER_OF_CORES ] = { pdFALSE };
+/*-----------------------------------------------------------*/
+
+static int lFindTaskIdx( TaskHandle_t xCurrntTaskHandle )
 {
-    UBaseType_t uxOriginalTaskPriority = uxTaskPriorityGet( NULL );
-    UBaseType_t uxOriginalTaskAffinity = vTaskCoreAffinityGet( NULL );
+    int i = 0;
+    int lMatchIdx = -1;
 
-    vTaskPrioritySet( NULL, mainTASK_A_PRIORITY );
-    vTaskCoreAffinitySet( NULL, 0x1 );
+    for( i = 0; i < configNUMBER_OF_CORES; i++ )
+    {
+        if( xCurrntTaskHandle == xTaskHanldes[ i ] )
+        {
+            lMatchIdx = i;
+            break;
+        }
+    }
 
-    xTaskCreateAffinitySet( vPrvTaskB, "TaskB", configMINIMAL_STACK_SIZE, NULL,
-                            mainTASK_B_PRIORITY, 0x2, &xTaskBHandler );
-
-    taskYIELD();
-
-    vPrvTaskA( NULL );
-
-    vTaskPrioritySet( NULL, uxOriginalTaskPriority );
-    vTaskCoreAffinitySet( NULL, uxOriginalTaskAffinity );
-
-    vTaskDelete( xTaskBHandler );
+    return lMatchIdx;
 }
 /*-----------------------------------------------------------*/
 
-static void vPrvTaskA( void * pvParameters )
+static void vPrvTaskCheckPinCore( void * pvParameters )
 {
+    int lCurrentTaskIdx = -1;
+    uint32_t ulIter;
     BaseType_t xCore;
 
-    uint32_t ulIter;
+    ( void ) pvParameters;
+
+    lCurrentTaskIdx = lFindTaskIdx( xTaskGetCurrentTaskHandle() );
+    TEST_ASSERT_TRUE( lCurrentTaskIdx >= 0 && lCurrentTaskIdx < configNUMBER_OF_CORES );
 
     for( ulIter = 1; ulIter < 25; ulIter++ )
     {
         vTaskDelay( pdMS_TO_TICKS( 10 ) );
         xCore = portGET_CORE_ID();
 
-        if( xCore != 0 )
+        if( xCore != lCurrentTaskIdx )
         {
-            xTaskAOnCorrectCore = pdFALSE;
+            TEST_ASSERT_EQUAL_INT( xCore, lCurrentTaskIdx );
+            break;
         }
     }
 
-    while( ( xTaskBDone == pdFALSE ) )
-    {
-        vPortBusyWaitMicroseconds( ( uint32_t ) 100000 );
-    }
-
-    TEST_ASSERT_TRUE( xTaskAOnCorrectCore == pdTRUE );
-    TEST_ASSERT_TRUE( xTaskBOnCorrectCore == pdTRUE );
-}
-/*-----------------------------------------------------------*/
-
-static void vPrvTaskB( void * pvParameters )
-{
-    BaseType_t xCore;
-    uint32_t ulIter;
-
-    for( ulIter = 1; ulIter < 25; ulIter++ )
-    {
-        vTaskDelay( pdMS_TO_TICKS( 10 ) );
-        xCore = portGET_CORE_ID();
-
-        if( xCore != 1 )
-        {
-            xTaskBOnCorrectCore = pdFALSE;
-        }
-    }
-
-    xTaskBDone = pdTRUE;
+    xHasTaskFinished[ lCurrentTaskIdx ] = pdTRUE;
 
     /* idle the task */
     for( ; ; )
@@ -150,14 +139,75 @@ static void vPrvTaskB( void * pvParameters )
 }
 /*-----------------------------------------------------------*/
 
+void Test_ScheduleAffinity( void )
+{
+    TickType_t xStartTick = xTaskGetTickCount();
+    int i = 0;
+
+    /* Wait other tasks. */
+    for( ; ; )
+    {
+        vTaskDelay( pdMS_TO_TICKS( 100 ) );
+
+        if( ( xTaskGetTickCount() - xStartTick ) / portTICK_PERIOD_MS >= TEST_TIMEOUT_MS )
+        {
+            break;
+        }
+    }
+
+    for( i = 0; i < configNUMBER_OF_CORES; i++ )
+    {
+        TEST_ASSERT_TRUE( xHasTaskFinished[ i ] == pdTRUE );
+    }
+}
+/*-----------------------------------------------------------*/
+
+/* Runs before every test, put init calls here. */
+void setUp( void )
+{
+    int i;
+    BaseType_t xTaskCreationResult;
+
+    /* Create configNUMBER_OF_CORES - 1 low priority tasks. */
+    for( i = 0; i < configNUMBER_OF_CORES; i++ )
+    {
+        xTaskCreationResult = xTaskCreateAffinitySet( vPrvTaskCheckPinCore,
+                                                      "CheckPinCore",
+                                                      configMINIMAL_STACK_SIZE,
+                                                      NULL,
+                                                      configMAX_PRIORITIES - 2,
+                                                      ( 1U << i ),
+                                                      &( xTaskHanldes[ i ] ) );
+
+        TEST_ASSERT_EQUAL_MESSAGE( pdPASS, xTaskCreationResult, "Task creation failed." );
+    }
+}
+/*-----------------------------------------------------------*/
+
+/* Runs after every test, put clean-up calls here. */
+void tearDown( void )
+{
+    int i;
+
+    /* Delete all the tasks. */
+    for( i = 0; i < configNUMBER_OF_CORES; i++ )
+    {
+        if( xTaskHanldes[ i ] )
+        {
+            vTaskDelete( xTaskHanldes[ i ] );
+        }
+    }
+}
+/*-----------------------------------------------------------*/
+
 /**
  * @brief A start entry for test runner to run FR05.
  */
-void vTestRunner( void )
+void vRunScheduleAffinityTest( void )
 {
     UNITY_BEGIN();
 
-    RUN_TEST( fr05_validateTasksOnlyRunOnAssignedCores );
+    RUN_TEST( Test_ScheduleAffinity );
 
     UNITY_END();
 }

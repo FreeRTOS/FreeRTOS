fbb541d9c5c7ad71451983a45f453ee10bcc5c9b
kar-rahul-aws <118818625+kar-rahul-aws@users.noreply.github.com>
2023-02-14 23:39:34 +0530

    Update main.c

--- FreeRTOS-Plus/Demo/FreeRTOS_Plus_TCP_Minimal_Windows_Simulator/main.c
+++ FreeRTOS-Plus/Demo/FreeRTOS_Plus_TCP_Minimal_Windows_Simulator/main.c
@@ -394,3 +394,13 @@ BaseType_t xApplicationGetRandomNumber( uint32_t * pulNumber )
     *( pulNumber ) = uxRand();
     return pdTRUE;
 }
+
+#if ( ( ipconfigUSE_TCP == 1 ) && ( ipconfigUSE_DHCP_HOOK != 0 ) )
+eDHCPCallbackAnswer_t xApplicationDHCPHook(eDHCPCallbackPhase_t eDHCPPhase,
+    uint32_t ulIPAddress)
+{
+    /* Provide a stub for this function. */
+    return eDHCPContinue;
+}
+
+#endif

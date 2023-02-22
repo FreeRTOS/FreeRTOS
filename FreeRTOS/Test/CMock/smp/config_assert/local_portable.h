624c16b832af0c4c252b93809a92f362cf7d336f
Alfred Gedeon <alfred2g@hotmail.com>
2023-02-21 21:06:41 -0800

    UT: config assert tests for multicore

---
+++ FreeRTOS/Test/CMock/smp/config_assert/local_portable.h
@@ -0,0 +1,27 @@
+#ifndef LOCAL_PORTABLE_H
+#define LOCAL_PORTABLE_H
+
+BaseType_t xPortStartScheduler( void );
+
+void vPortEndScheduler( void );
+
+/*
+StackType_t * pxPortInitialiseStack( StackType_t * pxTopOfStack,
+                                     StackType_t * pxEndOfStack,
+                                     TaskFunction_t pxCode,
+                                     void * pvParameters );
+
+StackType_t * pxPortInitialiseStack( StackType_t * pxTopOfStack,
+                                     StackType_t * pxEndOfStack,
+                                     TaskFunction_t pxCode,
+                                     void * pvParameters,
+                                     BaseType_t xRunPrivileged );
+*/
+
+StackType_t * pxPortInitialiseStack( StackType_t * pxTopOfStack,
+                                     TaskFunction_t pxCode,
+                                     void * pvParameters );
+StackType_t * pxPortInitialiseStack( StackType_t * pxTopOfStack,
+                                     TaskFunction_t pxCode,
+                                     void * pvParameters );
+#endif

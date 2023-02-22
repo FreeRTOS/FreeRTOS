4c643649207508d1f502763ee3c2a9da6abf52bc
Alfred Gedeon <alfred2g@hotmail.com>
2023-02-21 21:16:20 -0800

    Remove commented code

--- FreeRTOS/Test/CMock/smp/config_assert/local_portable.h
+++ FreeRTOS/Test/CMock/smp/config_assert/local_portable.h
@@ -5,22 +5,10 @@ BaseType_t xPortStartScheduler( void );
 
 void vPortEndScheduler( void );
 
-/*
-StackType_t * pxPortInitialiseStack( StackType_t * pxTopOfStack,
-                                     StackType_t * pxEndOfStack,
-                                     TaskFunction_t pxCode,
-                                     void * pvParameters );
-
-StackType_t * pxPortInitialiseStack( StackType_t * pxTopOfStack,
-                                     StackType_t * pxEndOfStack,
-                                     TaskFunction_t pxCode,
-                                     void * pvParameters,
-                                     BaseType_t xRunPrivileged );
-*/
-
 StackType_t * pxPortInitialiseStack( StackType_t * pxTopOfStack,
                                      TaskFunction_t pxCode,
                                      void * pvParameters );
+
 StackType_t * pxPortInitialiseStack( StackType_t * pxTopOfStack,
                                      TaskFunction_t pxCode,
                                      void * pvParameters );

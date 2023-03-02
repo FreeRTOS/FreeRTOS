3987bba6fe81361fa6f96c4eb7f7aa196ced2ca3
Keith Packard <keithpac@amazon.com>
2023-03-02 11:50:13 -0800

    Demo/RISC-V_RV32_QEMU_VIRT_GCC: Enable configUSE_TRACE_FACILITY
    I get a build error when this is not set as it (or
    configGENERATE_RUN_TIME_STATS) are required when using
    configUSE_STATS_FORMATTING_FUNCTIONS
    
    Signed-off-by: Keith Packard <keithpac@amazon.com>

--- FreeRTOS/Demo/RISC-V_RV32_QEMU_VIRT_GCC/FreeRTOSConfig.h
+++ FreeRTOS/Demo/RISC-V_RV32_QEMU_VIRT_GCC/FreeRTOSConfig.h
@@ -54,7 +54,7 @@
 #define configMINIMAL_STACK_SIZE		( ( unsigned short ) 120 )
 #define configTOTAL_HEAP_SIZE			( ( size_t ) ( 80 * 1024 ) )
 #define configMAX_TASK_NAME_LEN			( 12 )
-#define configUSE_TRACE_FACILITY		0
+#define configUSE_TRACE_FACILITY		1
 #define configUSE_16_BIT_TICKS			0
 #define configIDLE_SHOULD_YIELD			0
 #define configUSE_MUTEXES				1

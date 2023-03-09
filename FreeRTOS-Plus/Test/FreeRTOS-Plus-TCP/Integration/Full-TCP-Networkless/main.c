47d66489479477a3376c95cec20e1c3b746eef37
Tony Josi <tonyjosi@amazon.com>
2023-03-09 09:09:31 +0530

    Adding compatibility for xApplicationDNSQueryHook with latest dev branch for old demos (#957)
    * adding compatibility for xApplicationDNSQueryHook with latest dev branch
    
    * adding tcp echo server source
    
    * removing unused sub demos

--- FreeRTOS-Plus/Test/FreeRTOS-Plus-TCP/Integration/Full-TCP-Networkless/main.c
+++ FreeRTOS-Plus/Test/FreeRTOS-Plus-TCP/Integration/Full-TCP-Networkless/main.c
@@ -210,7 +210,12 @@ static LONG CALLBACK prvExceptionHandler( _In_ PEXCEPTION_POINTERS ExceptionInfo
 
 #if ( ipconfigUSE_LLMNR != 0 ) || ( ipconfigUSE_NBNS != 0 )
 
-    BaseType_t xApplicationDNSQueryHook( const char * pcName )
+    #if defined( ipconfigIPv4_BACKWARD_COMPATIBLE ) && ( ipconfigIPv4_BACKWARD_COMPATIBLE == 0 )
+        BaseType_t xApplicationDNSQueryHook_Multi( struct xNetworkEndPoint * pxEndPoint,
+                                                            const char * pcName );
+    #else
+        BaseType_t xApplicationDNSQueryHook( const char * pcName )
+    #endif /* if defined( ipconfigIPv4_BACKWARD_COMPATIBLE ) && ( ipconfigIPv4_BACKWARD_COMPATIBLE == 0 ) */
     {
         BaseType_t xReturn;
 

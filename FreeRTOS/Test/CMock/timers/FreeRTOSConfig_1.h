01ce72eccc2383ec8bc2db80afa5d74a2affa090
Paul Bartell <pbartell@amazon.com>
2023-03-14 14:54:02 -0700

    Update timers_1_utest.c for full coverage of xTimerGetStaticBuffer.

--- FreeRTOS/Test/CMock/timers/FreeRTOSConfig_1.h
+++ FreeRTOS/Test/CMock/timers/FreeRTOSConfig_1.h
@@ -72,7 +72,7 @@
 #define configUSE_TASK_NOTIFICATIONS                     1
 #define configTASK_NOTIFICATION_ARRAY_ENTRIES            5
 #define configSUPPORT_STATIC_ALLOCATION                  1
-#define configSUPPORT_DYNAMIC_ALLOCATION                 0
+#define configSUPPORT_DYNAMIC_ALLOCATION                 1
 #define configINITIAL_TICK_COUNT                         ( ( TickType_t ) 0 ) /* For test. */
 #define configSTREAM_BUFFER_TRIGGER_LEVEL_TEST_MARGIN    1                    /* As there are a lot of tasks running. */
 

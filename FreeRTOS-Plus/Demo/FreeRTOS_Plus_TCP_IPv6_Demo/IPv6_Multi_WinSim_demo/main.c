5bf8f6d382bb2ed88e4ad405cc628ef9fa4d576a
Tony Josi <tonyjosi@amazon.com>
2023-03-24 10:37:06 +0530

    replace mainCREATE_UDP_ECHO_SERVER_TASK with mainCREATE_UDP_ECHO_TASKS_SINGLE

--- FreeRTOS-Plus/Demo/FreeRTOS_Plus_TCP_IPv6_Demo/IPv6_Multi_WinSim_demo/main.c
+++ FreeRTOS-Plus/Demo/FreeRTOS_Plus_TCP_IPv6_Demo/IPv6_Multi_WinSim_demo/main.c
@@ -91,14 +91,14 @@
  * connections on the standard echo port (port 7), then echos back any data
  * received on that connection.
  *
- * mainCREATE_UDP_ECHO_SERVER_TASK:  When set to 1 a task is created that sends data
+ * mainCREATE_UDP_ECHO_TASKS_SINGLE:  When set to 1 a task is created that sends data
  * to the address configECHO_SERVER_ADDR_STRING (IPv4/Ipv6) where it is
  * expected to echo back the data, which, the created tasks receives.
  *
  */
 #define mainCREATE_TCP_ECHO_TASKS_SINGLE              1 /* 1 */
 #define mainCREATE_TCP_ECHO_SERVER_TASK               0
-#define mainCREATE_UDP_ECHO_SERVER_TASK               0
+#define mainCREATE_UDP_ECHO_TASKS_SINGLE              0
 /*-----------------------------------------------------------*/
 
 /* Define a task that is used to start and monitor several tests. */
@@ -412,7 +412,7 @@ void vApplicationIPNetworkEventHook_Multi( eIPCallbackEvent_t eNetworkEvent,
                 }
             #endif
 
-            #if ( mainCREATE_UDP_ECHO_SERVER_TASK == 1 )
+            #if ( mainCREATE_UDP_ECHO_TASKS_SINGLE == 1 )
                 {
                     vStartUDPEchoClientTasks_SingleTasks( mainECHO_SERVER_TASK_STACK_SIZE, mainECHO_SERVER_TASK_PRIORITY );
                 }

5b403a6a0737d2f7d0147750a02f3a1140f68c37
Gaurav-Aggarwal-AWS <33462878+aggarg@users.noreply.github.com>
2023-04-19 23:58:07 +0530

    Add reg tests to NuMaker M2351 project (#992)
    * Add reg tests to NuMaker M2351 project
    
    Signed-off-by: Gaurav Aggarwal <aggarg@amazon.com>
    
    * Add reg tests for IAR
    
    Signed-off-by: Gaurav Aggarwal <aggarg@amazon.com>
    
    * Fix formatting check
    
    Signed-off-by: Gaurav Aggarwal <aggarg@amazon.com>
    
    * Fix header check
    
    Signed-off-by: Gaurav Aggarwal <aggarg@amazon.com>
    
    ---------
    
    Signed-off-by: Gaurav Aggarwal <aggarg@amazon.com>

--- FreeRTOS/Demo/CORTEX_MPU_M23_Nuvoton_NuMaker_PFM_M2351_IAR_GCC/Projects/Keil/NonSecure/main_ns.c
+++ FreeRTOS/Demo/CORTEX_MPU_M23_Nuvoton_NuMaker_PFM_M2351_IAR_GCC/Projects/Keil/NonSecure/main_ns.c
@@ -35,6 +35,7 @@
 /* Demo includes. */
 #include "tz_demo.h"
 #include "mpu_demo.h"
+#include "reg_tests.h"
 
 /* Externs needed by the MPU setup code. These are defined in Scatter-Loading
  * description file (FreeRTOSDemo_ns.sct). */
@@ -97,10 +98,10 @@ int main( void )
 	DEBUG_PORT->LINE = UART_WORD_LEN_8 | UART_PARITY_NONE | UART_STOP_BIT_1;
 
 	/* Print banner. */
-	printf( "\n" );
-	printf( "+---------------------------------------------+\n" );
-	printf( "|           Nonsecure is running ...          |\n" );
-	printf( "+---------------------------------------------+\n" );
+	printf( "\r\n" );
+	printf( "+---------------------------------------------+\r\n" );
+	printf( "|           Nonsecure is running ...          |\r\n" );
+	printf( "+---------------------------------------------+\r\n" );
 
 	/* Create tasks. */
 	prvCreateTasks();
@@ -126,6 +127,8 @@ static void prvCreateTasks( void )
 	/* Create tasks for the TZ Demo. */
 	vStartTZDemo();
 
+	/* Create tasks for register tests. */
+	vStartRegTests();
 }
 /*-----------------------------------------------------------*/
 

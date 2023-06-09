193570d8db0bc6144a5c648e0a98667d3971a07d
Xiaodong Li <xiaodonn@amazon.com>
2023-06-09 14:21:40 -0700

    update freertos-plus-tcp-echo-posix git workflow to include echo server setup and git run action

--- FreeRTOS-Plus/Demo/FreeRTOS_Plus_TCP_Echo_Posix/FreeRTOSConfig.h
+++ FreeRTOS-Plus/Demo/FreeRTOS_Plus_TCP_Echo_Posix/FreeRTOSConfig.h
@@ -165,10 +165,10 @@ tasks.
 http://www.freertos.org/FreeRTOS-Plus/FreeRTOS_Plus_TCP/TCP_Echo_Clients.html
 http://www.freertos.org/FreeRTOS-Plus/FreeRTOS_Plus_TCP/UDP_Echo_Clients.html */
 
-#define configECHO_SERVER_ADDR0	127
-#define configECHO_SERVER_ADDR1 0
-#define configECHO_SERVER_ADDR2 0
-#define configECHO_SERVER_ADDR3 1
+#define configECHO_SERVER_ADDR0	192
+#define configECHO_SERVER_ADDR1 30
+#define configECHO_SERVER_ADDR2 255
+#define configECHO_SERVER_ADDR3 112
 
 /* Default MAC address configuration.  The demo creates a virtual network
 connection that uses this MAC address by accessing the raw Ethernet/WiFi data
@@ -185,10 +185,10 @@ configure the real network connection to use. */
 /* Default IP address configuration.  Used in ipconfigUSE_DNS is set to 0, or
 ipconfigUSE_DNS is set to 1 but a DNS server cannot be contacted. */
 
-#define configIP_ADDR0		172
-#define configIP_ADDR1		19
-#define configIP_ADDR2		195
-#define configIP_ADDR3		37
+#define configIP_ADDR0		192
+#define configIP_ADDR1		30
+#define configIP_ADDR2		255
+#define configIP_ADDR3		112
 
 /* Default gateway IP address configuration.  Used in ipconfigUSE_DNS is set to
 0, or ipconfigUSE_DNS is set to 1 but a DNS server cannot be contacted. */

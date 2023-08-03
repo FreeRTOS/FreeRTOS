fc1e70ce76b589df370a24b0a2250e45787cd6c9
Gaurav Aggarwal <aggarg@amazon.com>
2023-08-03 07:12:44 +0000

    Code Review - 3
    Signed-off-by: Gaurav Aggarwal <aggarg@amazon.com>

--- FreeRTOS/Demo/CORTEX_MPU_M3_MPS2_QEMU_GCC/FreeRTOSConfig.h
+++ FreeRTOS/Demo/CORTEX_MPU_M3_MPS2_QEMU_GCC/FreeRTOSConfig.h
@@ -54,7 +54,7 @@ extern void vAssertCalled( void );
 #define configCPU_CLOCK_HZ                               ( ( unsigned long ) 20000000 )
 #define configTICK_RATE_HZ                               ( ( TickType_t ) 1000 )
 #define configMINIMAL_STACK_SIZE                         ( ( unsigned short ) 256 )
-#define configTOTAL_HEAP_SIZE                            ( ( size_t ) ( 1024 ) )
+#define configTOTAL_HEAP_SIZE                            ( ( size_t ) ( 10 * 1024 ) )
 #define configMAX_TASK_NAME_LEN                          ( 10 )
 #define configUSE_TRACE_FACILITY                         1
 #define configUSE_16_BIT_TICKS                           0

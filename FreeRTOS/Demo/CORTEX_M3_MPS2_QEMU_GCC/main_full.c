cfc74b234aa78906578b81bbc528478ba0d9dc1d
Rahul Kar <karahulx@amazon.com>
2023-09-04 17:29:42 +0000

    Reduce duration of prvDemonstrateChangingTimerReloadMode  Task

--- FreeRTOS/Demo/CORTEX_M3_MPS2_QEMU_GCC/main_full.c
+++ FreeRTOS/Demo/CORTEX_M3_MPS2_QEMU_GCC/main_full.c
@@ -910,13 +910,13 @@ static void prvDemonstrateChangingTimerReloadMode( void * pvParameters )
 {
     TimerHandle_t xTimer;
     const char * const pcTimerName = "TestTimer";
-    const TickType_t x100ms = pdMS_TO_TICKS( 100UL );
+    const TickType_t x50ms = pdMS_TO_TICKS( 50UL );
 
     /* Avoid compiler warnings about unused parameter. */
     ( void ) pvParameters;
 
     xTimer = xTimerCreate( pcTimerName,
-                           x100ms,
+                           x50ms,
                            pdFALSE, /* Created as a one-shot timer. */
                            0,
                            prvReloadModeTestTimerCallback );
@@ -924,14 +924,14 @@ static void prvDemonstrateChangingTimerReloadMode( void * pvParameters )
     configASSERT( xTimerIsTimerActive( xTimer ) == pdFALSE );
     configASSERT( xTimerGetTimerDaemonTaskHandle() != NULL );
     configASSERT( strcmp( pcTimerName, pcTimerGetName( xTimer ) ) == 0 );
-    configASSERT( xTimerGetPeriod( xTimer ) == x100ms );
+    configASSERT( xTimerGetPeriod( xTimer ) == x50ms );
 
     /* Timer was created as a one-shot timer.  Its callback just increments the
      * timer's ID - so set the ID to 0, let the timer run for a number of timeout
      * periods, then check the timer has only executed once. */
     vTimerSetTimerID( xTimer, ( void * ) 0 );
     xTimerStart( xTimer, portMAX_DELAY );
-    vTaskDelay( 3UL * x100ms );
+    vTaskDelay( 3UL * x50ms );
     configASSERT( ( ( uintptr_t ) ( pvTimerGetTimerID( xTimer ) ) ) == 1UL );
 
     /* Now change the timer to be an auto-reload timer and check it executes
@@ -939,7 +939,7 @@ static void prvDemonstrateChangingTimerReloadMode( void * pvParameters )
     vTimerSetReloadMode( xTimer, pdTRUE );
     vTimerSetTimerID( xTimer, ( void * ) 0 );
     xTimerStart( xTimer, 0 );
-    vTaskDelay( ( 3UL * x100ms ) + ( x100ms / 2UL ) ); /* Three full periods. */
+    vTaskDelay( ( 3UL * x50ms ) + ( x50ms / 2UL ) ); /* Three full periods. */
     configASSERT( ( uintptr_t ) ( pvTimerGetTimerID( xTimer ) ) == 3UL );
     configASSERT( xTimerStop( xTimer, 0 ) != pdFAIL );
 
@@ -948,7 +948,7 @@ static void prvDemonstrateChangingTimerReloadMode( void * pvParameters )
     vTimerSetReloadMode( xTimer, pdFALSE );
     vTimerSetTimerID( xTimer, ( void * ) 0 );
     xTimerStart( xTimer, 0 );
-    vTaskDelay( 3UL * x100ms );
+    vTaskDelay( 3UL * x50ms );
     configASSERT( xTimerStop( xTimer, 0 ) != pdFAIL );
     configASSERT( ( uintptr_t ) ( pvTimerGetTimerID( xTimer ) ) == 1UL );
 

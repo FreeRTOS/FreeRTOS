12b10e36c7f7faf8dfb1fc23a849da747f9a8341
wly3155 <wuliyong3155@163.com>
2023-09-09 17:18:47 +0800

    RISC-V-Qemu-virt_GCC: correct configCPU_CLOCK_HZ define
    fix configCPU_CLOCK_HZ define for correct freeRTOS tick:
    Qemu simulator runs at 10MHZ, defined by
    RISCV_ACLINT_DEFAULT_TIMEBASE_FREQ at
    https://github.com/qemu/qemu/blob/master/include/hw/intc/riscv_aclint.h

--- FreeRTOS/Demo/RISC-V-Qemu-virt_GCC/FreeRTOSConfig.h
+++ FreeRTOS/Demo/RISC-V-Qemu-virt_GCC/FreeRTOSConfig.h
@@ -48,7 +48,7 @@
 #define configUSE_PREEMPTION			1
 #define configUSE_IDLE_HOOK				0
 #define configUSE_TICK_HOOK				1
-#define configCPU_CLOCK_HZ				( 1000000 )
+#define configCPU_CLOCK_HZ				( 10000000 )
 #define configTICK_RATE_HZ				( ( TickType_t ) 1000 )
 #define configMAX_PRIORITIES			( 7 )
 #define configMINIMAL_STACK_SIZE		( ( unsigned short ) 512 )

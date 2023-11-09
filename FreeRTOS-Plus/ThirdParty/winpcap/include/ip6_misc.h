bec774af3b2f34aae89c4f030bd4112fd7d0dd02
Soren Ptak <ptaksoren@gmail.com>
2023-11-09 14:48:40 -0800

    Fix winpcap re-def of a defined macro

--- FreeRTOS-Plus/ThirdParty/winpcap/include/ip6_misc.h
+++ FreeRTOS-Plus/ThirdParty/winpcap/include/ip6_misc.h
@@ -29,9 +29,11 @@
 
 #include <ws2tcpip.h>
 
-#ifndef __MINGW32__
-    #define IN_MULTICAST( a )    IN_CLASSD( a )
-#endif
+#ifndef IN_MULTICAST
+    #ifndef __MINGW32__
+        #define IN_MULTICAST( a )    IN_CLASSD( a )
+    #endif
+#endif /* IN_MULTICAST */
 
 #define IN_EXPERIMENTAL( a )     ( ( ( ( u_int32_t ) ( a ) ) & 0xf0000000 ) == 0xf0000000 )
 

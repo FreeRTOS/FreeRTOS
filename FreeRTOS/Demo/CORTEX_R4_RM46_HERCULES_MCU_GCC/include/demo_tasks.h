5079bd2ef0c7d8a15640d8c86f762fdce7bea0c5
Soren Ptak <ptaksoren@gmail.com>
2024-01-12 15:21:24 -0500

    Move some of the defines from main.c to demo_tasks.h Add new functions and demos to the file. Use a different bitfield for the demo selection.

--- FreeRTOS/Demo/CORTEX_R4_RM46_HERCULES_MCU_GCC/include/demo_tasks.h
+++ FreeRTOS/Demo/CORTEX_R4_RM46_HERCULES_MCU_GCC/include/demo_tasks.h
@@ -29,29 +29,67 @@
 
 /* ------------------------------ Demo Option ------------------------------ */
 
-/** @brief A Demo that uses static Queues to send info between two tasks */
-#define BLINKY_DEMO 0x1
+/** @brief Create Tasks that are written in assembly to test context swaps */
+#define REGISTER_DEMO       0x1
 
-/** @brief A Demo that causes data aborts and clears them to show MPU usage */
-#define MPU_DEMO    0x2
+/** @brief Demo that uses timers, timer callbacks, and Queues */
+#define QUEUE_DEMO          0x2
 
-/** @brief Set to 1 to select the Blinky Demo, or 2 to select the MPU demo.
- * @note This project provides two demo applications. A simple blinky demo,
- * and a MPU demo that triggers and clears data aborts.
+/** @brief Demo that causes data aborts and clears them to show MPU usage */
+#define MPU_DEMO            0x4
+
+/** @brief Demo that causes and unwinds a Nested IRQ */
+#define IRQ_DEMO            0x8
+
+/** @brief Demo that uses the Task Notification APIs */
+#define NOTIFICATION_DEMO   0x10
+
+/** @brief Bitfield used to select the Demo Tasks to build and run
  *
- * If mainDEMO_TYPE is 1 then the blinky demo from main_blinky.c will be built.
+ * @note This project contains multiple demo and test tasks. A bitfield is used
+ * to select which demos and tests are built and run as part of the executable.
+ * More information about what these demos and tests do can be found in their
+ * corresponding files.
  *
- * If mainDEMO_TYPE is 2 then the MPU demo from mpu_demo.c will be built.
+ * Bit 1 Set: Include the Register Test Tasks
  *
- * If mainDEMO_TYPE is 3 then both of the above demos will be built.
+ * Bit 2 Set: Include the Queue Send and Receive Test Tasks
+ *
+ * Bit 3 Set: Include the MPU Data Abort Test Tasks
+ *
+ * Bit 4 Set: Include the Nested IRQ Test Tasks
+ *
+ * Bit 5 Set: Include the Notification Test Tasks
  *
  */
 #ifndef mainDEMO_TYPE
-    #define mainDEMO_TYPE ( BLINKY_DEMO | MPU_DEMO )
+    #define mainDEMO_TYPE ( REGISTER_DEMO | QUEUE_DEMO | MPU_DEMO | IRQ_DEMO | NOTIFICATION_DEMO )
+    //#define mainDEMO_TYPE ( REGISTER_DEMO | QUEUE_DEMO | MPU_DEMO | IRQ_DEMO | NOTIFICATION_DEMO )
+    //#define mainDEMO_TYPE ( REGISTER_DEMO | QUEUE_DEMO | IRQ_DEMO | NOTIFICATION_DEMO )
+    //#define mainDEMO_TYPE ( REGISTER_DEMO | IRQ_DEMO )
+    //#define mainDEMO_TYPE ( NOTIFICATION_DEMO )
 #endif /* mainDEMO_TYPE */
 
 /* FreeRTOS includes. */
 #include "FreeRTOS.h"
+#include "portmacro.h"
+#include "mpu_wrappers.h"
+
+#define demoREG_PRIVILEGED_TASK_PRIORITY        ( configMAX_PRIORITIES - 2UL )
+#define demoREG_UNPRIVILEGED_TASK_PRIORITY      ( configMAX_PRIORITIES - 1UL )
+
+/** @brief Priority at which the prvQueueSendTask is created. */
+#define demoQUEUE_SEND_TASK_PRIORITY            ( tskIDLE_PRIORITY + 1UL )
+
+/** @brief Priority at which the prvQueueReceiveTask is created. */
+#define demoQUEUE_RECEIVE_TASK_PRIORITY         ( demoQUEUE_SEND_TASK_PRIORITY + 1UL )
+
+#define demoMPU_READ_WRITE_TASK_PRIORITY        ( tskIDLE_PRIORITY + 3UL )
+#define demoMPU_READ_ONLY_TASK_PRIORITY         ( tskIDLE_PRIORITY + 4UL )
+
+#define demoIRQ_TASK_PRIORITY                   ( configTIMER_TASK_PRIORITY + 2UL )
+
+#define demoNOTIFICATION_TASK_PRIORITY          ( configTIMER_TASK_PRIORITY + 1UL )
 
 /* -------------------------- Register Test Tasks -------------------------- */
 
@@ -74,7 +112,7 @@ BaseType_t xCreateRegisterTestTasks( void );
  * pdPASS if all objects are created.
  * pdFAIL if any object cannot be created.
  */
-BaseType_t xCreateBlinkyTasks( void );
+BaseType_t xCreateQueueTasks( void );
 
 /** @brief Create the MPU Tasks that trigger data aborts.
  *
@@ -88,6 +126,61 @@ BaseType_t xCreateBlinkyTasks( void );
  * the fault handler will get stuck in an infinite loop.
  */
 BaseType_t xCreateMPUTasks( void );
+
+/** @brief Create a task that waits for a response from a nested IRQ
+ *
+ * @return pdPASS if tasks are created
+ * pdFAIl if tasks are not created
+ */
+BaseType_t xCreateIRQTestTask( void );
+
+/** @brief Create tasks that send task notifications back and forth.
+ *
+ * @return pdPASS if tasks are created
+ * pdFAIl if tasks are not created
+ */
+BaseType_t xCreateNotificationTestTask( void );
+
+/** @brief Interrupt Handler used for Software Raised Interrupts */
+PRIVILEGED_FUNCTION void vIRQDemoHandler ( void );
+
+/* Registers required to configure the Real Time Interrupt (RTI). */
+#define portRTI_GCTRL_REG       ( *( ( volatile uint32_t * ) 0xFFFFFC00UL ) )
+#define portRTI_TBCTRL_REG      ( *( ( volatile uint32_t * ) 0xFFFFFC04UL ) )
+#define portRTI_COMPCTRL_REG    ( *( ( volatile uint32_t * ) 0xFFFFFC0CUL ) )
+#define portRTI_CNT0_FRC0_REG   ( *( ( volatile uint32_t * ) 0xFFFFFC10UL ) )
+#define portRTI_CNT0_UC0_REG    ( *( ( volatile uint32_t * ) 0xFFFFFC14UL ) )
+#define portRTI_CNT0_CPUC0_REG  ( *( ( volatile uint32_t * ) 0xFFFFFC18UL ) )
+#define portRTI_CNT0_COMP0_REG  ( *( ( volatile uint32_t * ) 0xFFFFFC50UL ) )
+#define portRTI_CNT0_UDCP0_REG  ( *( ( volatile uint32_t * ) 0xFFFFFC54UL ) )
+#define portRTI_SETINTENA_REG   ( *( ( volatile uint32_t * ) 0xFFFFFC80UL ) )
+#define portRTI_CLEARINTENA_REG ( *( ( volatile uint32_t * ) 0xFFFFFC84UL ) )
+#define portRTI_INTFLAG_REG     ( *( ( volatile uint32_t * ) 0xFFFFFC88UL ) )
+
+
+#define portEND_OF_INTERRUPT_REG ( ( ( volatile uint32_t * ) configEOI_ADDRESS ) )
+/* Registers used by the Vectored Interrupt Manager */
+typedef void ( *ISRFunction_t )( void );
+#define mainVIM_IRQ_INDEX   ( *( ( volatile uint32_t * ) 0xFFFFFE00 ) )
+#define mainVIM_IRQ_VEC_REG ( *( ( volatile ISRFunction_t * ) 0xFFFFFE70 ) )
+
+
+#define portSSI_INT_REG_BASE    ( ( ( volatile uint32_t * ) 0xFFFFFFB0 ) )
+
+#define portSSI_INT_REG_ONE     ( ( ( volatile uint32_t * ) 0xFFFFFFB0 ) )
+#define portSSI_ONE_KEY         0x7500UL
+
+#define portSSI_INT_REG_TWO     ( ( ( volatile uint32_t * ) 0xFFFFFFB4 ) )
+#define portSSI_TWO_KEY         0x8400UL
+
+#define portSSI_INT_REG_THREE   ( ( ( volatile uint32_t * ) 0xFFFFFFB8 ) )
+#define portSSI_THREE_KEY       0x9300UL
+
+#define portSSI_INT_REG_FOUR    ( ( ( volatile uint32_t * ) 0xFFFFFFBC ) )
+#define portSSI_FOUR_KEY        0xA200UL
+
+#define portSSI_VEC_REG         ( *( ( volatile uint32_t * ) 0xFFFFFFF4 ) )
+#define portSSI_INTFLAG_REG     ( *( ( volatile uint32_t * ) 0xFFFFFFF8 ) )
 
 /* ---------------------- Shared Function Deceleration ---------------------- */
 

cfd88a68877e935d982663601a2bc62ee0e30887
Ching-Hsin Lee <chinglee@amazon.com>
2024-02-07 17:11:48 +0800

    Fix format and spelling

--- FreeRTOS/Test/Target/tests/smp/only_one_task_enter_suspendall/only_one_task_enter_suspendall.c
+++ FreeRTOS/Test/Target/tests/smp/only_one_task_enter_suspendall/only_one_task_enter_suspendall.c
@@ -1,6 +1,6 @@
 /*
  * FreeRTOS V202212.00
- * Copyright (C) 2022 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ * Copyright (C) 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -73,7 +73,7 @@ static void prvTaskIncCounter( void * pvParameters );
 #endif /* if ( configNUMBER_OF_CORES < 2 ) */
 
 #if ( configMAX_PRIORITIES <= 2 )
-    #error configMAX_PRIORITIES must be larger than 2 to avoid scheduling idle tasks unexpectly.
+    #error configMAX_PRIORITIES must be larger than 2 to avoid scheduling idle tasks unexpectedly.
 #endif /* if ( configMAX_PRIORITIES <= 2 ) */
 /*-----------------------------------------------------------*/
 
@@ -198,7 +198,7 @@ void Test_OnlyOneTaskEnterSuspendAll( void )
 void setUp( void )
 {
     uint32_t i;
-    
+
     xTaskCounter = 0;
 
     for( i = 0; i < configNUMBER_OF_CORES; i++ )

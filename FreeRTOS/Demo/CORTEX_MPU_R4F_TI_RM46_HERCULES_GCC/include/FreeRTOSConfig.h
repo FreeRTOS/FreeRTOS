c8f2578c6b7a94e0a4ff61c5b73caa052501cdd2
Soren Ptak <ptaksoren@gmail.com>
2024-02-27 11:53:03 -0500

    Fix copyright headers in the demo files

--- FreeRTOS/Demo/CORTEX_MPU_R4F_TI_RM46_HERCULES_GCC/include/FreeRTOSConfig.h
+++ FreeRTOS/Demo/CORTEX_MPU_R4F_TI_RM46_HERCULES_GCC/include/FreeRTOSConfig.h
@@ -1,6 +1,6 @@
 /*
- * FreeRTOS V202112.00
- * Copyright (C) 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * FreeRTOS V202212.00
+ * Copyright (C) 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in

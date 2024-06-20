4d362fd38aa4f9a9584110f503dcf9b6e3480ce6
Rahul Kar <118818625+kar-rahul-aws@users.noreply.github.com>
2024-06-20 20:12:30 +0530

    Remove commented line

--- FreeRTOS-Plus/Demo/FreeRTOS_Plus_TCP_Minimal_Windows_Simulator/main.c
+++ FreeRTOS-Plus/Demo/FreeRTOS_Plus_TCP_Minimal_Windows_Simulator/main.c
@@ -186,7 +186,6 @@ int main( void )
         }
         #endif /* ( ipconfigUSE_DHCP != 0 ) */
 
-        /*memcpy( ipLOCAL_MAC_ADDRESS, ucMACAddress, sizeof( ucMACAddress ) ); */
         xResult = FreeRTOS_IPInit_Multi();
     #else /* if defined( ipconfigIPv4_BACKWARD_COMPATIBLE ) && ( ipconfigIPv4_BACKWARD_COMPATIBLE == 0 ) */
         /* Using the old /single /IPv4 library, or using backward compatible mode of the new /multi library. */

8e58d78f30ddd476aad834501bb3fce9b920b756
Gaurav Aggarwal <aggarg@amazon.com>
2024-10-16 17:06:19 +0000

    Address review feedback

--- FreeRTOS/Demo/RISC-V-Qemu-virt_GCC/ns16550.c
+++ FreeRTOS/Demo/RISC-V-Qemu-virt_GCC/ns16550.c
@@ -54,17 +54,17 @@
 
 static uint8_t readb( uintptr_t addr )
 {
-	return *( (uint8_t *) addr );
+	return *( (volatile uint8_t *) addr );
 }
 
 static void writeb( uint8_t b, uintptr_t addr )
 {
-	*( (uint8_t *) addr ) = b;
+	*( (volatile uint8_t *) addr ) = b;
 }
 
 void vOutNS16550( struct device *dev, unsigned char c )
 {
-	volatile uintptr_t addr = dev->addr;
+	uintptr_t addr = dev->addr;
 
 	while ( (readb( addr + REG_LSR ) & LSR_THRE) == 0 ) {
 		/* busy wait */

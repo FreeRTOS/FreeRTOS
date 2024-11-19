f0d78d27530315503c35752141203acbaa6f0eef
tony-josi-aws <tony-josi-aws@users.noreply.github.com>
2024-11-19 07:01:52 +0000

    [AUTO][RELEASE]: Bump file header version to "202123.56"

--- FreeRTOS/Demo/MSP430X_MSP430FR5969_LaunchPad_IAR_CCS/CCS_Only/RegTest.asm
+++ FreeRTOS/Demo/MSP430X_MSP430FR5969_LaunchPad_IAR_CCS/CCS_Only/RegTest.asm
@@ -1,6 +1,6 @@
 ;
 ;/*
-; * FreeRTOS V202212.00
+; * FreeRTOS V202123.56
 ; * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
 ; *
 ; * Permission is hereby granted, free of charge, to any person obtaining a copy of

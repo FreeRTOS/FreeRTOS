2abeb3e52f68eb0da051fa6f7275401da4d60e5e
Florian La Roche <florian.laroche@gmail.com>
2025-03-06 19:44:29 +0100

    Demo/CORTEX_MPS2_QEMU_IAR_GCC: increase min stack size from 80 to 88 (#1332)
    Compiling this demo with picolibc 1.8.9 (as packaged with Debian trixie)
    results in a stack overflow. Increasing the minimal stack size from
    80 to 88 bytes resolves this. (Debian trixie arm crosscompiler defaults
    to compile/link against picolibc and not newlib anymore.)
    
    Signed-off-by: Florian La Roche <Florian.LaRoche@gmail.com>
    Co-authored-by: Gaurav-Aggarwal-AWS <33462878+aggarg@users.noreply.github.com>

--- FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/FreeRTOSConfig.h
+++ FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/FreeRTOSConfig.h
@@ -46,7 +46,7 @@
 #define configUSE_TICK_HOOK                      1
 #define configCPU_CLOCK_HZ                       ( ( unsigned long ) 25000000 )
 #define configTICK_RATE_HZ                       ( ( TickType_t ) 1000 )
-#define configMINIMAL_STACK_SIZE                 ( ( unsigned short ) 80 )
+#define configMINIMAL_STACK_SIZE                 ( ( unsigned short ) 88 )
 #define configTOTAL_HEAP_SIZE                    ( ( size_t ) ( 60 * 1024 ) )
 #define configMAX_TASK_NAME_LEN                  ( 12 )
 

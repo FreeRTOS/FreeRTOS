14d07b9b5ee03e3c3158b4b790d4a47edabd14af
Aditi Chaudhary <aditi311203@gmail.com>
2025-05-20 08:11:34 +0530

    add jobs demo

--- FreeRTOS-Plus/Demo/AWS/Jobs_Windows_Simulator/Jobs_Demo/DemoTasks/JobsDemoExample.c
+++ FreeRTOS-Plus/Demo/AWS/Jobs_Windows_Simulator/Jobs_Demo/DemoTasks/JobsDemoExample.c
@@ -345,13 +345,16 @@ static JobActionType prvGetAction( const char * pcAction,
  * @param[in] pxMqttContext MQTT context pointer.
  * @param[in] pxPacketInfo Packet Info pointer for the incoming packet.
  * @param[in] pxDeserializedInfo Deserialized information from the incoming packet.
+ * @param[out] pReasonCode Pointer to success/failure reason code for sending Publish ACKs. 
+ * @param[out] sendPropsBuffer Buffer containing MQTT 5.0 properties for sending messages.
+ * @param[in] getPropsBuffer Buffer containing MQTT 5.0 properties received from broker.
  */
-static void prvEventCallback(MQTTContext_t* pxMqttContext,
-    MQTTPacketInfo_t* pxPacketInfo,
-    MQTTDeserializedInfo_t* pxDeserializedInfo
-    /*MQTTSuccessFailReasonCode_t* pReasonCode,
-    MqttPropBuilder_t* sendPropsBuffer,
-    MqttPropBuilder_t* getPropsBuffer*/); 
+static void prvEventCallback( MQTTContext_t* pxMqttContext,
+                              MQTTPacketInfo_t* pxPacketInfo,
+                              MQTTDeserializedInfo_t* pxDeserializedInfo,
+                              MQTTSuccessFailReasonCode_t* pReasonCode,
+                              MQTTPropBuilder_t* sendPropsBuffer,
+                              MQTTPropBuilder_t* getPropsBuffer ); 
 
 /**
  * @brief Process payload from NextJobExecutionChanged and DescribeJobExecution
@@ -693,10 +696,10 @@ static void prvNextJobHandler( MQTTPublishInfo_t * pxPublishInfo )
  */
 static void prvEventCallback( MQTTContext_t * pxMqttContext,
                               MQTTPacketInfo_t * pxPacketInfo,
-                              MQTTDeserializedInfo_t * pxDeserializedInfo
-                              /*MQTTSuccessFailReasonCode_t* pReasonCode,
-                              MqttPropBuilder_t* sendPropsBuffer,
-                              MqttPropBuilder_t* getPropsBuffer*/)
+                              MQTTDeserializedInfo_t * pxDeserializedInfo,
+                              MQTTSuccessFailReasonCode_t* pReasonCode,
+                              MQTTPropBuilder_t* sendPropsBuffer,
+                              MQTTPropBuilder_t* getPropsBuffer)
 {
     uint16_t usPacketIdentifier;
 
--- FreeRTOS-Plus/Demo/AWS/Mqtt_Demo_Helpers/mqtt_demo_helpers.c
+++ FreeRTOS-Plus/Demo/AWS/Mqtt_Demo_Helpers/mqtt_demo_helpers.c
@@ -651,7 +651,7 @@ static BaseType_t xHandlePublishResend( MQTTContext_t * pxMqttContext )
                        outgoingPublishPackets[ ucIndex ].packetId ) );
             xMQTTStatus = MQTT_Publish( pxMqttContext,
                                         &outgoingPublishPackets[ ucIndex ].pubInfo,
-                                        outgoingPublishPackets[ ucIndex ].packetId);
+                                        outgoingPublishPackets[ ucIndex ].packetId, NULL);
 
             if( xMQTTStatus != MQTTSuccess )
             {
@@ -728,7 +728,7 @@ BaseType_t xEstablishMqttSession( MQTTContext_t * pxMqttContext,
                                                 pOutgoingPublishRecords,
                                                 mqttexampleOUTGOING_PUBLISH_RECORD_LEN,
                                                 pIncomingPublishRecords,
-                                                mqttexampleINCOMING_PUBLISH_RECORD_LEN );
+                                                mqttexampleINCOMING_PUBLISH_RECORD_LEN , NULL, 0);
 
             if( xMQTTStatus != MQTTSuccess )
             {
@@ -786,7 +786,7 @@ BaseType_t xEstablishMqttSession( MQTTContext_t * pxMqttContext,
                                             &xConnectInfo,
                                             NULL,
                                             mqttexampleCONNACK_RECV_TIMEOUT_MS,
-                                            &sessionPresent);
+                                            &sessionPresent, NULL, NULL);
 
                 if( xMQTTStatus != MQTTSuccess )
                 {
@@ -851,7 +851,7 @@ BaseType_t xDisconnectMqttSession( MQTTContext_t * pxMqttContext,
     if( xMqttSessionEstablished == true )
     {
         /* Send DISCONNECT. */
-        xMQTTStatus = MQTT_Disconnect( pxMqttContext);
+        xMQTTStatus = MQTT_Disconnect( pxMqttContext, NULL, 0);
 
         if( xMQTTStatus != MQTTSuccess )
         {
@@ -896,7 +896,7 @@ BaseType_t xSubscribeToTopic( MQTTContext_t * pxMqttContext,
     xMQTTStatus = MQTT_Subscribe( pxMqttContext,
                                   pSubscriptionList,
                                   sizeof( pSubscriptionList ) / sizeof( MQTTSubscribeInfo_t ),
-                                  globalSubscribePacketIdentifier);
+                                  globalSubscribePacketIdentifier, NULL);
 
     if( xMQTTStatus != MQTTSuccess )
     {
@@ -959,7 +959,7 @@ BaseType_t xUnsubscribeFromTopic( MQTTContext_t * pxMqttContext,
     xMQTTStatus = MQTT_Unsubscribe( pxMqttContext,
                                     pSubscriptionList,
                                     sizeof( pSubscriptionList ) / sizeof( MQTTSubscribeInfo_t ),
-                                    globalUnsubscribePacketIdentifier);
+                                    globalUnsubscribePacketIdentifier, NULL);
 
     if( xMQTTStatus != MQTTSuccess )
     {
@@ -1029,7 +1029,7 @@ BaseType_t xPublishToTopic( MQTTContext_t * pxMqttContext,
         /* Send PUBLISH packet. */
         xMQTTStatus = MQTT_Publish( pxMqttContext,
                                     &outgoingPublishPackets[ ucPublishIndex ].pubInfo,
-                                    outgoingPublishPackets[ ucPublishIndex ].packetId);
+                                    outgoingPublishPackets[ ucPublishIndex ].packetId, NULL);
 
         if( xMQTTStatus != MQTTSuccess )
         {

d58d582eb379683f2371fd40cee1fddb76473f37
Florian La Roche <Florian.LaRoche@gmail.com>
2025-06-10 21:05:26 +0200

    Demo/CORTEX_MPS2_QEMU_IAR_GCC: fix warnings
    * fix compiler warning "gcc -Wmissing-prototypes" in startup_gcc.c
    * fix compiler warning "gcc -Wredundant-decls" in main.c
    * change stack size from 88 to 128
    * change heap from 60 to 64 Kbyte
    
    Signed-off-by: Florian La Roche <Florian.LaRoche@gmail.com>

--- FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/FreeRTOSConfig.h
+++ FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/FreeRTOSConfig.h
@@ -46,8 +46,8 @@
 #define configUSE_TICK_HOOK                      1
 #define configCPU_CLOCK_HZ                       ( ( unsigned long ) 25000000 )
 #define configTICK_RATE_HZ                       ( ( TickType_t ) 1000 )
-#define configMINIMAL_STACK_SIZE                 ( ( unsigned short ) 88 )
-#define configTOTAL_HEAP_SIZE                    ( ( size_t ) ( 60 * 1024 ) )
+#define configMINIMAL_STACK_SIZE                 ( ( unsigned short ) 128 )
+#define configTOTAL_HEAP_SIZE                    ( ( size_t ) ( 64 * 1024 ) )
 #define configMAX_TASK_NAME_LEN                  ( 12 )
 
 /* TODO TraceRecorder (Step 4): Enable configUSE_TRACE_FACILITY in FreeRTOSConfig.h. */

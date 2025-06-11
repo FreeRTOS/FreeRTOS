884070a871df133c35e124ac65dcf5d3dee27f0c
Rahul Kar <118818625+kar-rahul-aws@users.noreply.github.com>
2025-06-11 20:22:57 +0530

    Increase heap size for tracing on

--- FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/FreeRTOSConfig.h
+++ FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/FreeRTOSConfig.h
@@ -47,7 +47,7 @@
 #define configCPU_CLOCK_HZ                       ( ( unsigned long ) 25000000 )
 #define configTICK_RATE_HZ                       ( ( TickType_t ) 1000 )
 #define configMINIMAL_STACK_SIZE                 ( ( unsigned short ) 128 )
-#define configTOTAL_HEAP_SIZE                    ( ( size_t ) ( 64 * 1024 ) )
+#define configTOTAL_HEAP_SIZE                    ( ( size_t ) ( 100 * 1024 ) )
 #define configMAX_TASK_NAME_LEN                  ( 12 )
 
 /* TODO TraceRecorder (Step 4): Enable configUSE_TRACE_FACILITY in FreeRTOSConfig.h. */

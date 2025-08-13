d6b2450efbb83361750219d3838a96ada937abb2
Ubuntu <ubuntu@ip-172-31-15-241.ap-south-1.compute.internal>
2025-08-13 13:33:48 +0000

    Fix formatting check
    Signed-off-by: Ubuntu <ubuntu@ip-172-31-15-241.ap-south-1.compute.internal>

--- FreeRTOS-Plus/Demo/FreeRTOS_Plus_TCP_Echo_Qemu_mps2/main_networking.c
+++ FreeRTOS-Plus/Demo/FreeRTOS_Plus_TCP_Echo_Qemu_mps2/main_networking.c
@@ -142,9 +142,10 @@ static UBaseType_t ulNextRand;
 void main_tcp_echo_client_tasks( void )
 {
     BaseType_t xReturn;
-#ifndef __PICOLIBC__
-    const uint32_t ulLongTime_ms = pdMS_TO_TICKS( 1000UL );
-#endif
+
+    #ifndef __PICOLIBC__
+        const uint32_t ulLongTime_ms = pdMS_TO_TICKS( 1000UL );
+    #endif
 
     /*
      * Instructions for using this project are provided on:
@@ -205,9 +206,9 @@ void main_tcp_echo_client_tasks( void )
      * really applicable to the Linux simulator port). */
     for( ; ; )
     {
-#ifndef __PICOLIBC__
-        usleep( ulLongTime_ms * 1000 );
-#endif
+        #ifndef __PICOLIBC__
+            usleep( ulLongTime_ms * 1000 );
+        #endif
     }
 }
 /*-----------------------------------------------------------*/

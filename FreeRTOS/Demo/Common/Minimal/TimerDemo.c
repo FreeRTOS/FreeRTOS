9ac8db194f96dc69cae2e34d369dd82f2934c392
Florian La Roche <Florian.LaRoche@gmail.com>
2025-08-28 09:29:58 +0200

    Demo/Common: fix divide by zero possibility and non-used return values
    - In TimerDemo.c fix possible divide by zero in
      "xMaxBlockTimeUsedByTheseTests / xCycleFrequency".
    - Move this code in TimerDemo.c into if-clause where it is
      actually used.
    - In Minimal/StreamBufferDemo.c and Minimal/TaskNotify.c avoid
      unused return values.
    
    Signed-off-by: Florian La Roche <Florian.LaRoche@gmail.com>

--- FreeRTOS/Demo/Common/Minimal/TimerDemo.c
+++ FreeRTOS/Demo/Common/Minimal/TimerDemo.c
@@ -230,8 +230,8 @@ static void prvTimerTestTask( void * pvParameters )
 BaseType_t xAreTimerDemoTasksStillRunning( TickType_t xCycleFrequency )
 {
     static uint32_t ulLastLoopCounter = 0UL;
-    TickType_t xMaxBlockTimeUsedByTheseTests, xLoopCounterIncrementTimeMax;
     static TickType_t xIterationsWithoutCounterIncrement = ( TickType_t ) 0, xLastCycleFrequency;
+    TickType_t xMaxBlockTimeUsedByTheseTests, xLoopCounterIncrementTimeMax;
 
     if( xLastCycleFrequency != xCycleFrequency )
     {
@@ -241,17 +241,22 @@ BaseType_t xAreTimerDemoTasksStillRunning( TickType_t xCycleFrequency )
         xLastCycleFrequency = xCycleFrequency;
     }
 
-    /* Calculate the maximum number of times that it is permissible for this
-     * function to be called without ulLoopCounter being incremented.  This is
-     * necessary because the tests in this file block for extended periods, and the
-     * block period might be longer than the time between calls to this function. */
-    xMaxBlockTimeUsedByTheseTests = ( ( TickType_t ) configTIMER_QUEUE_LENGTH ) * xBasePeriod;
-    xLoopCounterIncrementTimeMax = ( xMaxBlockTimeUsedByTheseTests / xCycleFrequency ) + 1;
-
     /* If the demo task is still running then the loop counter is expected to
      * have incremented every xLoopCounterIncrementTimeMax calls. */
     if( ulLastLoopCounter == ulLoopCounter )
     {
+        /* Calculate the maximum number of times that it is permissible for this
+         * function to be called without ulLoopCounter being incremented.  This is
+         * necessary because the tests in this file block for extended periods, and the
+         * block period might be longer than the time between calls to this function. */
+        xMaxBlockTimeUsedByTheseTests = ( ( TickType_t ) configTIMER_QUEUE_LENGTH ) * xBasePeriod;
+        if( xCycleFrequency != ( TickType_t ) 0 )
+        {
+            xLoopCounterIncrementTimeMax = ( xMaxBlockTimeUsedByTheseTests / xCycleFrequency ) + 1;
+        } else {
+            xLoopCounterIncrementTimeMax = ( TickType_t ) 0;
+        }
+
         xIterationsWithoutCounterIncrement++;
 
         if( xIterationsWithoutCounterIncrement > xLoopCounterIncrementTimeMax )
